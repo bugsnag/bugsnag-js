fdd4d9638e6f46232915a6b5ec88d8e0f35f6eba
James Smith <james@loopj.com>
2013-02-11 22:55:03 -0800

    Add docco comments to source file, and generate documentation

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -1,20 +1,97 @@
+//
+// Bugsnag.js is the official JavaScript notifier for
+// [Bugsnag](https://bugsnag.com).
+//
+// Bugsnag gives you instant notification of errors and
+// exceptions in your website's JavaScript code.
+//
+// Bugsnag.js is incredibly small, and has no external dependencies (not even
+// jQuery!) so you can safely use it on any website.
+//
+
+// The `Bugsnag` object is the only globally exported variable
 window.Bugsnag = (function (window, document, navigator) {
   var self = {};
 
-  // Constants
+  //
+  // ### Manual error notification
+  //
+
+  // Notify Bugsnag about a given `exception`, typically that you've caught
+  // with a `try/catch` statement or that you've generated yourself.
+  //
+  // Since most JavaScript exceptions use the `Error` class, we also allow
+  // you to provide a custom error name when calling `notifyException`.
+  self.notifyException = function (exception, name, metaData) {
+    if (typeof metaDataOrName !== "string") {
+      metaData = name;
+    }
+
+    sendToBugsnag({
+      name: name || exception.name,
+      message: exception.message || exception.description,
+      stacktrace: exception.stack || exception.backtrace || exception.stacktrace || generateStacktrace(),
+      file: exception.fileName || exception.sourceURL,
+      lineNumber: exception.lineNumber || exception.line
+    }, metaData);
+  };
+
+  // Notify Bugsnag about an error by passing in a `name` and `message`,
+  // without requiring an exception.
+  self.notify = function (name, message, metaData) {
+    sendToBugsnag({
+      name: name,
+      message: message,
+      stacktrace: generateStacktrace()
+    }, metaData);
+  };
+
+
+  //
+  // ### Automatic error notification
+  //
+
+  // Keep a reference to any existing `window.onerror` handler
+  self._onerror = window.onerror;
+
+  // Attach to `window.onerror` events and notify Bugsnag when they happen.
+  // These are mostly js compile/parse errors, but on some browsers all
+  // "uncaught" exceptions will fire this event.
+  window.onerror = function (message, url, lineNo) {
+    sendToBugsnag({
+      name: "window.onerror",
+      message: message,
+      file: url,
+      lineNumber: lineNo
+    });
+
+    // Fire the existing `window.onerror` handler, if one exists
+    if (self._onerror) {
+      self._onerror(message, url, lineNo);
+    }
+  };
+
+
+  //
+  // ### Helpers & Setup
+  //
+
+  // Compile regular expressions upfront.
   var API_KEY_REGEX = /^[0-9a-f]{32}$/i;
   var FUNCTION_REGEX = /function\s*([\w\-$]+)?\s*\(/i;
-  var MAX_FAKE_STACK_SIZE = 10;
-  var ANONYMOUS_FUNCTION_PLACEHOLDER = "[anonymous]";
+
+  // Set up default notifier settings.
   var DEFAULT_ENDPOINT = "https://notify.bugsnag.com/js";
   var NOTIFIER_VERSION = "<%= pkg.version %>";
 
-  // Keep a reference to the running script
+  // Keep a reference to the currently executing script in the DOM.
+  // We'll use this later to extract settings from attributes.
   var scripts = document.getElementsByTagName("script");
   var thisScript = scripts[scripts.length - 1];
-  var data;
 
-  // Safe console.log wrapper
+  // Simple logging function that wraps `console.log` if available.
+  // This is useful for warning about configuration issues
+  // eg. forgetting to set an API key.
   function log(msg) {
     var console = window.console;
     if (console !== undefined && console.log !== undefined) {
@@ -22,7 +99,9 @@ window.Bugsnag = (function (window, document, navigator) {
     }
   }
 
-  // Serialize an object into a querystring
+  // Deeply serialize an object into a query string. We use the PHP-style
+  // nested object syntax, `nested[keys]=val`, to support heirachical
+  // objects. Similar to jQuery's `$.param` method.
   function serialize(obj, prefix) {
     var str = [];
     var encodeForQueryString = encodeURIComponent;
@@ -35,17 +114,9 @@ window.Bugsnag = (function (window, document, navigator) {
     return str.join("&");
   }
 
-  // Make a GET request with this url and payload
-  function request(url, payload) {
-    if (self.testRequest) {
-      self.testRequest(url, payload);
-    } else {
-      var img = new Image();
-      img.src = url + "?" + serialize(payload) + "&ct=img&cb=" + new Date().getTime();
-    }
-  }
-
-  // Merge source object into target
+  // Deep-merge the `source` object into the `target` object and return
+  // the `target`. Properties in source that will overwrite those in target.
+  // Similar to jQuery's `$.extend` method.
   function merge(target, source) {
     if (source == null) {
       return target;
@@ -69,7 +140,22 @@ window.Bugsnag = (function (window, document, navigator) {
     return target;
   }
 
-  // Get all the data- attributes from the script tag
+  // Make a HTTP request with given `url` and `params` object.
+  // For maximum browser compatibility and cross-domain support, requests are
+  // made by creating a temporary JavaScript `Image` object.
+  function request(url, params) {
+    if (!self.testRequest) {
+      var img = new Image();
+      img.src = url + "?" + serialize(params) + "&ct=img&cb=" + new Date().getTime();
+    } else {
+      self.testRequest(url, params);
+    }
+  }
+
+  // Extrat all `data-*` attributes from a DOM element and return them as an
+  // object. This is used to allow Bugsnag settings to be set via attributes
+  // on the `script` tag, eg. `<script data-apikey="xyz">`.
+  // Similar to jQuery's `$(el).data()` method.
   function getData(node) {
     var dataAttrs = {};
     var dataRegex = /^data\-([\w\-]+)$/;
@@ -85,66 +171,74 @@ window.Bugsnag = (function (window, document, navigator) {
     return dataAttrs;
   }
 
-  // Get a setting from self or data
+  // Get configuration settings from either `self` (the `Bugsnag` object)
+  // or `data` (the `data-*` attributes).
+  var data;
   function getSetting(name) {
     data = data || getData(thisScript);
     return self[name] || data[name.toLowerCase()];
   }
 
-  // Send error details to Bugsnag
+  // Send error details to Bugsnag:
+  //
+  // 1. Validate the configured API key.
+  // 2. Merge the local and global `metaData`.
+  // 3. Work out which endpoint to send to.
+  // 4. Combine error information with other data such as
+  //    user-agent and locale, `metaData` and settings.
+  // 5. Make the HTTP request.
   function sendToBugsnag(details, metaData) {
-    // Validate the API key
     var apiKey = getSetting("apiKey");
     if (apiKey == null || !apiKey.match(API_KEY_REGEX)) {
       log("Invalid API key '" + apiKey + "'");
       return;
     }
 
-    // Fetch and merge metaData objects
     var mergedMetaData = merge(getSetting("metaData"), metaData);
-
-    // Make the request
     var endpoint = getSetting("endpoint") || DEFAULT_ENDPOINT;
+
     var location = window.location;
-    request(endpoint, {
-      // Notifier
+    var payload = {
       notifierVersion: NOTIFIER_VERSION,
 
-      // Bugsnag settings
       apiKey: apiKey,
       projectRoot: getSetting("projectRoot") || location.protocol + "//" + location.host,
       context: getSetting("context") || location.pathname,
       metaData: mergedMetaData,
       releaseStage: getSetting("releaseStage"),
 
-      // Useful data
       url: window.location.href,
       userAgent: navigator.userAgent,
       language: navigator.language || navigator.userLanguage,
 
-      // Error details
       name: details.name,
       message: details.message,
       stacktrace: details.stacktrace,
       file: details.file,
       lineNumber: details.lineNumber
-    });
+    };
+
+    request(endpoint, payload);
   }
 
-  // Generate a stacktrace
+  // Generate a browser stacktrace (or approximation) from the current stack.
+  // This is used to add a stacktrace to `Bugsnag.notify` calls, and to add a
+  // stacktrace approximation where we can't get one from an exception.
   function generateStacktrace() {
     var stacktrace;
+    var MAX_FAKE_STACK_SIZE = 10;
+    var ANONYMOUS_FUNCTION_PLACEHOLDER = "[anonymous]";
 
-    // Try to generate a real stacktrace (most browsers)
+    // Try to generate a real stacktrace (most browsers, except IE9 and below).
     try {
-      throw new Error("stackgen");
+      throw new Error("");
     } catch (exception) {
       stacktrace = exception.stack || exception.backtrace || exception.stacktrace;
     }
 
-    // Otherwise, build a fake stacktrace from the list of method names (IE9 and lower)
+    // Otherwise, build a fake stacktrace from the list of method names by looping
+    // through the list of functions that called this one (and skip whoever called us).
     if (!stacktrace) {
-      // Loop through the list of functions that called this one (and skip whoever called us)
       var functionStack = [];
       var curr = arguments.callee.caller.caller;
       while (curr && functionStack.length < MAX_FAKE_STACK_SIZE) {
@@ -159,60 +253,6 @@ window.Bugsnag = (function (window, document, navigator) {
     return stacktrace;
   }
 
-
-
-  //
-  // Automatic error handlers
-  //
-
-  // window.onerror (mostly js compile/parse errors)
-  self._onerror = window.onerror;
-  window.onerror = function (message, url, lineNo) {
-    sendToBugsnag({
-      name: "window.onerror",
-      message: message,
-      file: url,
-      lineNumber: lineNo
-    });
-
-    // Fire any previous onerror function
-    if (self._onerror) {
-      self._onerror(message, url, lineNo);
-    }
-  };
-
-
-
-  //
-  // Manual error notification
-  //
-
-  // Notify Bugsnag of a named error
-  self.notify = function (name, message, metaData) {
-    sendToBugsnag({
-      name: name,
-      message: message,
-      stacktrace: generateStacktrace()
-    }, metaData);
-  };
-
-  // Notify Bugsnag of an exception
-  self.notifyException = function (exception, metaDataOrName, metaData) {
-    var name;
-    if (typeof metaDataOrName === "string") {
-      name = metaDataOrName;
-    } else {
-      metaData = metaDataOrName;
-    }
-
-    sendToBugsnag({
-      name: name || exception.name,
-      message: exception.message || exception.description,
-      stacktrace: exception.stack || exception.backtrace || exception.stacktrace || generateStacktrace(),
-      file: exception.fileName || exception.sourceURL,
-      lineNumber: exception.lineNumber || exception.line
-    }, metaData);
-  };
-
   return self;
+
 }(window, document, navigator));
