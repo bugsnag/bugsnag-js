2f8c0a2c396ef901511415808c35e2fbb08daf81
Conrad Irwin <conrad.irwin@gmail.com>
2014-01-17 11:44:12 -0800

    moar browsers

--- test/browserstack.js
+++ test/browserstack.js
@@ -55,11 +55,87 @@ browsers = {
     os: 'OS X',
     os_version: 'Mavericks'
   },
-  firefox: {
+  firefox3: {
+    browser: 'Firefox',
+    browser_version: '3.6',
+    os: 'Windows',
+    os_version: '7'
+  },
+  firefox4: {
+    browser: 'Firefox',
+    browser_version: '4',
+    os: 'Windows',
+    os_version: '7'
+  },
+  firefox5: {
+    browser: 'Firefox',
+    browser_version: '5',
+    os: 'Windows',
+    os_version: '7'
+  },
+  firefox10: {
+    browser: 'Firefox',
+    browser_version: '10',
+    os: 'Windows',
+    os_version: '7'
+  },
+  firefox17: {
+    browser: 'Firefox',
+    browser_version: '17',
+    os: 'Windows',
+    os_version: '7'
+  },
+  firefox20: {
+    browser: 'Firefox',
+    browser_version: '20',
+    os: 'Windows',
+    os_version: '7'
+  },
+  firefox24: {
+    browser: 'Firefox',
+    browser_version: '24',
+    os: 'Windows',
+    os_version: '7'
+  },
+  firefox26: {
     browser: 'Firefox',
     browser_version: '26.0',
     os: 'Windows',
     os_Version: '8'
+  },
+  opera12: {
+    browser: 'Opera',
+    browser_version: '12.16',
+    os: 'Windows',
+    os_version: '7'
+  },
+  chrome14: {
+    browser: 'Chrome',
+    browser_version: '14',
+    os: 'Windows',
+    version: '7'
+  },
+  chrome21: {
+    browser: 'Chrome',
+    browser_version: '21',
+    os: 'Windows',
+    version: '7'
+  },
+  chrome31: {
+    browser: 'Chrome',
+    browser_version: '31',
+    os: 'Windows',
+    version: '7'
+  },
+  iphone5s: {
+    browserName: 'iPhone',
+    platform: 'MAC',
+    device: 'iPhone 5s'
+  },
+  iphone5: {
+    browserName: 'iPhone',
+    platform: 'MAC',
+    device: 'iPhone 5'
   }
 };
 
