8efe06f2f571f773b467627e2169da3682058652
Anton Pawlik <anton.pawlik@gmail.com>
2016-09-24 01:16:44 +0200

    Add value check for nodeText function (#184)
    * Add value check for nodeText function
    
    This will make it easer to follow breadcrumbs for events that happened on submit buttons ect.
    
    * Add password security check to nodeText function

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -521,7 +521,10 @@
 
   // extract text content from a element
   function nodeText(el) {
-    var text = el.textContent || el.innerText || "";
+    if (el.getAttribute('type') === 'password') {
+      return 'Password value can not be shown or it is empty';
+    }
+    var text = el.textContent || el.innerText || el.value || "";
     text = text.replace(/^\s+|\s+$/g, ""); // trim whitespace
     return truncate(text, 140);
   }
