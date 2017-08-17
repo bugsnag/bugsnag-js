fb526b98294978ec1764a6b36c2a2e03d7382de6
Ben Gourley <bn@grly.me>
2017-08-17 10:09:48 +0100

    Tidy-up whitespace

--- examples/angular4/src/app/app.module.ts
+++ examples/angular4/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule, ErrorHandler }      from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BugsnagErrorHandler } from '../error-handler'
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ],
-  providers:    [ { provide: ErrorHandler, useClass: BugsnagErrorHandler } ]
+  bootstrap: [ AppComponent ],
+  providers: [ { provide: ErrorHandler, useClass: BugsnagErrorHandler } ]
 })
 
 export class AppModule { }
