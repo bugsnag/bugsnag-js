1f1ad649bf133ec7a2b6e1c594d3de1d086de874
Ben Gourley <bn@grly.me>
2017-08-24 09:26:38 +0100

    Add coveralls integration 🤞

--- karma.conf.js
+++ karma.conf.js
@@ -7,10 +7,11 @@ module.exports = function(config) {
     coverageReporter: {
       reporters: [
         { type: "text-summary" },
-        { type: "html" }
+        { type: "html" },
+        { type: "lcov" }
       ]
     },
-    reporters: [ "coverage", "progress", "saucelabs" ],
+    reporters: [ "coverage", "progress", "saucelabs" ].concat(process.env.TRAVIS ? [ "coveralls" ] : []),
     browsers: []
     .concat("PhantomJS")
     .concat(
