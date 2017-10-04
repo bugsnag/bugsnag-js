345a8bb856bba2530bb1fad822e58af80053f8db
Ben Gourley <bn@grly.me>
2017-10-04 11:50:50 +0100

    Reference bs_* browsers

--- karma-legacy.conf.js
+++ karma-legacy.conf.js
@@ -1,7 +1,7 @@
 var browsers = require("./browsers.json");
 var common = require('./karma-common.js');
 
-var LEGACY_BROWSERS = [ 'sl_ie_6', 'sl_ie_7', 'sl_ie_8', 'sl_ie_9' ]
+var LEGACY_BROWSERS = [ 'bs_ie_6', 'bs_ie_7', 'bs_ie_8', 'bs_ie_9' ]
 
 module.exports = function(config) {
   config.set(Object.assign({}, common, {
