579a84029592047cf86c3a6ef1dd16e6c8a64f6e
Ben Gourley <bn@grly.me>
2017-11-14 14:36:32 +0000

    Grab HTML when the script executes

--- browser/plugins/inline-script-content.js
+++ browser/plugins/inline-script-content.js
@@ -2,6 +2,7 @@ const { map } = require('../../base/lib/es-utils')
 
 module.exports = {
   init: (client) => {
+    const html = document.all[0].outerHTML
     const addInlineContent = report => {
       report.stacktrace = map(report.stacktrace, frame => {
         if (frame.file.replace(/#.*$/) !== window.location.href.replace(/#.*$/)) return frame
@@ -9,7 +10,7 @@ module.exports = {
         const start = Math.max(0, frame.lineNumber - 5 - 1)
         const end = frame.lineNumber + 5 - 1
         const code = {}
-        map([ '<!-- DOCUMENT START -->' ].concat(document.all[0].outerHTML.split('\n')).slice(start, end), (line, i) => {
+        map([ '<!-- DOCUMENT START -->' ].concat(html.split('\n')).slice(start, end), (line, i) => {
           code[`${start + i + 1}`] = line
         })
         return { ...frame, code }
