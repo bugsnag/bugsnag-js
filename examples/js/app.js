e21b9377f4f8f8cb069857f101a9fa2f3fa0db1d
tremlab <rbalmert@gmail.com>
2017-12-12 22:40:02 -0800

    formatting corrections.

--- examples/js/app.js
+++ examples/js/app.js
@@ -2,23 +2,19 @@
 // www.bugsnag.com
 // https://github.com/bugsnag/bugsnag-js/tree/master/examples/js
 //
-// this example app demosntrates some of the basic syntax to get
-// Bugsnag error reporting configured in your Javascript code.
+// this example app demonstrates some of the basic syntax to get Bugsnag error reporting configured in your Javascript code.
 // ***********************************************************
 
 document.getElementById("jsHandled").addEventListener("click", send_handled); 
 document.getElementById("jsUnhandled").addEventListener("click", send_unhandled); 
 
 
-// Below function will catch an error, and shows how 
-// you can add/modify information to the report right before sending.
-// Note that the configuration and beforeSend defined in the earlier 
-// initialization of bugsnag (in the html) will be applied *after* 
-// the statements executed here in notify().
+// Below function will catch an error, and shows how you can add/modify information to the report right before sending.
+// Note that the configuration and beforeSend defined in the earlier initialization of bugsnag (in the html) will be applied *after* the statements executed here in notify().
 function send_handled() { 
-  console.log("handled sent")
+    console.log("a handled error with metadata has been reported to your Bugsnag dashboard");
     try {
-      // deliberate Reference Error
+        // deliberate Reference Error
         console.log(doesntExist);
     } 
     catch (e) {
@@ -29,8 +25,8 @@ function send_handled() {
             // Note that metadata can be declared globally, 
             // in the noticification (as below) or in a beforeSend.
             metaData: {
-              company: {
-                  name: "Xavier's School for Gifted Youngsters"
+                company: {
+                    name: "Xavier's School for Gifted Youngsters"
                 }
             }
           });           
@@ -38,11 +34,9 @@ function send_handled() {
 }
 
 
-// Below function will trigger an unhandled error
-// which will report to bugsnag, along with all the info configured
-// in the initialization of bugsnag in the html.
+// Below function will trigger an unhandled error which will report to bugsnag, along with all the info configured in the initialization of bugsnag in the html.
 function send_unhandled() {
-    console.log("unhandled sent")
+    console.log("an unhandled error has been reported to your Bugsnag dashboard");
     var num = 1;
     // deliberate Type Error
     num.toUpperCase(); 
@@ -50,8 +44,3 @@ function send_unhandled() {
 
 // below is the simplest notification syntax, akin to logging.
 bugsnagClient.notify("End of file");
-
-
-
-
-
