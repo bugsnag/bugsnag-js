dc5e5d46daccf27c6a7780385b054d97f3daf2f9
Ben Gourley <bn@grly.me>
2017-12-14 16:03:40 +0000

    Export more types. Fixes #282.

--- types/bugsnag.d.ts
+++ types/bugsnag.d.ts
@@ -1,13 +1,13 @@
-import { Client, IPlugin } from "./client";
+import * as Bugsnag from "./client";
 import { IConfig } from "./common";
 import Report from "./report";
 
 // two ways to call the exported function: apiKey or config object
 declare function createBugsnagClient(
   apiKeyOrOpts: string | IConfig,
-  plugins?: IPlugin[],
-): Client;
+  plugins?: Bugsnag.IPlugin[],
+): Bugsnag.Client;
 
 // commonjs/requirejs export
 export default createBugsnagClient;
-export { Client, IPlugin };
+export { Bugsnag };
