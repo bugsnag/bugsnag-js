336ad840f95c0988cdb2fd8f9dbadaa319ea2282
tremlab <rbalmert@gmail.com>
2018-01-08 23:07:17 -0800

    added buttons to trigger error, trying to adapt initialization to new user/metadata syntax, trying to throw exception.

--- examples/typescript/app.ts
+++ examples/typescript/app.ts
@@ -20,41 +20,59 @@ var bugsnagClient = bugsnag({
         //  defines which release stages bugsnag should report. e.g. ignore staging errors.
         notifyReleaseStages: [ 'development', 'production'],
 
-        // one of the most powerful tools in our library, beforeSend lets you evaluate, modify, add and remove data before sending to bugsnag. The actions here will be applied to *all* errors, handled and unhandled.
-
+        // one of the most powerful tools in our library, beforeSend lets you evaluate, modify, add and remove data before sending the error to bugsnag. The actions here will be applied to *all* errors, handled and unhandled.
         beforeSend: function (report) {
-          report.user = {
-            name: "Grace Hopper",
-            email: "ghopper@code.com",
-            id: "123456789"
-          };
-          // add any custom attributes relevant to your app. Note that metadata can be added here, in a specific notify (example bleow) or globally.
-          // using updateMetaData() to preserve any metadata already attached to the report object.
-          report.updateMetaData('company', {
-            name: "Xavier's School for Gifted Youngsters"
-          })
+          if (report.errorClass === "Error") {
+            report.severity = "info"
+          }
         },
+
+        // user: {
+        //   name: "Grace Hopper",
+        //   email: "ghopper@code.com",
+        //   id: "123456789"
+        // },
+
+        // // add any custom attributes relevant to your app. Note that metadata can be added here, in a specific notify or in a beforeSend.
+        // metaData: { company: {
+        //   name: "Xavier's School for Gifted Youngsters"
+        //   }
+        // },
 
         // because this is a demo app, below extends the default of 10 notifications per pageload. click away!
         maxEvents: 50
       })
 
 
+var handled: HTMLElement = document.getElementById('jsHandled')!
+handled.addEventListener('click', sendHandled)
+
+var unhandled: HTMLElement = document.getElementById('jsUnhandled')!
+unhandled.addEventListener('click', sendUnhandled)
+
 var el: HTMLInputElement = <HTMLInputElement> document.getElementById('jsondata')
 var rawjson: string = el.value || ''
 
-// below will send a handled error to your dashboard.  It will contain both the metadata added here (content) and the metadata added in the beforeSend above (company).
-try {
-  JSON.parse(rawjson)
-} catch (e) {
-  console.log('a handled error with 2 sets of metadata has been reported to your Bugsnag dashboard')
-  bugsnagClient.notify(e, {
-    metaData: {
-      content:  { rawjson: rawjson }
-    }
-  })
+function sendHandled() {
+  try {
+    // potentially buggy code goes here
+    JSON.parse(rawjson)
+  } catch (e) {
+    console.log('a handled error with 2 sets of metadata has been reported to your Bugsnag dashboard')
+    bugsnagClient.notify(e, {
+      metaData: {
+        content:  { rawjson: rawjson }
+      }
+    })
+  }
+}
+
+function sendUnhandled() {
+  console.log('an unhandled error has been reported to your Bugsnag dashboard')
+  // below is throwing and exception, but all data is missing. :(
+  throw('Bad thing!')
 }
 
 // below is the simplest notification syntax, akin to logging.
 console.log('a notification with 1 set of metadata has been reported to your Bugsnag dashboard')
-bugsnagClient.notify("End of file");
+bugsnagClient.notify("End of file")
