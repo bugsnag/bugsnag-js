1c26b2279c7fa6e470de1dac8a4d73c64f7cc1d6
Ben Gourley <bn@grly.me>
2018-01-18 14:11:45 +0000

    Improve unhandled promise rejection support
    - extract errors from where bluebird puts them
    - serialize rejection reasons better (null, undefined, DOMException)
    - Don't send a stacktrace if it originates in Bugsnag's code
    - Hint to check the promise tab in error message

--- browser/plugins/unhandled-rejection.js
+++ browser/plugins/unhandled-rejection.js
@@ -1,34 +1,95 @@
 const ErrorStackParser = require('error-stack-parser')
 const hasStack = require('../../base/lib/has-stack')
+const { reduce } = require('../../base/lib/es-utils')
 
 /*
  * Automatically notifies Bugsnag when window.onunhandledrejection is called
  */
-
+let _listener
 module.exports = {
   init: (client) => {
-    // only attach for browsers that suppport promises
-    if (!('onunhandledrejection' in window)) return
+    if (!('addEventListener' in window)) return
+    const listener = event => {
+      let error = event.reason
+      let isBluebird = false
 
-    window.addEventListener('unhandledrejection', (event) => {
-      const error = event.reason
-      const handledState = { severity: 'error', unhandled: true, severityReason: { type: 'unhandledPromiseRejection' } }
+      if (event.detail && event.detail.reason) {
+        error = event.detail.reason
+        isBluebird = true
+      }
+
+      const handledState = {
+        severity: 'error',
+        unhandled: true,
+        severityReason: { type: 'unhandledPromiseRejection' }
+      }
 
       let report
       if (error && hasStack(error)) {
         // if it quacks like an Error…
         report = new client.BugsnagReport(error.name, error.message, ErrorStackParser.parse(error), handledState)
+        if (isBluebird) {
+          report.stacktrace = reduce(report.stacktrace, fixBluebirdStacktrace(error), [])
+        }
       } else {
         // if it doesn't…
-        const msg = typeof error === 'string' ? error : 'Unhandled promise rejection'
-        const err = new Error(msg)
-        err.type = 'UnhandledRejection'
-        report = new client.BugsnagReport(err.type, err.message, ErrorStackParser.parse(err), handledState)
+        const msg = typeof error === 'string'
+          ? error
+          : 'Rejection reason was not an Error. See "Promise" tab for more detail.'
+        report = new client.BugsnagReport('UnhandledRejection', msg, [], handledState)
         // stuff the rejection reason into metaData, it could be useful
-        report.updateMetaData('promise', 'rejection reason', error)
+        report.updateMetaData('promise', 'rejection reason', serializableReason(error))
       }
 
       client.notify(report)
-    })
+    }
+    window.addEventListener('unhandledrejection', listener)
+    _listener = listener
+  },
+  destroy: () => {
+    if (_listener) window.removeEventListener('unhandledrejection', _listener)
+    _listener = null
   }
+}
+
+const serializableReason = (err) => {
+  if (err === null || err === undefined) {
+    return 'undefined (or null)'
+  } else if (err.name && err.message) {
+    return {
+      'DOMException': {
+        name: err.name,
+        message: err.message,
+        code: err.code
+      }
+    }
+  } else {
+    return err
+  }
+}
+
+// The stack parser on bluebird stacks in FF get a suprious first frame:
+//
+// Error: derp
+//   b@http://localhost:5000/bluebird.html:22:24
+//   a@http://localhost:5000/bluebird.html:18:9
+//   @http://localhost:5000/bluebird.html:14:9
+//
+// results in
+//   […]
+//     0: Object { file: "Error: derp", method: undefined, lineNumber: undefined, … }
+//     1: Object { file: "http://localhost:5000/bluebird.html", method: "b", lineNumber: 22, … }
+//     2: Object { file: "http://localhost:5000/bluebird.html", method: "a", lineNumber: 18, … }
+//     3: Object { file: "http://localhost:5000/bluebird.html", lineNumber: 14, columnNumber: 9, … }
+//
+// so the following reduce/accumulator function removes such frames
+//
+// Bluebird pads method names with spaces so trim that too…
+// https://github.com/petkaantonov/bluebird/blob/b7f21399816d02f979fe434585334ce901dcaf44/src/debuggability.js#L568-L571
+const fixBluebirdStacktrace = (error) => (accum, frame) => {
+  if (frame.file === error.toString()) return accum
+  if (frame.method) {
+    frame.method = frame.method.replace(/^\s+/, '')
+  }
+  return accum.concat(frame)
 }
