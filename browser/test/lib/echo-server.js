7fb3ccc6a7c8d4ae66670096049dfb9067df4bf6
Ben Gourley <bn@grly.me>
2018-04-03 14:39:10 +0100

    test(plugins): Refactor network plugins tests
    - make the tests work offline/not hit real APIs
    - use jasmine's built-in setup/teardown mechanism

--- browser/test/lib/echo-server.js
+++ browser/test/lib/echo-server.js
@@ -1,12 +1,16 @@
 const http = require('http')
+const { parse } = require('url')
 
 console.log(`[ECHO SERVER] starting up`)
 http.createServer((req, res) => {
   console.log(`[ECHO SERVER] ${req.method} ${req.url}`)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Headers', 'content-type, bugsnag-api-key, bugsnag-sent-at, bugsnag-payload-version')
-    return res.end()
+  // append ?nocors to the request url to prevent CORS headers from being set
+  if (!('nocors' in parse(req.url, true).query)) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Headers', 'content-type, bugsnag-api-key, bugsnag-sent-at, bugsnag-payload-version')
+      return res.end()
+    }
   }
   let body = ''
   req.on('data', d => { body += d })
