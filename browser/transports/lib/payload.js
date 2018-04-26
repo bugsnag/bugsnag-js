15b00df30570da342f34d36a1b7bd6e9bf2f5669
Ben Gourley <bn@grly.me>
2018-04-26 11:26:56 +0100

    perf(misc): Shorten error message to improve bundle size

--- browser/transports/lib/payload.js
+++ browser/transports/lib/payload.js
@@ -7,8 +7,8 @@ module.exports = report => {
     report.events[0].metaData = {
       notifier:
 `WARNING!
-The serialized payload was ${payload.length / 10e5}MB. The limit is 1MB.
-report.metaData was stripped to make the payload of a deliverable size.`
+Serialized payload was ${payload.length / 10e5}MB (limit = 1MB)
+metaData was removed`
     }
     payload = jsonStringify(report)
     if (payload.length > 10e5) throw new Error('payload exceeded 1MB limit')
