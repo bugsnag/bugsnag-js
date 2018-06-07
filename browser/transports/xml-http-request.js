8068a9a9ecc6c6187a35a78650dc8eedf0e43937
Ben Gourley <bn@grly.me>
2018-06-07 12:06:04 +0100

    fix: Workaround CSP violation error in iOS9 Safari
    Fixes #357.

--- browser/transports/xml-http-request.js
+++ browser/transports/xml-http-request.js
@@ -4,34 +4,34 @@ const { isoDate } = require('../../base/lib/es-utils')
 
 module.exports = {
   sendReport: (logger, config, report, cb = () => {}) => {
-    const url = config.endpoints.notify
-    const req = new window.XMLHttpRequest()
-    req.onreadystatechange = function () {
-      if (req.readyState === window.XMLHttpRequest.DONE) cb(null, req.responseText)
-    }
-    req.open('POST', url)
-    req.setRequestHeader('Content-Type', 'application/json')
-    req.setRequestHeader('Bugsnag-Api-Key', report.apiKey || config.apiKey)
-    req.setRequestHeader('Bugsnag-Payload-Version', '4.0')
-    req.setRequestHeader('Bugsnag-Sent-At', isoDate())
     try {
+      const url = config.endpoints.notify
+      const req = new window.XMLHttpRequest()
+      req.onreadystatechange = function () {
+        if (req.readyState === window.XMLHttpRequest.DONE) cb(null, req.responseText)
+      }
+      req.open('POST', url)
+      req.setRequestHeader('Content-Type', 'application/json')
+      req.setRequestHeader('Bugsnag-Api-Key', report.apiKey || config.apiKey)
+      req.setRequestHeader('Bugsnag-Payload-Version', '4.0')
+      req.setRequestHeader('Bugsnag-Sent-At', isoDate())
       req.send(makePayload(report))
     } catch (e) {
       logger.error(e)
     }
   },
   sendSession: (logger, config, session, cb = () => {}) => {
-    const url = config.endpoints.sessions
-    const req = new window.XMLHttpRequest()
-    req.onreadystatechange = function () {
-      if (req.readyState === window.XMLHttpRequest.DONE) cb(null, req.responseText)
-    }
-    req.open('POST', url)
-    req.setRequestHeader('Content-Type', 'application/json')
-    req.setRequestHeader('Bugsnag-Api-Key', config.apiKey)
-    req.setRequestHeader('Bugsnag-Payload-Version', '1.0')
-    req.setRequestHeader('Bugsnag-Sent-At', isoDate())
     try {
+      const url = config.endpoints.sessions
+      const req = new window.XMLHttpRequest()
+      req.onreadystatechange = function () {
+        if (req.readyState === window.XMLHttpRequest.DONE) cb(null, req.responseText)
+      }
+      req.open('POST', url)
+      req.setRequestHeader('Content-Type', 'application/json')
+      req.setRequestHeader('Bugsnag-Api-Key', config.apiKey)
+      req.setRequestHeader('Bugsnag-Payload-Version', '1.0')
+      req.setRequestHeader('Bugsnag-Sent-At', isoDate())
       req.send(jsonStringify(session))
     } catch (e) {
       logger.error(e)
