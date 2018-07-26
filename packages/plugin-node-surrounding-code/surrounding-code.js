38b64cc83d70e722c30bbbeae2d916902a6e5420
Ben Gourley <bn@grly.me>
2018-07-26 14:08:07 +0100

    perf: Use streams for surrounding code loading
    It's possible for very large source files to cause the surrounding code loading plugin to run out of
    memory. This switches the implementation to use streams so that it uses much less memory.

--- packages/plugin-node-surrounding-code/surrounding-code.js
+++ packages/plugin-node-surrounding-code/surrounding-code.js
@@ -1,5 +1,8 @@
 const SURROUNDING_LINES = 3
-const { readFile } = require('fs')
+const { createReadStream } = require('fs')
+const { Writable } = require('stream')
+const pump = require('pump')
+const byline = require('byline')
 
 module.exports = {
   init: client => {
@@ -28,16 +31,56 @@ module.exports = {
 }
 
 const getSurroundingCode = (file, lineNumber, cb) => {
-  readFile(file, 'utf8', (err, data) => {
-    if (err) return cb(err)
-    const lines = data.split('\n')
-    const start = Math.max(0, lineNumber - SURROUNDING_LINES - 1)
-    const end = Math.min(lines.length, lineNumber + SURROUNDING_LINES)
-    cb(null, lines
-      .slice(start, end)
-      .reduce((accum, line, i) => {
-        accum[start + (i + 1)] = line
-        return accum
-      }, {}))
+  const start = lineNumber - SURROUNDING_LINES
+  const end = lineNumber + SURROUNDING_LINES
+
+  const reader = createReadStream(file, { encoding: 'utf8' })
+  const splitter = new byline.LineStream({ keepEmptyLines: true })
+  const slicer = new CodeRange({ start, end })
+
+  // if the slicer has enough lines already, no need to keep reading from the file
+  slicer.on('done', () => reader.destroy())
+
+  pump(reader, splitter, slicer, (err) => {
+    // reader.destroy() causes a "premature close" error which we can tolerate
+    if (err && err.message !== 'premature close') return cb(err)
+    cb(null, slicer.getCode())
   })
+}
+
+// This writable stream takes { start, end } options specifying the
+// range of lines that should be extracted from a file. Pipe a readable
+// stream to it that provides source lines as each chunk. If the range
+// is satisfied before the end of the readable stream, it will emit the
+// 'done' event. Once a 'done' or 'finish' event has been seen, call getCode()
+// to get the range in the following format:
+// {
+//   '10': 'function getSquare (cb) {',
+//   '11': '  rectangles.find({',
+//   '12': '    length: 12',
+//   '13': '    width: 12',
+//   '14': '  }, err => cb)',
+//   '15': '}'
+// }
+class CodeRange extends Writable {
+  constructor (opts) {
+    super({ ...opts, decodeStrings: false })
+    this._start = opts.start
+    this._end = opts.end
+    this._n = 0
+    this._code = {}
+  }
+  _write (chunk, enc, cb) {
+    this._n++
+    if (this._n < this._start) return cb(null)
+    if (this._n <= this._end) {
+      this._code[String(this._n)] = chunk
+      return cb(null)
+    }
+    this.emit('done')
+    return cb(null)
+  }
+  getCode () {
+    return this._code
+  }
 }
