dca553f006b765013093099f157bd857a63c49c4
Ben Gourley <bn@grly.me>
2018-07-27 09:59:34 +0100

    refactor: Move statement outside of loop

--- packages/plugin-node-in-project/in-project.js
+++ packages/plugin-node-in-project/in-project.js
@@ -3,13 +3,11 @@ const normalizePath = require('@bugsnag/core/lib/path-normalizer')
 
 module.exports = {
   init: client => client.config.beforeSend.push(report => {
+    const projectRoot = normalizePath(client.config.projectRoot)
     report.stacktrace = map(report.stacktrace, stackframe => {
-      const projectRoot = normalizePath(client.config.projectRoot)
-
       stackframe.inProject = typeof stackframe.file === 'string' &&
         stackframe.file.indexOf(projectRoot) === 0 &&
         !/\/node_modules\//.test(stackframe.file)
-
       return stackframe
     })
   })
--- packages/plugin-strip-project-root/strip-project-root.js
+++ packages/plugin-strip-project-root/strip-project-root.js
@@ -3,13 +3,11 @@ const normalizePath = require('@bugsnag/core/lib/path-normalizer')
 
 module.exports = {
   init: client => client.config.beforeSend.push(report => {
+    const projectRoot = normalizePath(client.config.projectRoot)
     report.stacktrace = map(report.stacktrace, stackframe => {
-      const projectRoot = normalizePath(client.config.projectRoot)
-
       if (typeof stackframe.file === 'string' && stackframe.file.indexOf(projectRoot) === 0) {
         stackframe.file = stackframe.file.replace(projectRoot, '')
       }
-
       return stackframe
     })
   })
