a844351db63122f3c4630851a61eb8f03bc97121
Ben Gourley <bn@grly.me>
2018-07-27 15:57:27 +0100

    fix: Add missing properties to breadcrumb.d.ts
    Fixes #372

--- types/breadcrumb.d.ts
+++ types/breadcrumb.d.ts
@@ -1,4 +1,8 @@
 declare class Breadcrumb {
+  public name: string;
+  public metaData: object;
+  public type: string;
+  public timestamp: string;
   constructor(name: string, metaData?: object, type?: string, timestamp?: string);
 }
 
