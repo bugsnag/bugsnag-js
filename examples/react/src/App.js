cfe569d6a96ef80fe3c368bc65fc21db570d832a
Ben Gourley <bn@grly.me>
2018-11-06 15:48:23 +0000

    docs(examples): Add React example

---
+++ examples/react/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css'
+import BadButtons from './components/BadButtons'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <img width='200' src='bugsnag.png' alt='Bugsnag Logo' />
+        <h1>React example</h1>
+        <p>
+          This is an example of how to include <code>@bugsnag/js</code> and <code>@bugsnag/plugin-react</code> in a React app.
+        </p>
+        <BadButtons />
+      </div>
+    )
+  }
+}
+
+export default App
