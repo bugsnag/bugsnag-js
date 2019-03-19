1afc55dd05dddec68516550fbed2ef4a731e83ee
Ben Gourley <bn@grly.me>
2019-03-19 14:58:23 +0000

    feat(expo): Load @bugsnag/plugin-react by default on Expo notifier

--- packages/expo/src/notifier.js
+++ packages/expo/src/notifier.js
@@ -1,6 +1,7 @@
 const name = 'Bugsnag Expo'
 const { version } = require('../package.json')
 const url = 'https://github.com/bugsnag/bugsnag-js'
+const React = require('react')
 
 const Client = require('@bugsnag/core/client')
 
@@ -10,6 +11,8 @@ const plugins = [
   require('@bugsnag/plugin-react-native-global-error-handler'),
   require('@bugsnag/plugin-react-native-unhandled-rejection')
 ]
+
+const bugsnagReact = require('@bugsnag/plugin-react')
 
 module.exports = (opts) => {
   // handle very simple use case where user supplies just the api key as a string
@@ -21,6 +24,7 @@ module.exports = (opts) => {
   bugsnag.configure(schema)
 
   plugins.forEach(pl => bugsnag.use(pl))
+  bugsnag.use(bugsnagReact, React)
 
   bugsnag._logger.debug(`Loaded!`)
 
