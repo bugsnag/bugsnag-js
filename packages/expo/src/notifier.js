197510fbc170f04eb6110775aefb3f09e357106c
Ben Gourley <bn@grly.me>
2019-03-19 15:54:01 +0000

    feat(expo): Allow apiKey to be set in app.json rather than in code

--- packages/expo/src/notifier.js
+++ packages/expo/src/notifier.js
@@ -1,6 +1,7 @@
 const name = 'Bugsnag Expo'
 const { version } = require('../package.json')
 const url = 'https://github.com/bugsnag/bugsnag-js'
+const { Constants } = require('expo')
 
 const Client = require('@bugsnag/core/client')
 
@@ -14,6 +15,18 @@ const plugins = [
 module.exports = (opts) => {
   // handle very simple use case where user supplies just the api key as a string
   if (typeof opts === 'string') opts = { apiKey: opts }
+
+  // ensure opts is actually an object (at this point it
+  // could be null, undefined, a number, a boolean etc.)
+  opts = { ...opts }
+
+  // attempt to fetch apiKey from app.json if we didn't get one explicitly passed
+  if (!opts.apiKey &&
+    Constants.manifest.extra &&
+    Constants.manifest.extra.bugsnag &&
+    Constants.manifest.extra.bugsnag.apiKey) {
+    opts.apiKey = Constants.manifest.extra.bugsnag.apiKey
+  }
 
   const bugsnag = new Client({ name, version, url })
 
