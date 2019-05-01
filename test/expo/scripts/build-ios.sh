a831fce8849a9cf308d3e5d9538776ca0bfdf324
Alex Moinet <alex@bugsnag.com>
2019-05-01 13:06:33 +0100

    Expo-ci/iOS: Output to a known location

--- test/expo/scripts/build-ios.sh
+++ test/expo/scripts/build-ios.sh
@@ -4,4 +4,4 @@ npm i turtle-cli
 perl -0777 -i.original -pe "s/entitlements\\['aps-environment'\\] =[^;]+;//gs" node_modules/turtle-cli/node_modules/xdl/build/detach/IosNSBundle.js
 mkdir build
 env
-node_modules/.bin/turtle build:ios -c ./app.json --team-id $APPLE_TEAM_ID --dist-p12-path $EXPO_P12_PATH --provisioning-profile-path $EXPO_PROVISIONING_PROFILE_PATH -o build/output.ipa
+node_modules/.bin/turtle build:ios -c ./app.json --team-id $APPLE_TEAM_ID --dist-p12-path $EXPO_P12_PATH --provisioning-profile-path $EXPO_PROVISIONING_PROFILE_PATH -o $BUILDKITE_BUILD_CHECKOUT_PATH/build/output.ipa
