6f3d6a295fc60bb2804f5621b40767d2b6af2ce8
Ben Gourley <bn@grly.me>
2019-06-03 13:27:32 +0100

    fix(node): Re-add misconfigured proxy test

---
+++ test/node/features/fixtures/proxy/scenarios/misconfigured-proxy.js
@@ -0,0 +1,11 @@
+var bugsnag = require('@bugsnag/node')
+var ProxyAgent = require('http-proxy-agent')
+var bugsnagClient = bugsnag({
+  apiKey: process.env.BUGSNAG_API_KEY,
+  endpoints: {
+    notify: process.env.BUGSNAG_NOTIFY_ENDPOINT,
+    sessions: process.env.BUGSNAG_SESSIONS_ENDPOINT
+  },
+  agent: new ProxyAgent('http://whoops:32228')
+})
+bugsnagClient.notify(new Error('hi via proxy'))
