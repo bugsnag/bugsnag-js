7c75063c8e76635206f1a14d40f72d2e7c1bf205
fractalwrench <fractalwrench@gmail.com>
2019-06-24 16:34:57 +0100

    test: setup instrumentation test target for bugsnag-react-native android code

---
+++ packages/react-native/android/src/androidTest/java/com/reactnative/SanityTest.java
@@ -0,0 +1,23 @@
+package com.bugsnag.android;
+
+import static org.junit.Assert.assertEquals;
+
+import com.bugsnag.reactnative.BugsnagReactNative;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+import org.junit.Test;
+
+public class SanityTest {
+
+    @Test
+    public void testFoo() {
+        ReactApplicationContext ctx
+            = new ReactApplicationContext(InstrumentationRegistry.getContext());
+        assertEquals("BugsnagReactNative", new BugsnagReactNative(ctx).getName());
+    }
+
+}
