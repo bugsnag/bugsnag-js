9829c06cd3372a01a438aa68dfabc22c4c7df780
Ben Gourley <bn@grly.me>
2019-07-08 15:00:38 +0100

    refactor(types): Update type definitions and tests for state/notify refactor

--- packages/core/types/client.d.ts
+++ packages/core/types/client.d.ts
@@ -4,30 +4,37 @@ import Report from "./report";
 import Session from "./session";
 
 declare class Client {
-  public app: object;
-  public device: object;
-  public context: string | void;
-  public config: common.IConfig;
-  public user: object;
-  public metaData: object;
-
   public BugsnagReport: typeof Report;
   public BugsnagBreadcrumb: typeof Breadcrumb;
   public BugsnagSession: typeof Session;
 
+  private config: common.IConfig;
+
+  public set(updates: common.IStateUpdateObj): void;
+  public set(key: string, ...args: any[]): void;
+  public get(key: string, ...nestedKeys: string[]): any;
+  public clear(key: string, ...nestedKeys: string[]): void;
+
   public use(plugin: common.IPlugin, ...args: any[]): Client;
   public getPlugin(name: string): any;
+
   public setOptions(opts: common.IConfig): Client;
   public configure(schema?: common.IConfigSchema): Client;
+
   public delivery(delivery: common.IDelivery): Client;
+
   public logger(logger: common.ILogger): Client;
+
   public sessionDelegate(sessionDelegate: common.ISessionDelegate): Client;
+
   public notify(
     error: common.NotifiableError,
-    opts?: common.INotifyOpts,
+    beforeSend?: common.BeforeSend,
     cb?: (err: any, report: Report) => void,
   ): void;
+
   public leaveBreadcrumb(name: string, metaData?: any, type?: string, timestamp?: string): Client;
+
   public startSession(): Client;
 }
 
