2a31233f8191b28aed13bed56c6fe2ae59c78470
Alex Moinet <alex@bugsnag.com>
2019-07-31 10:35:14 +0100

    Better cache invalidation: Ensure tar archive has a file-based hash

--- test/scripts/create_package.json_tar.sh
+++ test/scripts/create_package.json_tar.sh
@@ -1,18 +1,5 @@
 #!
 
 rm -f min_packages.tar
-mkdir -p min_packages
 
-cd packages
-for dir in */
-do
-  mkdir ../min_packages/$dir
-  cp $dir/package.json $dir/package-lock.json ../min_packages/$dir/
-done
-
-cd ../min_packages
-
-tar -cf ../min_packages.tar *
-
-cd ..
-rm -rf min_packages
+find packages/** -type f \( -name package.json -or -name package-lock.json \) -maxdepth 2 | tar cf - -T - | gzip --no-name > min_packages.tar
