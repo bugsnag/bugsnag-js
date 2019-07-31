87c78b16e888af92858bffb9f5f3c1ca908fad1a
Alex Moinet <alex@bugsnag.com>
2019-07-31 11:32:48 +0100

    Better cache invalidation: Package with preset time

--- test/scripts/create_package.json_tar.sh
+++ test/scripts/create_package.json_tar.sh
@@ -2,4 +2,4 @@
 
 rm -f min_packages.tar
 
-find packages/** -type f \( -name package.json -or -name package-lock.json \) -maxdepth 2 | tar cf - -T - | gzip --no-name > min_packages.tar
+find packages/** -maxdepth 2 -type f \( -name package.json -or -name package-lock.json \) | tar --mtime='1970-01-01' -cf - -T - | gzip --no-name > min_packages.tar
