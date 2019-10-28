273c601b9c9e054a37b8c8781baeae2d56034a7d
Alex Moinet <alex@bugsnag.com>
2019-10-28 17:02:33 +0000

    Tests: Update CI to make iOS build less flakey

--- test/expo/scripts/build-android-locally.sh
+++ test/expo/scripts/build-android-locally.sh
@@ -1,6 +1,8 @@
 set -e
 
 export EXPO_RELEASE_CHANNEL=local
-docker-compose up --build expo-publisher
-docker-compose up --build expo-android-builder
+docker-compose build expo-publisher
+docker-compose run expo-publisher
+docker-compose build expo-android-builder
+docker-compose run expo-android-builder
 docker-compose build expo-maze-runner
