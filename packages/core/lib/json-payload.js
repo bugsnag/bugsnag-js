a28b249d4b439dcc5f3782ba5b913ac37a4d4590
Ben Gourley <bn@grly.me>
2019-12-20 15:39:50 +0000

    refactor(core): Update safe-json-stringify module

--- packages/core/lib/json-payload.js
+++ packages/core/lib/json-payload.js
@@ -1,19 +1,12 @@
 const jsonStringify = require('@bugsnag/safe-json-stringify')
-const EVENT_FILTER_PATHS = [
-  'events.[].app',
+const EVENT_REDACTION_PATHS = [
   'events.[].metaData',
-  'events.[].breadcrumbs',
-  'events.[].request',
-  'events.[].device'
-]
-const SESSION_FILTER_PATHS = [
-  'device',
-  'app',
-  'user'
+  'events.[].breadcrumbs.[].metaData',
+  'events.[].request'
 ]
 
-module.exports.event = (event, filterKeys) => {
-  let payload = jsonStringify(event, null, null, { filterPaths: EVENT_FILTER_PATHS, filterKeys })
+module.exports.event = (event, redactedKeys) => {
+  let payload = jsonStringify(event, null, null, { redactedPaths: EVENT_REDACTION_PATHS, redactedKeys })
   if (payload.length > 10e5) {
     event.events[0]._metadata = {
       notifier:
@@ -21,14 +14,14 @@ module.exports.event = (event, filterKeys) => {
 Serialized payload was ${payload.length / 10e5}MB (limit = 1MB)
 metadata was removed`
     }
-    payload = jsonStringify(event, null, null, { filterPaths: EVENT_FILTER_PATHS, filterKeys })
+    payload = jsonStringify(event, null, null, { redactedPaths: EVENT_REDACTION_PATHS, redactedKeys })
     if (payload.length > 10e5) throw new Error('payload exceeded 1MB limit')
   }
   return payload
 }
 
-module.exports.session = (event, filterKeys) => {
-  const payload = jsonStringify(event, null, null, { filterPaths: SESSION_FILTER_PATHS, filterKeys })
+module.exports.session = (event, redactedKeys) => {
+  const payload = jsonStringify(event, null, null)
   if (payload.length > 10e5) throw new Error('payload exceeded 1MB limit')
   return payload
 }
