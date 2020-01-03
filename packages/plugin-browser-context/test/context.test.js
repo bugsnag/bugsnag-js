f37796dfd68f29db825ceac4e6bf43d5e3761ce0
Ben Gourley <bn@grly.me>
2020-01-03 17:31:37 +0000

    refactor(plugin-browser-context): Ensure auto value is only used when manual is not supplied

--- packages/plugin-browser-context/test/context.test.js
+++ packages/plugin-browser-context/test/context.test.js
@@ -11,29 +11,46 @@ const window = {
 }
 
 describe('plugin: context', () => {
-  it('sets client.context (and event.context) to window.location.pathname', () => {
+  it('sets client.context (and event.context) to window.location.pathname', done => {
     const client = new Client({ apiKey: 'API_KEY_YEAH' })
     const payloads = []
     client.use(plugin, window)
 
-    client._setDelivery(client => ({ sendEvent: (payload) => payloads.push(payload) }))
-    client.notify(new Error('noooo'))
+    client._setDelivery(client => ({
+      sendEvent: (payload, cb) => {
+        payloads.push(payload)
+        cb()
+      }
+    }))
 
-    expect(payloads.length).toEqual(1)
-    expect(payloads[0].events[0].context).toBe(window.location.pathname)
+    client.notify(new Error('noooo'), (event) => {
+      expect(event.context).toBe(window.location.pathname)
+    }, () => {
+      expect(payloads.length).toEqual(1)
+      expect(payloads[0].events[0].context).toBe(window.location.pathname)
+      done()
+    })
   })
 
-  it('sets doesn’t overwrite an existing context', () => {
+  it('sets doesn’t overwrite an existing context', done => {
     const client = new Client({ apiKey: 'API_KEY_YEAH' })
     const payloads = []
     client.use(plugin, window)
 
     client.setContext('something else')
 
-    client._setDelivery(client => ({ sendEvent: (payload) => payloads.push(payload) }))
-    client.notify(new Error('noooo'))
-
-    expect(payloads.length).toEqual(1)
-    expect(payloads[0].events[0].context).toBe('something else')
+    client._setDelivery(client => ({
+      sendEvent: (payload, cb) => {
+        payloads.push(payload)
+        cb()
+      }
+    }))
+    client.notify(new Error('noooo'), (event) => {
+      expect(event.context.toBe('something else'))
+    }, () => {
+      expect(payloads.length).toEqual(1)
+      expect(payloads[0].events[0].context).toBe('something else')
+      done()
+    })
   })
 })
