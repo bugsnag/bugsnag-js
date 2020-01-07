9827455c17b044b9402c09a0789a1f97b33878d9
Ben Gourley <bn@grly.me>
2020-01-07 13:33:46 +0000

    refactor(examples): Update angular app to work with V7 notifier

--- examples/angular/src/app/app.module.ts
+++ examples/angular/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule, ErrorHandler } from '@angular/core';
 // import our Angular plugin
 import { BugsnagErrorHandler } from '@bugsnag/plugin-angular';
 // import the bugsnag-js client you initialized in bugsnag.ts
-import bugsnagClient from './bugsnag';
+import Bugsnag from '@bugsnag/js';
 import { AppComponent } from './app.component';
+
+Bugsnag.init('YOUR_API_KEY');
 
 // There are certain errors within Angular that get caught by its own error handler
 // and only logged to the console. These errors will never make it to Bugsnag by
 // themselves and so require a little wiring up.
 export function errorHandlerFactory() {
-  return new BugsnagErrorHandler(bugsnagClient)
+  return new BugsnagErrorHandler()
 }
 
 @NgModule({
