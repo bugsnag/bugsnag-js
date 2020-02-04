797411140504f61e1df739f73d7e5f1267c0cb6a
Ben Gourley <bn@grly.me>
2020-02-04 16:54:30 +0000

    chore(delivery-x-domain-request): Add comment explaining JSON.parse/stringify madness

--- packages/delivery-x-domain-request/delivery.js
+++ packages/delivery-x-domain-request/delivery.js
@@ -36,6 +36,9 @@ module.exports = (client, win = window) => ({
 })
 
 const getApiUrl = (config, endpoint, version, win) => {
+  // IE8 doesn't support Date.prototype.toISOstring(), but it does convert a date
+  // to an ISO string when you use JSON stringify. Simply parsing the result of
+  // JSON.stringify is smaller than using a toISOstring() polyfill.
   const isoDate = JSON.parse(JSON.stringify(new Date()))
   const url = matchPageProtocol(config.endpoints[endpoint], win.location.protocol)
   return `${url}?apiKey=${encodeURIComponent(config.apiKey)}&payloadVersion=${version}&sentAt=${encodeURIComponent(isoDate)}`
