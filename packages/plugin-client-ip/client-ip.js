3173bc255da1134a823f9a5d4cb8dee2ab2917e9
Ben Gourley <bn@grly.me>
2020-02-12 17:09:42 +0000

    refactor(plugin-client-ip): Use the value [REDACTED] instead of [NOT COLLECTED]

--- packages/plugin-client-ip/client-ip.js
+++ packages/plugin-client-ip/client-ip.js
@@ -11,8 +11,8 @@ module.exports = {
       // If user.id is explicitly undefined, it will be missing from the payload. It needs
       // removing so that the following line replaces it
       if (event._user && typeof event._user.id === 'undefined') delete event._user.id
-      event._user = assign({ id: '[NOT COLLECTED]' }, event._user)
-      event.request = assign({ clientIp: '[NOT COLLECTED]' }, event.request)
+      event._user = assign({ id: '[REDACTED]' }, event._user)
+      event.request = assign({ clientIp: '[REDACTED]' }, event.request)
     })
   },
   configSchema: {
