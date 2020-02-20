af086b08e70dbd70fae00f474c879bb267a37746
fractalwrench <fractalwrench@gmail.com>
2020-02-20 09:16:19 +0000

    address review feedback

--- packages/react-native/android/src/main/java/com/bugsnag/reactnative/AppSerializer.kt
+++ packages/react-native/android/src/main/java/com/bugsnag/reactnative/AppSerializer.kt
@@ -4,15 +4,15 @@ import com.bugsnag.android.AppWithState
 
 internal class AppSerializer : WritableMapSerializer<AppWithState> {
     override fun serialize(map: MutableMap<String, Any?>, app: AppWithState) {
-        map["duration"] = app.duration
-        map["durationInForeground"] = app.durationInForeground
-        map["inForeground"] = app.inForeground
+        map["appType"] = app.type
         map["binaryArch"] = app.binaryArch
         map["buildUuid"] = app.buildUuid
         map["codeBundleId"] = app.codeBundleId
+        map["duration"] = app.duration
+        map["durationInForeground"] = app.durationInForeground
         map["id"] = app.id
+        map["inForeground"] = app.inForeground
         map["releaseStage"] = app.releaseStage
-        map["appType"] = app.type
         map["version"] = app.version
         map["versionCode"] = app.versionCode
     }
--- packages/react-native/android/src/main/java/com/bugsnag/reactnative/MapExtensions.kt
+++ packages/react-native/android/src/main/java/com/bugsnag/reactnative/MapExtensions.kt
@@ -14,7 +14,7 @@ internal fun Map<String, Any?>.toWritableMap(): WritableMap {
             is Boolean -> nativeMap.putBoolean(key, obj)
             is Int -> nativeMap.putInt(key, obj)
             is Long -> nativeMap.putDouble(key, obj.toDouble())
-            is Number -> nativeMap.putDouble(key, obj as Double)
+            is Number -> nativeMap.putDouble(key, obj.toDouble())
             is String -> nativeMap.putString(key, obj)
             is Map<*, *> -> nativeMap.putMap(key, Arguments.makeNativeMap(obj as MutableMap<String, Any>))
             is Collection<*> ->  nativeMap.putArray(key, Arguments.makeNativeArray<Any>(obj.toTypedArray()))
