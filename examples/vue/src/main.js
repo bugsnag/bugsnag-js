04949bd39a45836531fea82c36265bb21f639679
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-03-13 11:13:01 +0000

    chore(examples): update Vue example for new plugin interface

--- examples/vue/src/main.js
+++ examples/vue/src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Bugsnag from '@bugsnag/js'
-import bugsnagVue from '@bugsnag/plugin-vue'
+import BugsnagVuePlugin from '@bugsnag/plugin-vue'
 
-Bugsnag.start('YOUR_API_KEY')
-Bugsnag.use(bugsnagVue, Vue)
+Bugsnag.start({
+  apiKey: 'YOUR_API_KEY',
+  plugins: [new BugsnagVuePlugin(Vue)]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
