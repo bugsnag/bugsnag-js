48a257a0727c804c6b72c48c05d8fd9b84e38a78
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-03-14 22:38:58 +0000

    chore(examples): update Restify example for latest release candidate

--- examples/restify/app.js
+++ examples/restify/app.js
@@ -1,9 +1,12 @@
 const restify = require('restify')
 const Bugsnag = require('@bugsnag/js')
 const { readFileSync } = require('fs')
+const BugsnagRestifyPlugin = require('@bugsnag/plugin-restify')
 
-Bugsnag.start(process.env.BUGSNAG_API_KEY)
-Bugsnag.use(require('@bugsnag/plugin-restify'))
+Bugsnag.start({
+  apiKey: process.env.BUGSNAG_API_KEY,
+  plugins: [BugsnagRestifyPlugin]
+})
 
 const server = restify.createServer()
 const { requestHandler, errorHandler } = Bugsnag.getPlugin('restify')
