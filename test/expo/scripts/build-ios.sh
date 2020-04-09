f63e4857068678d0059d348974d21262f56edd8e
Alex Moinet <alex@bugsnag.com>
2020-04-09 13:27:19 +0100

    Tests: Remove debug & update local script

--- test/expo/scripts/build-ios.sh
+++ test/expo/scripts/build-ios.sh
@@ -8,8 +8,6 @@ cd test/expo/features/fixtures/test-app
 npm i turtle-cli@0.14 bunyan
 perl -0777 -i.original -pe "s/entitlements\\['aps-environment'\\] =[^;]+;//gs" node_modules/\@expo/xdl/build/detach/IosNSBundle.js
 perl -0777 -i.original -pe "s/entitlements\\['aps-environment'\\] =[^;]+;//gs" node_modules/turtle-cli/node_modules/\@expo/xdl/build/detach/IosNSBundle.js
-mkdir -p ~/expo-bundle-files
-cp node_modules/\@expo/xdl/build/detach/IosNSBundle.js ~/expo-bundle-files/$EXPO_RELEASE_CHANNEL.js
 node_modules/.bin/turtle build:ios \
   -c ./app.json \
   --team-id $APPLE_TEAM_ID \
