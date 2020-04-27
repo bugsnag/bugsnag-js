26cd9baad08c1b8e1db8f35fedd1eac5491fd8ba
fractalwrench <fractalwrench@gmail.com>
2020-04-27 15:34:17 +0100

    feat: update React Bridge method signatures for Cocoa

--- packages/react-native/ios/BugsnagReactNative/BugsnagReactNative.m
+++ packages/react-native/ios/BugsnagReactNative/BugsnagReactNative.m
@@ -12,21 +12,27 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(configure) {
-  if (![Bugsnag bugsnagStarted]) {
-    // TODO: fail loudly here
-    return nil;
-  }
+RCT_EXPORT_METHOD(configureAsync:(NSDictionary *)readableMap
+                         resolve:(RCTPromiseResolveBlock)resolve
+                          reject:(RCTPromiseRejectBlock)reject) {
+    resolve([self configure:readableMap]);
+}
 
-  // TODO: use this emitter to notifier JS of changes to user, context and metadata
-  BugsnagReactNativeEmitter *emitter = [BugsnagReactNativeEmitter new];
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(configure:(NSDictionary *)readableMap) {
+    if (![Bugsnag bugsnagStarted]) {
+        // TODO: fail loudly here
+        return nil;
+    }
 
-  // TODO: convert the entire config into a map
-  BugsnagConfiguration *config = [Bugsnag configuration];
-  return @{
-    @"apiKey": [config apiKey],
-    @"releaseStage": [config releaseStage],
-  };
+    // TODO: use this emitter to inform JS of changes to user, context and metadata
+    BugsnagReactNativeEmitter *emitter = [BugsnagReactNativeEmitter new];
+
+    // TODO: convert the entire config into a map
+    BugsnagConfiguration *config = [Bugsnag configuration];
+    return @{
+        @"apiKey": [config apiKey],
+        @"releaseStage": [config releaseStage],
+    };
 }
 
 RCT_EXPORT_METHOD(updateMetadata
@@ -35,39 +41,46 @@ RCT_EXPORT_METHOD(updateMetadata
   //TODO
 }
 
-RCT_EXPORT_METHOD(updateContext
-                  :(NSString *)context) {
+RCT_EXPORT_METHOD(updateContext:(NSString *)context) {
     [Bugsnag setContext:context];
 }
 
-RCT_EXPORT_METHOD(updateUser
-                  :(NSString *)userId
-         withEmail:(NSString *)email
-          withName:(NSString *)name) {
+RCT_EXPORT_METHOD(updateCodeBundleId:(NSString *)codeBundleId) {
+    // TODO
+}
+
+RCT_EXPORT_METHOD(updateUser:(NSString *)userId
+                   withEmail:(NSString *)email
+                    withName:(NSString *)name) {
     [Bugsnag setUser:userId withEmail:email andName:name];
 }
 
-RCT_EXPORT_METHOD(dispatch
-                  :(NSDictionary *)payload
-           resolve:(RCTPromiseResolveBlock)resolve
-            reject:(RCTPromiseRejectBlock)reject) {
-  resolve(@{});
+RCT_EXPORT_METHOD(dispatch:(NSDictionary *)payload
+                   resolve:(RCTPromiseResolveBlock)resolve
+                    reject:(RCTPromiseRejectBlock)reject) {
+    resolve(@{});
 }
 
-RCT_EXPORT_METHOD(leaveBreadcrumb
-                  :(NSDictionary *)options) {
+RCT_EXPORT_METHOD(leaveBreadcrumb:(NSDictionary *)options) {
   //TODO
 }
 
-RCT_EXPORT_METHOD(startSession) { [Bugsnag startSession]; }
-RCT_EXPORT_METHOD(pauseSession) { [Bugsnag pauseSession]; }
-RCT_EXPORT_METHOD(resumeSession) { [Bugsnag resumeSession]; }
+RCT_EXPORT_METHOD(startSession) {
+    [Bugsnag startSession];
+}
 
-RCT_EXPORT_METHOD(getPayloadInfo
-                  :(NSDictionary *)options
-           resolve:(RCTPromiseResolveBlock)resolve
-            reject:(RCTPromiseRejectBlock)reject) {
-  resolve(@{});
+RCT_EXPORT_METHOD(pauseSession) {
+    [Bugsnag pauseSession];
+}
+
+RCT_EXPORT_METHOD(resumeSession) {
+    [Bugsnag resumeSession];
+}
+
+RCT_EXPORT_METHOD(getPayloadInfo:(NSDictionary *)options
+                         resolve:(RCTPromiseResolveBlock)resolve
+                          reject:(RCTPromiseRejectBlock)reject) {
+    resolve(@{});
 }
 
 @end
