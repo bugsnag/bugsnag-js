b3beccb9e767147ed727eefe39b60bf5eadb86d3
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-05-08 23:00:21 +0100

    Actually install

--- scripts/publish.js
+++ scripts/publish.js
@@ -5,7 +5,7 @@ function publish(publishUrl) {
   let version = common.determineVersion()
   let distTag = common.getCommitId()
   console.log(`Publishing as: ${version}`)
-  console.log(`./node_modules/.bin/lerna publish ${version} --dist-tag ${distTag} --yes --force-publish --no-push --no-git-tag-version --registry ${publishUrl}`)
+  common.run(`./node_modules/.bin/lerna publish ${version} --dist-tag ${distTag} --yes --force-publish --no-push --no-git-tag-version --registry ${publishUrl}`)
 }
 
 if (process.argv.length !== 3) {
