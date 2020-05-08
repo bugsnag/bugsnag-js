c3cf93d52d029fcac113ef6b5fa20ed93067c9b9
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-05-08 22:46:44 +0100

    Updated approach

--- scripts/common.js
+++ scripts/common.js
@@ -4,24 +4,25 @@ module.exports = {
     run: function run(command) {
         return execSync(command).toString().trim()
     },
-
-    determineBaseVersion: function determineBaseVersion() {
-        /*
-         * Form version string: Include branch name and commit id to avoid most clashes.  Also include the year
-         * and month in the version to publish to make it easier to clear out our private NPM repository occasionally.
-         */
-        let commitId = this.run('git rev-parse --short HEAD')
+    getCommitId: function getCommitId() {
+        return this.run('git rev-parse --short HEAD');
+    },
+    determineVersion: function determineVersion() {
+        // Form version string - branch name and commit id
+        let commitId = this.getCommitId();
         let branchName;
         if (process.env.BRANCH_NAME) {
             console.log('Using BRANCH_NAME from the environment')
             branchName = process.env.BRANCH_NAME
         } else {
             console.log('Using local Git repo to determine branch name (environment BRANCH_NAME may also be used)')
-            branchName = this.run('git branch --show-current')
+            branchName = this.run('git branch --show-current').replace(/\//g,"-")
         }
 
-        let d = new Date()
-        let month = d.getFullYear().toString().substr(-2) + '-' + (d.getMonth() + 1).toString().padStart(2, '0')
-        return `0.0.0-ci-${month}-${branchName}-${commitId}`
+        // Get the current version from lerna.json
+        const lerna = require("../lerna.json");
+        let lernaVersion = lerna.version.match(/^[1-9][0-9]*\.[0-9]+\.[0-9]+/)[0];
+
+        return `${lernaVersion}-${branchName}.${commitId}`
     }
 }
--- scripts/publish.js
+++ scripts/publish.js
@@ -2,27 +2,10 @@ const common = require('./common')
 
 function publish(publishUrl) {
 
-  let versionBase = common.determineBaseVersion()
-
-  // Find any existing pre-releases for the branch/commit combination - to be sure we never collide.
-  let versions = JSON.parse(common.run(`npm view @bugsnag/js versions --registry ${publishUrl} --json`))
-  console.log(`@bugsnag/js has a total of ${versions.length} versions in NPM`)
-
-  versions = versions.filter(function (str) { return str.indexOf(versionBase) !== -1; })
-  console.log(`Of those, ${versions.length} match our version base: ${versionBase}`)
-
-  // Find the highest existing pre-release identifier
-  let preId = -1
-  versions.forEach(function(version) {
-    let id = parseInt(version.toString().substring(versionBase.length + 1))
-    if (id > preId) preId = id
-  })
-
-  // Finally, publish
-  preId += 1
-  let fullVersion = `${versionBase}.${preId}`
-  console.log(`Publishing as: ${fullVersion}`)
-  console.log(`./node_modules/.bin/lerna publish ${fullVersion} --yes --force-publish --no-push --no-git-tag-version --registry ${publishUrl}`)
+  let version = common.determineVersion()
+  let distTag = common.getCommitId()
+  console.log(`Publishing as: ${version}`)
+  console.log(`./node_modules/.bin/lerna publish ${version} --dist-tag ${distTag} --yes --force-publish --no-push --no-git-tag-version --registry ${publishUrl}`)
 }
 
 if (process.argv.length !== 3) {
