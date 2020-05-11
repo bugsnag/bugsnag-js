93428927df6901ca73a9002268b415ca6394d18d
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-05-11 15:14:27 +0100

    chore(linter): scripts directory added to Linter image and violations corrected

--- scripts/common.js
+++ scripts/common.js
@@ -1,31 +1,31 @@
-const lerna = require("../lerna.json");
-const { execSync } = require("child_process");
+const lerna = require('../lerna.json')
+const { execSync } = require('child_process')
 
 module.exports = {
-    run: function run(command) {
-        return execSync(command).toString().trim()
-    },
-    getCommitId: function getCommitId() {
-        return this.run('git rev-parse --short HEAD');
-    },
-    determineVersion: function determineVersion() {
-        // Form version string - branch name and commit id
-        let commitId = this.getCommitId();
-        let branchName;
-        if (process.env.BRANCH_NAME) {
-            console.log('Using BRANCH_NAME from the environment')
-            branchName = process.env.BRANCH_NAME
-        } else {
-            console.log('Using local Git repo to determine branch name (environment BRANCH_NAME may also be used)')
-            branchName = this.run('git rev-parse --abbrev-ref HEAD').replace(/\//g,"-")
-        }
-
-        // Distinguish from local use
-        let ciIndicator = (process.env.BUILDKITE ? "ci-" : "")
-
-        // Get the current version from lerna.json
-        let lernaVersion = lerna.version.match(/^[1-9][0-9]*\.[0-9]+\.[0-9]+/)[0];
-
-        return `${lernaVersion}-${ciIndicator}${branchName}.${commitId}`
+  run: function run (command) {
+    return execSync(command).toString().trim()
+  },
+  getCommitId: function getCommitId () {
+    return this.run('git rev-parse --short HEAD')
+  },
+  determineVersion: function determineVersion () {
+    // Form version string - branch name and commit id
+    const commitId = this.getCommitId()
+    let branchName
+    if (process.env.BRANCH_NAME) {
+      console.log('Using BRANCH_NAME from the environment')
+      branchName = process.env.BRANCH_NAME
+    } else {
+      console.log('Using local Git repo to determine branch name (environment BRANCH_NAME may also be used)')
+      branchName = this.run('git rev-parse --abbrev-ref HEAD').replace(/\//g, '-')
     }
+
+    // Distinguish from local use
+    const ciIndicator = (process.env.BUILDKITE ? 'ci-' : '')
+
+    // Get the current version from lerna.json
+    const lernaVersion = lerna.version.match(/^[1-9][0-9]*\.[0-9]+\.[0-9]+/)[0]
+
+    return `${lernaVersion}-${ciIndicator}${branchName}.${commitId}`
+  }
 }
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,15 +1,14 @@
 const common = require('./common')
 
-function publish(publishUrl) {
-
-  let version = common.determineVersion()
-  let distTag = common.getCommitId()
+function publish (publishUrl) {
+  const version = common.determineVersion()
+  const distTag = common.getCommitId()
   console.log(`Publishing as: ${version}`)
   common.run(`./node_modules/.bin/lerna publish ${version} --dist-tag ${distTag} --yes --force-publish --no-push --no-git-tag-version --registry ${publishUrl}`)
 }
 
 if (process.argv.length !== 3) {
-  console.error("Usage: publish.js <registry url>")
+  console.error('Usage: publish.js <registry url>')
   process.exit(1)
 }
 publish(process.argv[2])
