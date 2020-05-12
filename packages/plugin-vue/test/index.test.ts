112dc8a6dbdd6caad037988217a275035b755d57
Ben Gourley <bn@grly.me>
2020-05-12 16:15:05 +0100

    refactor(plugin-vue): Support deferred passing of framework reference

--- packages/plugin-vue/test/index.test.ts
+++ packages/plugin-vue/test/index.test.ts
@@ -5,12 +5,29 @@ import Vue from 'vue'
 describe('bugsnag vue', () => {
   it('throws when missing Vue', () => {
     expect(() => {
-      new BugsnagVuePlugin().load(new Client({ apiKey: 'API_KEYYY' }))
+      new BugsnagVuePlugin(undefined).load(new Client({ apiKey: 'API_KEYYY' }))
     }).toThrow()
   })
 
   it('installs Vue.config.errorHandler', done => {
     const client = new Client({ apiKey: 'API_KEYYY', plugins: [new BugsnagVuePlugin(Vue)] })
+    client._setDelivery(client => ({
+      sendEvent: (payload) => {
+        expect(payload.events[0].errors[0].errorClass).toBe('Error')
+        expect(payload.events[0].errors[0].errorMessage).toBe('oops')
+        expect(payload.events[0]._metadata.vue).toBeDefined()
+        done()
+      },
+      sendSession: () => {}
+    }))
+    expect(typeof Vue.config.errorHandler).toBe('function')
+    Vue.config.errorHandler(new Error('oops'), { $root: true, $options: {} } as unknown as Vue, 'callback for watcher "fooBarBaz"')
+  })
+
+  it('supports Vue being passed later', done => {
+    const client = new Client({ apiKey: 'API_KEYYY', plugins: [new BugsnagVuePlugin()] })
+    // eslint-disable-next-line
+    client.getPlugin('vue')!.installVueErrorHandler(Vue)
     client._setDelivery(client => ({
       sendEvent: (payload) => {
         expect(payload.events[0].errors[0].errorClass).toBe('Error')
