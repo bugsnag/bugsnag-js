f2b8f4a43c3142c8b38c2340d3c819fd45905191
Ben Gourley <bn@grly.me>
2020-05-14 11:23:06 +0100

    feat(plugin-express): Add complete type definitions

--- packages/plugin-express/types/bugsnag-express.d.ts
+++ packages/plugin-express/types/bugsnag-express.d.ts
@@ -1,3 +1,28 @@
-import { Plugin } from '@bugsnag/node'
+import { Plugin, Client } from '@bugsnag/core'
+import express from 'express'
+
 declare const bugsnagPluginExpress: Plugin
 export default bugsnagPluginExpress
+
+interface BugsnagPluginExpressResult {
+  errorHandler: express.ErrorRequestHandler
+  requestHandler: express.RequestHandler
+}
+
+// add a new call signature for the getPlugin() method that types the react plugin result
+declare module '@bugsnag/core' {
+  interface Client {
+    getPlugin(id: 'express'): BugsnagPluginExpressResult | undefined
+  }
+}
+
+// define req.bugsnag for express request handlers by declaratin merging on the
+// global interfaces according to the pattern described in the DefinitelyTyped repo:
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/0bd28530564c3da2e728518084f22648af3a683c/types/express-serve-static-core/index.d.ts#L18-L26
+declare global {
+  namespace Express {
+    export interface Request {
+      bugsnag?: Client
+    }
+  }
+}
