3575f719f7c8be75d259df3a4587e1c6e1c09742
Joe Haines <hello@joehaines.co.uk>
2020-06-01 18:03:03 +0100

    feat(plugin-browser-device) add orientation field

--- packages/plugin-browser-device/device.js
+++ packages/plugin-browser-device/device.js
@@ -3,11 +3,15 @@ const assign = require('@bugsnag/core/lib/es-utils/assign')
 /*
  * Automatically detects browser device details
  */
-module.exports = (nav = navigator) => ({
+module.exports = (nav = navigator, _screen = window.screen) => ({
   load: (client) => {
     const device = {
       locale: nav.browserLanguage || nav.systemLanguage || nav.userLanguage || nav.language,
       userAgent: nav.userAgent
+    }
+
+    if (_screen && _screen.orientation && _screen.orientation.type) {
+      device.orientation = _screen.orientation.type
     }
 
     client.addOnSession(session => {
--- packages/plugin-browser-device/test/device.test.ts
+++ packages/plugin-browser-device/test/device.test.ts
@@ -3,6 +3,7 @@ import plugin from '../device'
 import Client, { SessionDeliveryPayload, EventDeliveryPayload } from '@bugsnag/core/client'
 
 const navigator = { language: 'en-GB', userAgent: 'testing browser 1.2.3' } as unknown as Navigator
+const screen = { orientation: { type: 'landscape-primary' } } as unknown as Screen
 
 describe('plugin: device', () => {
   it('should add an onError callback which captures device information', () => {
@@ -19,6 +20,24 @@ describe('plugin: device', () => {
     expect(payloads[0].events[0].device.time instanceof Date).toBe(true)
     expect(payloads[0].events[0].device.locale).toBe(navigator.language)
     expect(payloads[0].events[0].device.userAgent).toBe(navigator.userAgent)
+    expect(payloads[0].events[0].device.orientation).toBe(undefined)
+  })
+
+  it('should capture the screen orientation if possible and add it to the event', () => {
+    const client = new Client({ apiKey: 'API_KEY_YEAH', plugins: [plugin(navigator, screen)] })
+    const payloads: EventDeliveryPayload[] = []
+
+    expect(client._cbs.e.length).toBe(1)
+
+    client._setDelivery(client => ({ sendEvent: (payload) => payloads.push(payload), sendSession: () => {} }))
+    client.notify(new Error('noooo'))
+
+    expect(payloads.length).toEqual(1)
+    expect(payloads[0].events[0].device).toBeDefined()
+    expect(payloads[0].events[0].device.time instanceof Date).toBe(true)
+    expect(payloads[0].events[0].device.locale).toBe(navigator.language)
+    expect(payloads[0].events[0].device.userAgent).toBe(navigator.userAgent)
+    expect(payloads[0].events[0].device.orientation).toBe('landscape-primary')
   })
 
   it('should add an onSession callback which captures device information', () => {
@@ -39,5 +58,27 @@ describe('plugin: device', () => {
     expect(payloads[0].device).toBeDefined()
     expect(payloads[0].device && payloads[0].device.locale).toBe(navigator.language)
     expect(payloads[0].device && payloads[0].device.userAgent).toBe(navigator.userAgent)
+    expect(payloads[0].device && payloads[0].device.orientation).toBe(undefined)
+  })
+
+  it('should capture the screen orientation if possible and add it to the session', () => {
+    const client = new Client({ apiKey: 'API_KEY_YEAH', plugins: [plugin(navigator, screen)] })
+    const payloads: SessionDeliveryPayload[] = []
+    client._sessionDelegate = {
+      startSession: (client, session) => {
+        client._delivery.sendSession(session, () => {})
+      }
+    }
+
+    expect(client._cbs.s.length).toBe(1)
+
+    client._setDelivery(client => ({ sendEvent: () => {}, sendSession: (payload) => payloads.push(payload) }))
+    client.startSession()
+
+    expect(payloads.length).toEqual(1)
+    expect(payloads[0].device).toBeDefined()
+    expect(payloads[0].device && payloads[0].device.locale).toBe(navigator.language)
+    expect(payloads[0].device && payloads[0].device.userAgent).toBe(navigator.userAgent)
+    expect(payloads[0].device && payloads[0].device.orientation).toBe('landscape-primary')
   })
 })
