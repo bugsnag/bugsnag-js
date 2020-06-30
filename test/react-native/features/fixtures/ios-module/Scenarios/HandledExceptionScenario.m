ee4909f876d6e80d11e7a31fbe4fc68519da93a0
Alex Moinet <alex@bugsnag.com>
2020-06-30 12:11:34 +0100

    V7 react-native: Add handledException ios scenario

---
+++ test/react-native/features/fixtures/ios-module/Scenarios/HandledExceptionScenario.m
@@ -0,0 +1,16 @@
+import "BreadcrumbCallbackRemovalScenario.h"
+
+@implementation HandledExceptionScenario
+
+- (void)run {
+    @try {
+        // Code that can potentially throw an Exception:
+        NSDictionary *actuallyReallyJSON = nil;
+        [NSJSONSerialization dataWithJSONObject:actuallyReallyJSON options:0 error:nil];
+    }
+    @catch (NSException *exception) {
+        [Bugsnag notify:exception];
+    }
+}
+
+@end
