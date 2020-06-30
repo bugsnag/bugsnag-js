ed11f3106eb6cdb0ef3ba2391ea1ddfbcc3568c1
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-06-30 13:51:08 +0100

    ci(publish): detect existing published packages

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,8 +3,16 @@ const common = require('./common')
 function publish (publishUrl) {
   const version = common.determineVersion()
   const distTag = common.getCommitId()
-  console.log(`Publishing as: ${version}`)
-  common.run(`./node_modules/.bin/lerna publish ${version} --dist-tag ${distTag} --yes --force-publish --no-push --no-git-tag-version --registry ${publishUrl}`)
+
+  // Check for existing published packages
+  const allVersions = JSON.parse(common.run(`npm view @bugsnag/js versions --registry ${publishUrl} --json`).toString())
+  const myVersions = allVersions.filter(function (str) { return str.indexOf(version) !== -1; })
+  if (myVersions.length === 0) {
+    console.log(`Publishing as '${version}'`)
+    common.run(`./node_modules/.bin/lerna publish ${version} --dist-tag ${distTag} --yes --force-publish --no-push --no-git-tag-version --registry ${publishUrl}`)
+  } else {
+    console.log(`Version '${version}' already found in registry - skipping publishing`)
+  }
 }
 
 if (process.argv.length !== 3) {
