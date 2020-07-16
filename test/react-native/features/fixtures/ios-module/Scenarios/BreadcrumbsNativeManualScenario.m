f884f5b03c8683ffde3cc029f3245b587b24f265
Alex Moinet <alex@bugsnag.com>
2020-07-16 20:11:30 +0100

    V7 RN: Update Breadcrumb scenario for cocoa

--- test/react-native/features/fixtures/ios-module/Scenarios/BreadcrumbsNativeManualScenario.m
+++ test/react-native/features/fixtures/ios-module/Scenarios/BreadcrumbsNativeManualScenario.m
@@ -3,8 +3,8 @@
 @implementation BreadcrumbsNativeManualScenario
 
 - (void)run {
-  [Bugsnag leaveBreadcrumb(@"oh native crumbs")]
-  NSException * exception = [[NSException alloc] initWithName:@"NSException" reason:@"BreadcrumbsNativeManualScenario" userInfo:nil];
+  [Bugsnag leaveBreadcrumbWithMessage:@"oh native crumbs"];
+  NSException *exception = [[NSException alloc] initWithName:@"NSException" reason:@"BreadcrumbsNativeManualScenario" userInfo:nil];
   [Bugsnag notify:exception];
 }
 
