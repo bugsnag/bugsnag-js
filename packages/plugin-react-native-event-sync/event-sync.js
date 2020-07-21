5ff76ba3c9262598f0630b85ed8a009f8e5e4a14
Ben Gourley <bn@grly.me>
2020-07-21 09:51:42 +0100

    feat(react-native): Add native device/app metadata to event

--- packages/plugin-react-native-event-sync/event-sync.js
+++ packages/plugin-react-native-event-sync/event-sync.js
@@ -5,13 +5,17 @@ module.exports = (NativeClient) => ({
         threads,
         breadcrumbs,
         app,
-        device
+        device,
+        deviceMetadata,
+        appMetadata
       } = await NativeClient.getPayloadInfo({ unhandled: event.unhandled })
 
       event.breadcrumbs = breadcrumbs
       event.app = { ...event.app, ...app }
       event.device = { ...event.device, ...device }
       event.threads = threads
+      event.addMetadata('device', deviceMetadata)
+      event.addMetadata('app', appMetadata)
     }, true)
   }
 })
