33b1a0851acae8ad97718b4234f8e2be5e46d11e
Alex Moinet <alex@bugsnag.com>
2020-07-24 14:17:57 +0100

    V7 RN: Make sure UserJSNativeScenario is synchronous

--- test/react-native/features/fixtures/app/scenarios/UserJsNativeScenario.js
+++ test/react-native/features/fixtures/app/scenarios/UserJsNativeScenario.js
@@ -4,8 +4,6 @@ import { NativeModules } from 'react-native'
 export class UserJsNativeScenario extends Scenario {
   run() {
     Bugsnag.setUser('123', 'bug@sn.ag', 'Bug Snag')
-    setTimeout(() => {
-      NativeModules.BugsnagTestInterface.runScenario('UnhandledNativeErrorScenario', () => {})
-    }, 500)
+    NativeModules.BugsnagTestInterface.runScenario('UnhandledNativeErrorScenario', () => {})
   }
 }
