a03c46868c3218b0dc958aa9bdb252565835778b
Jamie Lynch <fractalwrench@gmail.com>
2020-08-04 16:07:31 +0100

    Apply suggestions from code review
    Co-authored-by: Ben Gourley <bn@grly.me>

--- test/react-native/features/fixtures/app/scenarios/EventApiKeyOverrideScenario.js
+++ test/react-native/features/fixtures/app/scenarios/EventApiKeyOverrideScenario.js
@@ -2,13 +2,8 @@ import Scenario from './Scenario'
 import Bugsnag from '@bugsnag/react-native'
 
 export class EventApiKeyOverrideScenario extends Scenario {
-  constructor(configuration, extraData, jsConfig) {
-    super()
-  }
-
   run() {
     Bugsnag.notify(new Error('EventApiKeyOverrideScenario'), event => {
-      event.setUser('123', 'bug@sn.ag', 'Bug Snag')
       event.apiKey = 'abf0deabf0deabf0deabf0deabf0de12'
     })
   }
