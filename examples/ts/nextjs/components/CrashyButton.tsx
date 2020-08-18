88c9e11580020e358f6d870eafbe4bc4a4a50d0b
Dan Skinner <dan@bugsnag.com>
2020-08-18 15:18:32 +0100

    remove custom express server

--- examples/ts/nextjs/components/CrashyButton.tsx
+++ examples/ts/nextjs/components/CrashyButton.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { useState } from 'react'
 
 export function CrashyButton(props: { children: string }) {
-  const [badState, setBadState ] = useState(false);
+  const [badState, setBadState] = useState(false);
 
   const doSomethingBad = React.useCallback(() => {
     setBadState(true)
