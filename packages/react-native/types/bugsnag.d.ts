29b8810747157da3ad751c69a2bad38087760949
Ben Gourley <bn@grly.me>
2020-08-20 16:41:32 +0100

    fix(react-native): Add codeBundleId to config type definitions

--- packages/react-native/types/bugsnag.d.ts
+++ packages/react-native/types/bugsnag.d.ts
@@ -1,9 +1,13 @@
 import { Client, Config } from '@bugsnag/core'
 
-// these properties are allowed to be configured in the JS layer
-type Configurable = 'onError' | 'onBreadcrumb' | 'logger' | 'metadata' | 'user' | 'context' | 'plugins'
+interface ReactNativeSchema extends Config {
+  codeBundleId?: string
+}
 
-type ReactNativeConfig = Pick<Config, Configurable>
+// these properties are allowed to be configured in the JS layer
+type Configurable = 'onError' | 'onBreadcrumb' | 'logger' | 'metadata' | 'user' | 'context' | 'plugins' | 'codeBundleId'
+
+type ReactNativeConfig = Pick<ReactNativeSchema, Configurable>
 
 interface ReactNativeBugsnagStatic extends Client {
   start(jsOpts?: ReactNativeConfig): Client
