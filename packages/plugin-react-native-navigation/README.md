23b240eef8c36afd5044599f0e787f9961eaa603
Joe Haines <hello@joehaines.co.uk>
2020-09-22 17:45:27 +0100

    feat(plugin-react-native-navigation): Leave breadcrumbs when navigating

--- packages/plugin-react-native-navigation/README.md
+++ packages/plugin-react-native-navigation/README.md
@@ -1,6 +1,6 @@
 # @bugsnag/plugin-react-native-navigation
 
-This plugin hooks in to React Native Navigation, updating the `context` with the current component name.
+This plugin hooks in to React Native Navigation, updating the `context` with the current component name and leaving breadcrumbs for navigation changes.
 
 ## License
 
--- packages/plugin-react-native-navigation/react-native-navigation.js
+++ packages/plugin-react-native-navigation/react-native-navigation.js
@@ -4,8 +4,20 @@ module.exports = class BugsnagPluginReactNativeNavigation {
   }
 
   load (client) {
+    let lastComponent
+
     this.Navigation.events().registerComponentDidAppearListener(event => {
       client.setContext(event.componentName)
+
+      if (lastComponent !== event.componentName) {
+        client.leaveBreadcrumb(
+          'React Native Navigation componentDidAppear',
+          { to: event.componentName, from: lastComponent },
+          'navigation'
+        )
+      }
+
+      lastComponent = event.componentName
     })
   }
 }
