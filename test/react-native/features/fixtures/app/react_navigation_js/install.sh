9d91421a6ecdafca3e14ba68f6be14c1c09d0381
Alex Moinet <alex@bugsnag.com>
2020-09-28 11:55:04 +0100

    Tests/RN-nav: Move React-navigation JS to app folder

---
+++ test/react-native/features/fixtures/app/react_navigation_js/install.sh
@@ -0,0 +1,7 @@
+npm i @react-native-community/masked-view --registry=$REGISTRY_URL
+npm i @react-navigation/native --registry=$REGISTRY_URL
+npm i @react-navigation/stack --registry=$REGISTRY_URL
+npm i react-native-gesture-handler --registry=$REGISTRY_URL
+npm i react-native-reanimated --registry=$REGISTRY_URL
+npm i react-native-safe-area-context --registry=$REGISTRY_URL
+npm i react-native-screens --registry=$REGISTRY_URL
