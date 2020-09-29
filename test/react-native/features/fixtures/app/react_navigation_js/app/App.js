428277fd901cf6ad9895ae6fb6a80d0f2da0c507
Alex Moinet <alex@bugsnag.com>
2020-09-29 13:16:46 +0100

    Tests/RN-nav: Update react-navigation App class to work with maze-runner

--- test/react-native/features/fixtures/app/react_navigation_js/app/App.js
+++ test/react-native/features/fixtures/app/react_navigation_js/app/App.js
@@ -1,19 +1,9 @@
-import * as React from 'react';
+import React, {Component} from 'react';
 import {View, Text, Button, NativeModules} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Bugsnag from '@bugsnag/react-native';
 import BugsnagReactNavigation from '@bugsnag/plugin-react-navigation';
-
-NativeModules.BugsnagTestInterface.startBugsnag({
-  apiKey: '12312312312312312312312312312312',
-  endpoint: 'http://bs-local.com:9339',
-  autoTrackSessions: false
-}, () => {
-  Bugsnag.start({
-    plugins: [new BugsnagReactNavigation()],
-  })
-})
 
 function HomeScreen({navigation}) {
   return (
@@ -38,7 +28,7 @@ function DetailsScreen({navigation}) {
       <Text>Details Screen</Text>
       <Button title='Navigate'
         accessibilityLabel='navigate'
-        onPress={() => navigation.navigate('Details')} />
+        onPress={() => navigation.navigate('Home')} />
       <Button title='Notify handled error'
         accessibilityLabel='sendHandled'
         onPress={() => Bugsnag.notify(new Error('DetailsNavigationError'))} />
@@ -49,27 +39,53 @@ function DetailsScreen({navigation}) {
   );
 }
 
-const Stack = createStackNavigator();
 
-const BugsnagNavigationContainer = Bugsnag.getPlugin('reactNavigation').createNavigationContainer(NavigationContainer);
 
-function App() {
-  return (
-    <BugsnagNavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'HomeScreen'}}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{title: 'DetailsScreen'}}
-        />
-      </Stack.Navigator>
-    </BugsnagNavigationContainer>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      showNav: false
+    }
+    NativeModules.BugsnagTestInterface.startBugsnag({
+      apiKey: '12312312312312312312312312312312',
+      endpoint: 'http://bs-local.com:9339',
+      autoTrackSessions: false
+    }, () => {
+      Bugsnag.start({
+        plugins: [new BugsnagReactNavigation()],
+      })
+      console.log("Started")
+      this.setState(() => ({showNav: true}))
+    })
+  }
+
+  render () {
+    if (this.state.showNav) {
+      const Stack = createStackNavigator();
+      const BugsnagNavigationContainer = Bugsnag.getPlugin('reactNavigation').createNavigationContainer(NavigationContainer);
+      return (
+        <BugsnagNavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{title: 'HomeScreen'}}
+            />
+            <Stack.Screen
+              name="Details"
+              component={DetailsScreen}
+              options={{title: 'DetailsScreen'}}
+            />
+          </Stack.Navigator>
+        </BugsnagNavigationContainer>
+      );
+    } else {
+      return (
+        <Text>Waiting</Text>
+      )
+    }
+  }
 }
 
-export default App;
