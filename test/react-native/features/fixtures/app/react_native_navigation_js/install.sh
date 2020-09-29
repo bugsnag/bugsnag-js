6c4725c30b18c10144de0ea9b23afb1b381d4514
Alex Moinet <alex@bugsnag.com>
2020-09-30 00:01:02 +0100

    Tests/RN-native-nav: Update iOS test fixtures to iOS 11.0 minimum target

--- test/react-native/features/fixtures/app/react_native_navigation_js/install.sh
+++ test/react-native/features/fixtures/app/react_native_navigation_js/install.sh
@@ -1,4 +1,2 @@
 npm i react-native-navigation@^7.0.0
 npx rnn-link
-
-find ./ios/reactnative.xcodeproj/project.pbxproj -type f -exec sed -i '' -e "s/IPHONEOS_DEPLOYMENT_TARGET = [^ ;]*;/IPHONEOS_DEPLOYMENT_TARGET = 11.0;/g" {} \;
