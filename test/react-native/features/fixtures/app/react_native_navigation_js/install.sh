00e01bdbabfd147a45af01ed010772dfc46e59d6
Alex Moinet <alex@bugsnag.com>
2020-09-29 21:00:19 +0100

    Tests/RN-native-nav: Update required substitutions/build dependencies

--- test/react-native/features/fixtures/app/react_native_navigation_js/install.sh
+++ test/react-native/features/fixtures/app/react_native_navigation_js/install.sh
@@ -1,4 +1,4 @@
 npm i react-native-navigation@^7.0.0
 npx rnn-link
 
-sed -i "s/IPHONEOS_DEPLOYMENT_TARGET = 9.3/IPHONEOS_DEPLOYMENT_TARGET = 11.0/g" ./ios/reactnative.xcodeproj/project.pbxproj
+find ./ios/reactnative.xcodeproj/project.pbxproj -type f -exec sed -i '' -e "s/IPHONEOS_DEPLOYMENT_TARGET = [^ ;]*;/IPHONEOS_DEPLOYMENT_TARGET = 11.0;/g" {} \;
