858a08490694ef7f5d771527a02f5e779395795e
Alex Moinet <alex@bugsnag.com>
2020-09-30 14:35:22 +0100

    Tests/RN-native-nav: Fix rendering issues in react-native-navigation

--- test/react-native/features/fixtures/app/react_native_navigation_js/index.js
+++ test/react-native/features/fixtures/app/react_native_navigation_js/index.js
@@ -6,36 +6,20 @@ import {Navigation} from 'react-native-navigation';
 import HomeScreen from './screens/Home';
 import DetailsScreen from './screens/Details';
 
-import {NativeModules} from 'react-native'
-
-import Bugsnag from '@bugsnag/react-native';
-import BugsnagReactNativeNavigation from '@bugsnag/plugin-react-native-navigation';
-
-NativeModules.BugsnagTestInterface.startBugsnag({
-  apiKey: '12312312312312312312312312312312',
-  endpoint: 'http://bs-local.com:9339',
-  autoTrackSessions: false
-}, () => {
-  Bugsnag.start({
-    plugins: [new BugsnagReactNativeNavigation()],
-  })
-  Navigation.registerComponent('Home', () => HomeScreen);
-  Navigation.registerComponent('Details', () => DetailsScreen);
-  Navigation.events().registerAppLaunchedListener(async () => {
-    Navigation.setRoot({
-      root: {
-        stack: {
-          children: [
-            {
-              component: {
-                name: 'Home',
-              },
+Navigation.registerComponent('Home', () => HomeScreen);
+Navigation.registerComponent('Details', () => DetailsScreen);
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home',
             },
-          ],
-        },
+          },
+        ],
       },
-    })
+    },
   })
 })
-
-
--- test/react-native/features/fixtures/app/react_native_navigation_js/screens/Details.js
+++ test/react-native/features/fixtures/app/react_native_navigation_js/screens/Details.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {Navigation} from 'react-native-navigation';
 import Bugsnag from '@bugsnag/react-native'
 
-const DetailsScreen = () => {
+const DetailsScreen = (props) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
