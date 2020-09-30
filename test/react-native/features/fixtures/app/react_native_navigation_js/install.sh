ec6879b7e360d4e5febfe24589ab6b2e23fae893
Alex Moinet <alex@bugsnag.com>
2020-09-30 10:11:34 +0100

    Tests/RN-native-nav: Ensure only compatible versions of react-native-navigation are installed in RN0.60

--- test/react-native/features/fixtures/app/react_native_navigation_js/install.sh
+++ test/react-native/features/fixtures/app/react_native_navigation_js/install.sh
@@ -1,2 +1,10 @@
-npm i react-native-navigation@^7.0.0
+#!/bin/bash
+
+if [ "$REACT_NATIVE_VERSION" == "rn0.60" ];
+then
+   npm i react-native-navigation@7.0.0
+else
+   npm i react-native-navigation@^7.0.0
+fi
+
 npx rnn-link
