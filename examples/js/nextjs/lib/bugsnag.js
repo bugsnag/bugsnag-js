8d9d2a240077f877aa5314105181a7bdaf02ac87
Kyle Holmberg <inbox@kylemh.com>
2020-10-05 14:19:53 -0700

    Update next.js example

--- examples/js/nextjs/lib/bugsnag.js
+++ examples/js/nextjs/lib/bugsnag.js
@@ -4,6 +4,14 @@ import BugsnagPluginReact from '@bugsnag/plugin-react'
 import getConfig from 'next/config'
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 
+// If you only leverage Bugsnag on the client.
+// if (!Bugsnag._client) {
+//   Bugsnag.start({
+//     apiKey: serverRuntimeConfig.BUGSNAG_API_KEY || publicRuntimeConfig.BUGSNAG_API_KEY,
+//     plugins: [new BugsnagPluginReact(React)]
+//   });
+// }
+
 Bugsnag.start({
   apiKey: serverRuntimeConfig.BUGSNAG_API_KEY || publicRuntimeConfig.BUGSNAG_API_KEY,
   plugins: [new BugsnagPluginReact(React)]
