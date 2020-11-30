f5d81629ef751909b35dfca1b30f6b369cd42a5b
Joe Haines <hello@joehaines.co.uk>
2020-11-30 17:10:44 +0000

    Start adding tests for 'install' command

--- test/react-native-cli/features/install.feature
+++ test/react-native-cli/features/install.feature
@@ -1,6 +1,35 @@
 Feature: install command
 
-Scenario: running 'install' on an empty fixture
+Scenario: no git repo, do not run
     When I run the React Native service interactively
-    And I input "bugsnag-react-native-cli" interactively
-    Then I wait for the shell to output "bugsnag-react-native-cli <command>" to stdout
+    And I input "bugsnag-react-native-cli install" interactively
+    Then I wait for the shell to output a line containing "No repo detected." to stdout
+    And I wait for the shell to output the following to stdout
+        """
+        This command may make modifications to your project. It is recommended that you commit the
+        current status of your code to a git repo before continuing.
+        """
+    And the current stdout line contains "Do you want to continue anyway?"
+    When I press enter
+    Then the last interactive command exited successfully
+    And bugsnag has not been added to the package.json file
+
+Scenario: no git repo, run anyway, default version
+    When I run the React Native service interactively
+    And I input "bugsnag-react-native-cli install" interactively
+    Then I wait for the shell to output a line containing "No repo detected." to stdout
+    And I wait for the shell to output the following to stdout
+        """
+        This command may make modifications to your project. It is recommended that you commit the
+        current status of your code to a git repo before continuing.
+        """
+    And the current stdout line contains "Do you want to continue anyway?"
+    When I input "y" interactively
+    Then I wait for the shell to output a line containing "Adding @bugsnag/react-native dependency" to stdout
+    And I wait for the shell to output a line containing "Using yarn or npm" to stdout
+    When I press enter
+    Then I wait for the current stdout line to contain "If you want the latest version of @bugsnag/react-native hit enter, otherwise type the version you want"
+    When I press enter
+    Then I wait for the shell to output a line containing "+ @bugsnag/react-native" to stdout
+    And the last interactive command exited successfully
+    And bugsnag has been added to the package.json file
--- test/react-native-cli/features/steps/steps.rb
+++ test/react-native-cli/features/steps/steps.rb
@@ -24,3 +24,85 @@ end
 When('I run the React Native service interactively') do
   step("I run the service '#{current_fixture}' interactively")
 end
+
+When('I press enter') do
+  step('I input "" interactively')
+end
+
+When('I wait for the shell to output the following to stdout') do |expected|
+  wait = Maze::Wait.new(timeout: MazeRunner.config.receive_requests_wait)
+
+  success = wait.until do
+    stdout = Runner.interactive_session.stdout_lines.join("\n")
+
+    stdout.include?(expected)
+  end
+
+  assert(
+    success,
+    <<~ERROR
+      Did not find the expected message in stdout
+
+      stdout:
+      #{Runner.interactive_session.stdout_lines.inspect}
+    ERROR
+  )
+end
+
+Then("I wait for the shell to output a line containing {string} to stdout") do |expected|
+  wait = Maze::Wait.new(timeout: MazeRunner.config.receive_requests_wait)
+  success = wait.until do
+    Runner.interactive_session.stdout_lines.any? do |line|
+      line.include?(expected)
+    end
+  end
+
+  assert(
+    success,
+    <<~ERROR
+      No stdout lines contained '#{expected}'
+
+      stdout:
+      #{Runner.interactive_session.stdout_lines.inspect}
+    ERROR
+  )
+end
+
+Then("I wait for the current stdout line to contain {string}") do |expected|
+  wait = Maze::Wait.new(timeout: MazeRunner.config.receive_requests_wait)
+  wait.until do
+    Runner.interactive_session.current_buffer.include?(expected)
+  end
+
+  assert_includes(Runner.interactive_session.current_buffer, expected)
+end
+
+def parse_package_json
+  before = Runner.interactive_session.stdout_lines.dup
+
+  steps %Q{
+    When I input "cat package.json" interactively
+    Then I wait for the shell to output '"dependencies": \{' to stdout
+  }
+
+  after = Runner.interactive_session.stdout_lines
+
+  difference = after - before
+
+  # Drop one line as it's the "cat package.json" command
+  JSON.parse(difference.drop(1).join("\n"))
+end
+
+Then("bugsnag has been added to the package.json file") do
+  json = parse_package_json
+
+  assert_includes(json, "dependencies")
+  assert_includes(json["dependencies"], "@bugsnag/react-native")
+end
+
+Then("bugsnag has not been added to the package.json file") do
+  json = parse_package_json
+
+  assert_includes(json, "dependencies")
+  refute_includes(json["dependencies"], "@bugsnag/react-native")
+end
