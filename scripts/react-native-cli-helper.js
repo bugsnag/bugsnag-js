777377d8e6068fc83cc5d29395854dab574e00a9
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-12-01 12:12:59 +0000

    ci(react-native-cli): install cli from artifactory

--- scripts/react-native-cli-helper.js
+++ scripts/react-native-cli-helper.js
@@ -5,12 +5,16 @@ const common = require('./common')
 const fs = require('fs')
 
 module.exports = {
-  buildAndroid: function buildAndroid (sourceFixtures, destFixtures) {
-    const version = common.determineVersion()
-    const rnVersion = process.env.REACT_NATIVE_VERSION
+  installCli: function installCli() {
+    const version = process.env.NOTIFIER_VERSION || common.determineVersion()
     const registryUrl = process.env.REGISTRY_URL
 
     console.log(`Installing CLI version: ${version}`)
+    const installCommand = `npm install -g bugsnag-react-native-cli@${version} --registry ${registryUrl}`
+    common.run(installCommand, true)
+  },
+  buildAndroid: function buildAndroid (sourceFixtures, destFixtures) {
+    const rnVersion = process.env.REACT_NATIVE_VERSION
 
     // Copy in files required from the host (just the Android ones)
     common.run(`mkdir -p ${destFixtures}/${rnVersion}`)
@@ -19,11 +23,10 @@ module.exports = {
 
     // JavaScript layer
     common.changeDir(`${destFixtures}/${rnVersion}`)
-    common.run(`npm install --registry ${registryUrl}`, true)
+    common.run(`npm install`, true)
 
-    // Install and run the CLI
-    const installCommand = `npm install bugsnag-react-native-cli@${version} --registry ${registryUrl}`
-    common.run(installCommand, true)
+    // Install and run the CLI to integrate Bugsnag
+    this.installCli()
     // TODO App will not build at present if init is run
     // const initCommand = './node-modules/bugsnag-react-native-cli/bin/react-native-cli init'
     // common.run(initCommand, true)
@@ -37,7 +40,6 @@ module.exports = {
                     `${process.env.PWD}/build/${rnVersion}.apk`)
   },
   buildIOS: function buildIOS () {
-    const version = common.determineVersion()
     const rnVersion = process.env.REACT_NATIVE_VERSION
     const registryUrl = process.env.REGISTRY_URL
     const fixturesDir = 'test/react-native-cli/features/fixtures'
@@ -55,6 +57,7 @@ module.exports = {
     common.run(`npm install --registry ${registryUrl}`, true)
 
     // Install and run the CLI
+    this.installCli()
     const installCommand = `npm install bugsnag-react-native-cli@${version} --registry ${registryUrl}`
     common.run(installCommand, true)
     // TODO Need to provide an answers file for the init command
