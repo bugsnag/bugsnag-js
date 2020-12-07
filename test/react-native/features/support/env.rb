b834d77da185d80864e1c425f17d9b5d2517ff50
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-12-07 20:54:53 +0000

    Pin Node to MazeRunner v3.6.0

--- test/react-native/features/support/env.rb
+++ test/react-native/features/support/env.rb
@@ -1,7 +1,5 @@
 AfterConfiguration do |_config|
   MazeRunner.config.receive_no_requests_wait = 15
-  # TODO: Remove once the Bugsnag-Integrity header has been implemented
-  MazeRunner.config.enforce_bugsnag_integrity = false
 end
 
 Before('@android_only') do |scenario|
