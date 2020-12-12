2fb59c8b284fd7e87ac3ab2c41b8c7d8d605ae6a
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-12-12 14:29:45 +0000

    Add buttons to remaining fixtures

--- test/react-native-cli/features/fixtures/rn0_63/App.js
+++ test/react-native-cli/features/fixtures/rn0_63/App.js
@@ -13,14 +13,11 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
-} from 'react-native';
+  StatusBar, Button
+} from 'react-native'
 
 import {
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
@@ -37,32 +34,15 @@ const App: () => React$Node = () => {
             </View>
           )}
           <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+            <Text>React Native CLI end-to-end test app</Text>
+            <Button style={styles.clickyButton}
+                    accessibilityLabel='js_notify'
+                    title='JS Notify'
+                    onPress={this.jsNotify}/>
+            <Button style={styles.clickyButton}
+                    accessibilityLabel='native_notify'
+                    title='Native Notify'
+                    onPress={this.nativeNotify}/>
           </View>
         </ScrollView>
       </SafeAreaView>
