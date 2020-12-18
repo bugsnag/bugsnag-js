dd8948bd4e04d2d806c9e35e394e6ce36885e254
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-12-18 22:04:31 +0000

    Enable source map uploads during build

--- test/react-native-cli/features/fixtures/rn-cli-init-android.sh
+++ test/react-native-cli/features/fixtures/rn-cli-init-android.sh
@@ -35,7 +35,7 @@ send -- 5.5.0-alpha01\r
 
 # TODO: Use the usual 123123... test API key
 expect "What is your Bugsnag API key?"
-send -- b161f2dbabe3204527bbe5ed4b9334a4\r
+send -- 12312312312312312312312312312312\r
 
 expect "Do you want to automatically upload source maps as part of the Xcode build?"
 send -- n
@@ -44,10 +44,9 @@ expect "This will enable you to see full native stacktraces. It can't be done au
 send -- \r
 
 expect "Do you want to automatically upload source maps as part of the Gradle build?"
-send -- n
+send -- y
 
-# TODO: Disable source map uploads for now
-#expect "If you want the latest version of @bugsnag/source-maps hit enter, otherwise type the version you want"
-#send -- latest\r
+expect "If you want the latest version of @bugsnag/source-maps hit enter, otherwise type the version you want"
+send -- latest\r
 
 expect eof
