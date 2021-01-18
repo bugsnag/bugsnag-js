7fcc540d617d8cd3671bed30277887e2a262f055
Dan Skinner <dan@bugsnag.com>
2021-01-18 16:57:04 +0000

    chore: JSON parse converage files before diffing

--- dangerfile.js
+++ dangerfile.js
@@ -9,13 +9,13 @@ const istanbulDiff = require('istanbul-diff')
 const before = {
   minified: parseInt(readFileSync(`${__dirname}/.diff/size-before-minified`, 'utf8').trim()),
   gzipped: parseInt(readFileSync(`${__dirname}/.diff/size-before-gzipped`, 'utf8').trim()),
-  coverage: readFileSync(`${__dirname}/.diff/coverage-before.json`, 'utf8').trim()
+  coverage: JSON.parse(readFileSync(`${__dirname}/.diff/coverage-before.json`, 'utf8').trim())
 }
 
 const after = {
   minified: parseInt(readFileSync(`${__dirname}/.diff/size-after-minified`, 'utf8').trim()),
   gzipped: parseInt(readFileSync(`${__dirname}/.diff/size-after-gzipped`, 'utf8').trim()),
-  coverage: readFileSync(`${__dirname}/.diff/coverage-after.json`, 'utf8').trim()
+  coverage: JSON.parse(readFileSync(`${__dirname}/.diff/coverage-after.json`, 'utf8').trim())
 }
 
 const formatKbs = (n) => `${(n / 1000).toFixed(2)} kB`
@@ -45,9 +45,6 @@ codeCoverage([{
 }])
 
 const diff = istanbulDiff.diff(before.coverage, after.coverage)
-
-markdown(before.coverage)
-markdown(after.coverage)
 
 markdown(JSON.stringify(diff, null, 2))
 
