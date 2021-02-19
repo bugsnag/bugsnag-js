e4a9ac7919b661f439365c0d3f56b34ad93e594a
Alex Moinet <alex@bugsnag.com>
2021-02-19 17:26:48 +0000

    Tests/Expo-v4: Update fixture endpoints

--- test/expo/features/fixtures/test-app/app/bugsnag.js
+++ test/expo/features/fixtures/test-app/app/bugsnag.js
@@ -1,8 +1,8 @@
 import Bugsnag from '@bugsnag/expo'
 
 const endpoints = {
-  notify: 'http://bs-local.com:9339',
-  sessions: 'http://bs-local.com:9339'
+  notify: 'http://bs-local.com:9339/notify',
+  sessions: 'http://bs-local.com:9339/sessions'
 }
 
 const bugsnagClient = Bugsnag.createClient({
