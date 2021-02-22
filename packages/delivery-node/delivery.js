df46ea3787998793c42f96c22dddfbc8f54b04bb
Joe Haines <hello@joehaines.co.uk>
2021-02-22 13:22:05 +0000

    Add a flush function to delivery-node
    This returns a promise which will resolve if all requests finish
    sending within the specified timeout. If the timeout is exceeded,
    the promise will reject instead

--- packages/delivery-node/delivery.js
+++ packages/delivery-node/delivery.js
@@ -1,9 +1,23 @@
 const payload = require('@bugsnag/core/lib/json-payload')
+const cuid = require('@bugsnag/cuid')
 const request = require('./request')
+
+const DEFAULT_FLUSH_TIMEOUT_MS = 2000
+const FLUSH_POLL_INTERVAL_MS = 50
+
+// this map holds requests that we have not got responses or errors from
+const inFlightRequests = new Map()
 
 module.exports = (client) => ({
   sendEvent: (event, cb = () => {}) => {
+    // mark this request as in-flight
+    const id = cuid()
+    inFlightRequests.set(id, true)
+
     const _cb = err => {
+      // this request has finished so remove it from the in-flight requests
+      inFlightRequests.delete(id)
+
       if (err) client._logger.error(`Event failed to send…\n${(err && err.stack) ? err.stack : err}`, err)
       cb(err)
     }
@@ -24,8 +38,16 @@ module.exports = (client) => ({
       _cb(e)
     }
   },
+
   sendSession: (session, cb = () => {}) => {
+    // mark this request as in-flight
+    const id = cuid()
+    inFlightRequests.set(id, true)
+
     const _cb = err => {
+      // this request has finished so remove it from the in-flight requests
+      inFlightRequests.delete(id)
+
       if (err) client._logger.error(`Session failed to send…\n${(err && err.stack) ? err.stack : err}`, err)
       cb(err)
     }
@@ -45,5 +67,27 @@ module.exports = (client) => ({
     } catch (e) {
       _cb(e)
     }
+  },
+
+  _flush: (timeoutMs = DEFAULT_FLUSH_TIMEOUT_MS) => {
+    return new Promise((resolve, reject) => {
+      const timeout = setTimeout(
+        () => { reject(new Error(`_flush timed out after ${timeoutMs}ms`)) },
+        timeoutMs
+      )
+
+      const resolveIfNoRequests = () => {
+        if (inFlightRequests.size === 0) {
+          clearTimeout(timeout)
+          resolve()
+
+          return
+        }
+
+        setTimeout(resolveIfNoRequests, FLUSH_POLL_INTERVAL_MS)
+      }
+
+      resolveIfNoRequests()
+    })
   }
 })
--- packages/delivery-node/test/delivery.test.ts
+++ packages/delivery-node/test/delivery.test.ts
@@ -1,8 +1,10 @@
 import delivery from '../'
 import http from 'http'
 import { Client } from '@bugsnag/core'
-import { EventDeliveryPayload } from '@bugsnag/core/client'
+import { Delivery, EventDeliveryPayload, SessionDeliveryPayload } from '@bugsnag/core/client'
 import { AddressInfo } from 'net'
+
+type NodeDelivery = Delivery & { _flush(timeoutMs?: number): Promise<void> }
 
 interface Request {
   url?: string
@@ -31,6 +33,8 @@ const mockServer = (successCode = 200) => {
     })
   }
 }
+
+const noop = () => {}
 
 describe('delivery:node', () => {
   it('sends events successfully', done => {
@@ -66,7 +70,7 @@ describe('delivery:node', () => {
     server.listen((err: Error) => {
       expect(err).toBeUndefined()
 
-      const payload = { sample: 'payload' } as unknown as EventDeliveryPayload
+      const payload = { sample: 'payload' } as unknown as SessionDeliveryPayload
       const config = {
         apiKey: 'aaaaaaaa',
         endpoints: { notify: 'blah', sessions: `http://0.0.0.0:${(server.address() as AddressInfo).port}/sessions/` },
@@ -155,6 +159,109 @@ describe('delivery:node', () => {
         server.close()
         done()
       })
+    })
+  })
+
+  it('can flush successfully', done => {
+    const { requests, server } = mockServer()
+    server.listen((err: Error) => {
+      expect(err).toBeUndefined()
+
+      const eventPayload = { sample: 'eventPayload' } as unknown as EventDeliveryPayload
+      const eventPayload2 = { example: 'eventPayload2' } as unknown as EventDeliveryPayload
+      const sessionPayload = { sample: 'sessionPayload' } as unknown as SessionDeliveryPayload
+      const sessionPayload2 = { sample: 'sessionPayload2' } as unknown as SessionDeliveryPayload
+
+      const url = `http://0.0.0.0:${(server.address() as AddressInfo).port}`
+      const config = {
+        apiKey: 'aaaaaaaa',
+        endpoints: { notify: `${url}/notify/`, sessions: `${url}/sessions/` },
+        redactedKeys: []
+      }
+
+      const client = { _logger: { error: console.error.bind(console) }, _config: config } as unknown as Client
+      const _delivery = delivery(client) as NodeDelivery
+
+      _delivery.sendEvent(eventPayload, noop)
+      _delivery.sendEvent(eventPayload2, noop)
+      _delivery.sendSession(sessionPayload, noop)
+      _delivery.sendSession(sessionPayload2, noop)
+
+      _delivery._flush().then(() => {
+        // the payloads could be delivered out of order so we can't match them
+        // to a specific request body, i.e. payloads[1] may not be requests[1]
+        const payloads = [
+          JSON.stringify(eventPayload),
+          JSON.stringify(eventPayload2),
+          JSON.stringify(sessionPayload),
+          JSON.stringify(sessionPayload2)
+        ]
+
+        expect(requests.length).toBe(payloads.length)
+
+        payloads.forEach((_, i) => {
+          expect(requests[i].method).toBe('POST')
+          expect(requests[i].url).toMatch(new RegExp('/(notify|sessions)/'))
+          expect(requests[i].headers['content-type']).toEqual('application/json')
+          expect(requests[i].headers['bugsnag-api-key']).toEqual('aaaaaaaa')
+          expect(requests[i].headers['bugsnag-payload-version']).toMatch(/^(1|4)$/)
+          expect(requests[i].headers['bugsnag-sent-at']).toMatch(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)
+
+          expect(payloads).toContain(requests[i].body)
+        })
+
+        // ensure the received payloads are different as the 'toContain' would
+        // pass if they were all the same
+        expect(requests[0].body).not.toBe(requests[1].body)
+        expect(requests[0].body).not.toBe(requests[2].body)
+        expect(requests[0].body).not.toBe(requests[3].body)
+        expect(requests[1].body).not.toBe(requests[2].body)
+        expect(requests[1].body).not.toBe(requests[3].body)
+        expect(requests[2].body).not.toBe(requests[3].body)
+
+        server.close()
+        done()
+      })
+    })
+  })
+
+  it('will timeout if flush takes too long', done => {
+    const { requests, server } = mockServer()
+    server.listen((err: Error) => {
+      expect(err).toBeUndefined()
+
+      const numberOfEventsToSend = 25
+      const events: EventDeliveryPayload[] =
+        Array.from({ length: numberOfEventsToSend })
+          .map((_, i) => ({ sample: `event payload ${i}` } as unknown as EventDeliveryPayload))
+
+      const config = {
+        apiKey: 'aaaaaaaa',
+        endpoints: { notify: `http://0.0.0.0:${(server.address() as AddressInfo).port}/notify/` },
+        redactedKeys: []
+      }
+
+      const client = { _logger: { error: console.error.bind(console) }, _config: config } as unknown as Client
+      const _delivery = delivery(client) as NodeDelivery
+
+      events.forEach(event => _delivery.sendEvent(event, noop))
+
+      _delivery._flush(0)
+        .then(() => { throw new Error('Promise resolved when it should not!') })
+        .catch((err: Error) => {
+          expect(err.message).toBe('_flush timed out after 0ms')
+
+          // it's possible for some events to be delivered at this point, but
+          // it's extremely unlikely for all of them to have been delivered
+          expect(requests.length).toBeLessThan(numberOfEventsToSend)
+
+          // allow any outstanding events to be delivered before closing the
+          // server, otherwise this will cause errors
+          _delivery._flush().then(() => {
+            server.close()
+            done()
+          })
+        })
     })
   })
 })
