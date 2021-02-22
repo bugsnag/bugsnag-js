6e321d3d02cac3d05bf737fbbef521c5e80678de
Delisa Mason <iskanamagus@gmail.com>
2021-02-22 16:37:45 +0000

    build: add custom test matcher to check path exists on disk

---
+++ test/setup.ts
@@ -0,0 +1,28 @@
+import { access } from 'fs/promises'
+
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeAFile: () => CustomMatcherResult
+    }
+  }
+}
+
+expect.extend({
+  async toBeAFile (received: string) {
+    try {
+      await access(received)
+      return {
+        message: () => `expected ${received} not to exist in the file system`,
+        pass: true
+      }
+    } catch (e) {
+      return {
+        message: () => `expected ${received} to exist in the file system`,
+        pass: false
+      }
+    }
+  }
+})
