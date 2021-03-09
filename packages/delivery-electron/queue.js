575ed419c94431e0cda3b1b87fa4d4e6b36a861f
Delisa Mason <iskanamagus@gmail.com>
2021-03-09 17:14:20 +0000

    feat(delivery-electron): implement queue

---
+++ packages/delivery-electron/queue.js
@@ -0,0 +1,124 @@
+const { join } = require('path')
+const { promises } = require('fs')
+const { mkdir, readdir, writeFile, readFile, unlink } = promises
+const { randomBytes } = require('crypto')
+
+const MAX_ITEMS = 64
+const filenameRe = /^bugsnag-.*\.json$/
+
+module.exports = class PayloadQueue {
+  constructor (path, resource, onerror = () => {}) {
+    this._onerror = onerror
+    this._path = path
+    this._truncating = false
+    this._generateFilename = () =>
+      `bugsnag-${resource}-${(new Date()).toISOString()}-${uid()}.json`
+    this._init = null
+  }
+
+  /*
+   * Create storage path (if needed). Succeeds if the directory is created or
+   * already exists.
+   */
+  async init () {
+    if (!this._init) { // initialize only once
+      this._init = mkdir(this._path, { recursive: true })
+    }
+
+    return this._init
+  }
+
+  /*
+   * Adds an item to the end of the queue
+   */
+  async enqueue (item) {
+    try {
+      await this.init()
+      const data = JSON.stringify(item)
+      await writeFile(join(this._path, this._generateFilename()), data)
+      await this._truncate()
+    } catch (e) {
+      this._onerror(e)
+    }
+  }
+
+  /*
+   * Returns the oldest item in the queue without removing it
+   */
+  async peek () {
+    try {
+      const payloads = await this._getPayloads()
+      // loop from first to last until we find a valid payload
+      for (const filepath of payloads) {
+        try {
+          const payload = JSON.parse(await readFile(filepath))
+          return { path: filepath, payload }
+        } catch (e) {
+          // if we got here it's because
+          // a) JSON.parse failed or
+          // b) the file can no longer be read (maybe it was truncated?)
+          // in both cases we want to speculatively remove it and try the next result
+          await this.remove(filepath)
+        }
+      }
+    } catch (e) {
+      this._onerror(e)
+    }
+    // no payloads or all were invalid (and removed)
+    return null
+  }
+
+  /*
+   * Removes an item from the queue by its full path.
+   * Tolerant of errors while removing.
+   */
+  async remove (filepath) {
+    try {
+      await unlink(filepath)
+    } catch (e) {
+      this._onerror(e)
+    }
+  }
+
+  /*
+   * Keeps the queue size bounded by MAX_ITEMS
+   * Tolerant of errors while removing.
+   */
+  async _truncate () {
+    // this isn't atomic so only enter this method once at any time
+    if (this._truncating) return
+    this._truncating = true
+
+    try {
+      const payloads = await this._getPayloads()
+
+      // figure out how many over MAX_ITEMS we are
+      const diff = payloads.length - MAX_ITEMS
+
+      // do nothing if within the limit
+      if (diff > 0) {
+        // wait for each of the items over the limit to be removed
+        await Promise.all(
+          payloads.slice(0, diff)
+            .map(f => this.remove(f))
+        )
+      }
+    } catch (e) {
+      this._onerror(e)
+    } finally {
+      this._truncating = false
+    }
+  }
+
+  /*
+   * List the payloads in order
+   */
+  async _getPayloads () {
+    return (await readdir(this._path, { withFileTypes: true }))
+      .filter(f => f.isFile() && filenameRe.test(f.name))
+      .map(f => join(this._path, f.name))
+      .sort()
+  }
+}
+
+const uid = () => randomBytes(8).toString('hex')
---
+++ packages/delivery-electron/test/queue.test.ts
@@ -0,0 +1,159 @@
+import { promises, constants } from 'fs'
+import { join } from 'path'
+import PayloadQueue from '../queue'
+const { access, mkdtemp, readdir, readFile, rmdir, writeFile } = promises
+const { F_OK } = constants
+
+describe('delivery: electron -> queue', () => {
+  let tempdir = ''
+
+  beforeEach(async () => {
+    tempdir = await mkdtemp('delivery-queue-')
+  })
+
+  afterEach(async () => {
+    await rmdir(tempdir, { recursive: true })
+  })
+
+  describe('init()', () => {
+    it('creates the storage directory', async () => {
+      const storagePath = join(tempdir, 'foooo')
+      const queue = new PayloadQueue(storagePath, 'stuff')
+
+      expect(storagePath).not.toBeAFile()
+      await queue.init()
+      expect(storagePath).toBeAFile()
+    })
+
+    it('throws an error when the directory was not succesfully created', async (done) => {
+      const storagePath = join('/invalidpath', 'foooo')
+      const queue = new PayloadQueue(storagePath, 'stuff')
+      let didErr = false
+
+      try {
+        await queue.init()
+      } catch (e) {
+        // eslint-disable-next-line jest/no-try-expect
+        expect(e).toBeTruthy()
+        didErr = true
+      }
+      expect(didErr).toBe(true)
+      done()
+    })
+
+    it('rejects all pending promises', async () => {
+      const storagePath = join('/invalidpath', 'foooo')
+      const queue = new PayloadQueue(storagePath, 'stuff')
+      const errs: any[] = []
+
+      // eslint-disable-next-line jest/valid-expect-in-promise
+      await Promise.all([
+        queue.init().catch(err => errs.push(err)),
+        queue.init().catch(err => errs.push(err)),
+        queue.init().catch(err => errs.push(err))
+      ])
+      expect(errs.length).toBe(3)
+    })
+  })
+
+  describe('peek()', () => {
+    it('returns null if there are no files', async () => {
+      const queue = new PayloadQueue(tempdir, 'stuff')
+      expect(await queue.peek()).toBe(null)
+    })
+
+    it('returns null if there are only files that don’t match the expected pattern', async () => {
+      await writeFile(join(tempdir, '.info'), '{}')
+      await writeFile(join(tempdir, 'other.json'), '{}')
+
+      const queue = new PayloadQueue(tempdir, 'stuff')
+      expect(await queue.peek()).toBe(null)
+    })
+
+    it('parses an existing file into JSON', async () => {
+      const path = join(tempdir, 'bugsnag-stuff-01.json')
+      await writeFile(path, JSON.stringify({ colors: 3 }))
+
+      const queue = new PayloadQueue(tempdir, 'stuff')
+      const item = await queue.peek()
+      expect(item).toEqual({ path: path, payload: { colors: 3 } })
+    })
+
+    it('calls the onerror callback and returns null if there is an error', async (done) => {
+      const queue = new PayloadQueue('/non/existent/path', 'stuff', err => {
+        expect(err).not.toBe(null)
+        done()
+      })
+      const item = await queue.peek()
+      expect(item).toBe(null)
+    })
+
+    it('removes a file if it’s not valid JSON', async () => {
+      const path = join(tempdir, 'bugsnag-stuff-02.json')
+      await writeFile(path, '{"colors":')
+
+      const queue = new PayloadQueue(tempdir, 'stuff')
+      const item = await queue.peek()
+      expect(item).toBe(null)
+      await expect(access(path, F_OK)).rejects.toBeTruthy()
+    })
+  })
+
+  describe('enqueue()', () => {
+    it('handles creating the directory if needed', async () => {
+      const path = join(tempdir, 'this-does-not-exist-yet')
+      const queue = new PayloadQueue(path, 'stuff')
+      await queue.enqueue({ color: 'yellow' })
+
+      const items = await readdir(path)
+      expect(items.length).toEqual(1)
+
+      const contents = await readFile(join(path, items[0]))
+      expect(JSON.parse(contents.toString())).toEqual({ color: 'yellow' })
+    })
+
+    it('calls the onerror callback if there is an error', async (done) => {
+      const queue = new PayloadQueue('/non/existent/path', 'stuff', (err) => {
+        expect(err).toBeTruthy()
+        done()
+      })
+      await queue.enqueue({})
+    })
+
+    it('purges items that are over the limit', async () => {
+      const uid = (index: number) => index.toString().padStart(2, '0')
+      Array(70).fill(1).map(async (_, index) => {
+        await writeFile(join(tempdir, `bugsnag-stuff-${uid(index)}.json`), '{}')
+      })
+
+      const queue = new PayloadQueue(tempdir, 'stuff')
+      await queue.enqueue({ color: 'yellow' })
+
+      const items = await readdir(tempdir)
+      expect(items.length).toEqual(64)
+
+      for (let index = 7; index < 70; index++) {
+        await access(join(tempdir, `bugsnag-stuff-${uid(index)}.json`), F_OK)
+      }
+    })
+  })
+
+  describe('remove()', () => {
+    it('deletes the file', async () => {
+      const path = join(tempdir, 'bugsnag-stuff-03.json')
+      await writeFile(path, '{}')
+
+      const queue = new PayloadQueue(tempdir, 'stuff')
+      await queue.remove(path)
+      await expect(access(path, F_OK)).rejects.toBeTruthy()
+    })
+
+    it('calls the onerror callback if there is an error', async (done) => {
+      const queue = new PayloadQueue(tempdir, 'stuff', (err) => {
+        expect(err).toBeTruthy()
+        done()
+      })
+      await queue.remove('/non/existent/file')
+    })
+  })
+})
