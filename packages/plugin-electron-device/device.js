60cff7841ed6fe792c6bc588a3f655c526d12641
Joe Haines <hello@joehaines.co.uk>
2021-03-22 12:09:52 +0000

    Wait for the app to be ready before using screen

--- packages/plugin-electron-device/device.js
+++ packages/plugin-electron-device/device.js
@@ -22,8 +22,6 @@ module.exports = (app, screen, process, filestore, NativeClient, powerMonitor) =
     const device = {}
     const updateDevice = createDeviceUpdater(client, NativeClient, device)
 
-    const primaryDisplay = screen.getPrimaryDisplay() || {}
-
     updateDevice({
       totalMemory: kibibytesToBytes(process.getSystemMemoryInfo().total),
       locale: app.getLocale(),
@@ -36,16 +34,40 @@ module.exports = (app, screen, process, filestore, NativeClient, powerMonitor) =
       }
     })
 
-    // 'isLocked' goes in metadata.device
     client.addMetadata('device', {
-      screenResolution: primaryDisplay.size,
-      screenDensity: primaryDisplay.scaleFactor,
       // the value for 'idleThreshold' doesn't matter here because it is ignored
       // if the system is locked and that's the only state we care about
       isLocked: powerMonitor.getSystemIdleState(1) === 'locked',
       // note: this is only available from Electron 12, earlier versions cannot
       // read the battery state without 'on-ac'/'on-battery' events
       usingBattery: powerMonitor.onBatteryPower
+    })
+
+    // the screen module can't be used until the app is ready
+    app.whenReady().then(() => {
+      const primaryDisplay = screen.getPrimaryDisplay() || {}
+
+      client.addMetadata('device', {
+        screenResolution: primaryDisplay.size,
+        screenDensity: primaryDisplay.scaleFactor
+      })
+
+      screen.on('display-metrics-changed', (event, display, changedMetrics) => {
+        // ignore updates to non-primary screens
+        if (display.id !== primaryDisplay.id) {
+          return
+        }
+
+        // ignore updates that don't affect the size or scale factor
+        if (!changedMetrics.includes('bounds') && !changedMetrics.includes('scaleFactor')) {
+          return
+        }
+
+        client.addMetadata('device', {
+          screenResolution: display.size,
+          screenDensity: display.scaleFactor
+        })
+      })
     })
 
     // fetch the device ID from the filestore - if one does not exist it will be
@@ -58,23 +80,6 @@ module.exports = (app, screen, process, filestore, NativeClient, powerMonitor) =
         }
       })
       .catch(err => { client._logger.error(err) })
-
-    screen.on('display-metrics-changed', (event, display, changedMetrics) => {
-      // ignore updates to non-primary screens
-      if (display.id !== primaryDisplay.id) {
-        return
-      }
-
-      // ignore updates that don't affect the size or scale factor
-      if (!changedMetrics.includes('bounds') && !changedMetrics.includes('scaleFactor')) {
-        return
-      }
-
-      client.addMetadata('device', {
-        screenResolution: display.size,
-        screenDensity: display.scaleFactor
-      })
-    })
 
     powerMonitor.on('on-ac', () => {
       client.addMetadata('device', { usingBattery: false })
