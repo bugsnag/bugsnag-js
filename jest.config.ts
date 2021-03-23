0de7cf033db8ef43ec15e09cfb73b74409a2b6ab
Delisa <kattrali@users.noreply.github.com>
2021-03-23 17:32:56 +0000

    build: exclude integration dependencies from unit test haste map (#48)
    Jest's module resolution will run into conflicts if attempts to load
    the integration test NPM cache or the generated artifacts from building
    the test app

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,8 @@ import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
-  clearMocks: true
+  clearMocks: true,
+  modulePathIgnorePatterns: ['.verdaccio', 'test/fixtures', 'test\\fixtures']
 }
 
 export default config
