175a97b9f5329a5bd0fa4d81f1ad09a06a91617c
Karl Stenerud <kstenerud@gmail.com>
2021-03-25 09:43:47 +0100

    Remove android vendoring from the repo and use a script to pull on demand instead

--- packages/react-native/update-android.sh
+++ packages/react-native/update-android.sh
@@ -1,30 +1,79 @@
 #!/usr/bin/env bash
-set -e
 
-ANDROID_REPO_DIR=../../../bugsnag-android
-MAVEN_REPO_DIR=~/.m2/repository/com/bugsnag
-AAR_DST=android/com/bugsnag
+# ----------
+# Setup Code
+# ----------
 
-# artefact version needs to be unique to avoid clash with bugsnag-android artefacts
-# deployed on mavenCentral/jcenter, append a 'react-native' suffix
-ANDROID_VERSION=$(cat $ANDROID_REPO_DIR/gradle.properties | grep VERSION_NAME)
-AMENDED_VERSION="${ANDROID_VERSION#*=}-react-native"
+set -euo pipefail
 
-echo "Clearing local maven repo"
-rm -rf $MAVEN_REPO_DIR
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_NAME="${0##*/}"
+CONFIG_FILE="$SCRIPT_DIR/prepare-android-vendor.config"
 
-echo "Building local copy of bugsnag-android $AMENDED_VERSION"
-./$ANDROID_REPO_DIR/gradlew -p $ANDROID_REPO_DIR clean assembleRelease publishToMavenLocal -PVERSION_NAME=$AMENDED_VERSION
+print_help() {
+    echo "Usage: $SCRIPT_NAME [options]"
+    echo
+    echo "Options (must select one):"
+    echo "    --version <version> (example: $SCRIPT_NAME --version 5.8.0)"
+    echo "    --local <path>      (example: $SCRIPT_NAME --local ../../../bugsnag-android)"
+    echo "    --sha <version>     (example: $SCRIPT_NAME --sha c8210a3)"
+    echo "    --list-versions     (lists all versions available and exits)"
+}
 
-echo "Removing previous AARs from bugsnag-js"
-rm -rf $AAR_DST
+error_bad_opt(){
+    echo "$SCRIPT_NAME: invalid option '$1'";
+    echo
+    print_help
+    exit 1
+}
 
-echo "Copying new AARs to bugsnag-js"
-cp -r $MAVEN_REPO_DIR $AAR_DST
+error_missing_field(){
+    echo "$SCRIPT_NAME: $1 missing required field";
+    echo
+    print_help
+    exit 1
+}
 
-echo "Recording version"
-rm -rf ./android/.bugsnag-android-version
-echo $(cd $ANDROID_REPO_DIR && git rev-parse HEAD) >> ./android/.bugsnag-android-version
+MODE=unset
+GIT_TAG=
+BUGSNAG_ANDROID_REPO_DIR=
 
-sed -i '' "s/api \"com.bugsnag:bugsnag-android:.*/api \"com.bugsnag:bugsnag-android:$AMENDED_VERSION\"/" android/build.gradle
-sed -i '' "s/api \"com.bugsnag:bugsnag-plugin-react-native:.*/api \"com.bugsnag:bugsnag-plugin-react-native:$AMENDED_VERSION\"/" android/build.gradle
+# BSD-friendly getopt-style supporting longopt
+while [ $# -gt 0 ]; do
+    case $1 in
+        --) shift; break;;
+        -*) case $1 in
+            --list-versions)
+                git ls-remote --tags https://github.com/bugsnag/bugsnag-android.git | \
+                    grep -v "{}" | awk "{print \$2}" | sed 's/refs\/tags\/v//g' | sed 's/refs\/tags\///g'
+                exit 0
+                ;;
+            --version)
+                if [ $# -lt 2 ]; then error_missing_field $1; fi
+                echo "version" >"$CONFIG_FILE"
+                echo "v$2" >>"$CONFIG_FILE"
+                exit 0
+                ;;
+            --local)
+                if [ $# -lt 2 ]; then error_missing_field $1; fi
+                full_local_path="$(cd "$2" && pwd)"
+                echo "local" >"$CONFIG_FILE"
+                echo "$full_local_path" >>"$CONFIG_FILE"
+                exit 0
+                ;;
+            --sha)
+                if [ $# -lt 2 ]; then error_missing_field $1; fi
+                echo "sha" >"$CONFIG_FILE"
+                echo "$2" >>"$CONFIG_FILE"
+                exit 0
+                ;;
+            -*)
+                error_bad_opt $1;;
+            esac;;
+        *)  error_bad_opt $1;;
+    esac
+    shift
+done
+
+print_help
+exit 1
