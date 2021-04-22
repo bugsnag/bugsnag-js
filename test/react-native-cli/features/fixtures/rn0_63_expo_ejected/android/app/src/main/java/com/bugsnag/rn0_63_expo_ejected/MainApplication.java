41fc71a02d60a9af16b243a954e5705e9c3da9d2
Ben Gourley <bn@grly.me>
2021-04-22 16:05:19 +0100

    test(react-native-cli): Add missing semi colon. Java!!!

--- test/react-native-cli/features/fixtures/rn0_63_expo_ejected/android/app/src/main/java/com/bugsnag/rn0_63_expo_ejected/MainApplication.java
+++ test/react-native-cli/features/fixtures/rn0_63_expo_ejected/android/app/src/main/java/com/bugsnag/rn0_63_expo_ejected/MainApplication.java
@@ -46,7 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
-      packages.add(new CrashyPackage())
+      packages.add(new CrashyPackage());
       return packages;
     }
 
