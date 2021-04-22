0b25e64648c2da2138845e8ceb42258fbb375c5f
Ben Gourley <bn@grly.me>
2021-04-22 16:39:57 +0100

    Add basic example app

---
+++ examples/electron/electron-basic/src/index.js
@@ -0,0 +1,65 @@
+const Bugsnag = require('@bugsnag/electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+Bugsnag.start('YOUR_API_KEY')
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit()
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'))
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
+ipcMain.on('bugsnag-handled-error', () => {
+  Bugsnag.notify(new Error('handled error in main'))
+})
+
+ipcMain.on('bugsnag-unhandled-error', () => {
+  throw new Error('unhandled error in main')
+})
+
+ipcMain.on('bugsnag-promise-rejection', () => {
+  Promise.reject(new Error('unhandled promise rejection in main'))
+})
