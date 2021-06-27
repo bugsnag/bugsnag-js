efd10ddb2a232e89ba10caed97c027d031eb7bb3
Steve Kirkland <steve@bugsnag.com>
2021-06-27 18:31:30 +0100

    Add bugsnag buttons

--- test/react-native-cli/features/fixtures/rn0_64/App.js
+++ test/react-native-cli/features/fixtures/rn0_64/App.js
@@ -7,16 +7,15 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import Bugsnag from "@bugsnag/react-native";
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  ScrollView,
   View,
-} from 'react-native';
+  Text,
+  StatusBar, Button, NativeModules
+} from 'react-native'
 
 import {
   Colors,
@@ -25,6 +24,20 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+function jsNotify() {
+  try { // execute crashy code
+    iMadeThisUp();
+  } catch (error) {
+    console.log('Bugsnag.notify JS error')
+    Bugsnag.notify(error);
+  }
+}
+
+function nativeNotify() {
+  console.log('Bugsnag.notify native error')
+  NativeModules.CrashyCrashy.handledError()
+}
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -53,37 +66,26 @@ const Section = ({children, title}): Node => {
 };
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+        {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+        )}
+        <View style={styles.body}>
+          <Text>React Native CLI end-to-end test app</Text>
+          <Button style={styles.clickyButton}
+                  accessibilityLabel='js_notify'
+                  title='JS Notify'
+                  onPress={jsNotify}/>
+          <Button style={styles.clickyButton}
+                  accessibilityLabel='native_notify'
+                  title='Native Notify'
+                  onPress={nativeNotify}/>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -91,6 +93,9 @@ const App: () => Node = () => {
 };
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
