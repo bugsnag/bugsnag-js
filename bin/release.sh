0d694dcbb68ed09f1d9e2c49494622889955b8a0
Ben Gourley <bn@grly.me>
2021-06-29 15:01:16 +0100

    build: enable publishing

--- bin/release.sh
+++ bin/release.sh
@@ -28,26 +28,16 @@ npm run bootstrap -- --ci
 # check if the browser package changed – if it didn't we don't need to upload to the CDN
 BROWSER_PACKAGE_CHANGED=`npx lerna changed --parseable | grep -c packages/js$`
 
-prerelease () {
-  npx lerna publish "$VERSION" --dist-tag next
-  if [ $BROWSER_PACKAGE_CHANGED -eq 1 ]; then
-    npm run cdn-upload
-  fi
-}
-
-release () {
-  echo "npx lerna publish $VERSION"
-  if [ $BROWSER_PACKAGE_CHANGED -eq 1 ]; then
-    echo "npm run cdn-upload"
-  fi
-}
-
 case $VERSION in
   "prerelease" | "prepatch" | "preminor" | "premajor")
-    prerelease
+    npx lerna publish "$VERSION" --dist-tag next
     ;;
 
   *)
-    release
+    npx lerna publish $VERSION
     ;;
 esac
+
+if [ $BROWSER_PACKAGE_CHANGED -eq 1 ]; then
+  npm run cdn-upload
+fi
