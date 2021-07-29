d67bf9eb5843172b2877549098d91b37f63d172a
Steve Kirkland <steve@bugsnag.com>
2021-07-29 17:50:17 +0100

    Dismiss any system (crash) dialog on app launch

--- examples/reactnative/rn063example/android/app/src/main/java/com/rn063example/MainActivity.java
+++ examples/reactnative/rn063example/android/app/src/main/java/com/rn063example/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rn063example;
 
+import android.content.Intent
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
@@ -13,5 +14,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "rn063example";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Attempt to dismiss any system dialogs (such as "reactnative keeps stopping")
+    Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+    sendBroadcast(closeDialog);
   }
 }
--- test/react-native/features/fixtures/rn0.60/android/app/src/main/java/com/rn060/MainActivity.java
+++ test/react-native/features/fixtures/rn0.60/android/app/src/main/java/com/rn060/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rn060;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
@@ -13,6 +14,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "reactnative";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Attempt to dismiss any system dialogs (such as "reactnative keeps stopping")
+        Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+        sendBroadcast(closeDialog);
     }
 
     @Override
