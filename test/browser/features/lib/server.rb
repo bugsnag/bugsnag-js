1b941a22610cc564c3569b09dc1b777fd7bf51b3
Alex Moinet <alex@bugsnag.com>
2022-01-26 18:15:29 +0000

    Add additional logging middleware to server

--- test/browser/features/lib/server.rb
+++ test/browser/features/lib/server.rb
@@ -1,5 +1,9 @@
 require 'webrick'
 
 root = File.expand_path File.join(__dir__, '..', 'fixtures')
-server = WEBrick::HTTPServer.new :Port => ENV['PORT'], :DocumentRoot => root
+request_callback = Proc.new { |req, res|
+  pp "INTERCEPTED REQUEST TO SERVER: #{req}"
+}
+
+server = WEBrick::HTTPServer.new :Port => ENV['PORT'], :DocumentRoot => root, :RequestCallback => request_callback
 server.start
