863545aea1ac765d1de3a4f29830dab7d654e9b9
Alex Moinet <alex@bugsnag.com>
2022-01-28 14:54:26 +0000

    Add more debugging

--- test/browser/features/lib/server.rb
+++ test/browser/features/lib/server.rb
@@ -3,6 +3,9 @@ require 'webrick'
 root = File.expand_path File.join(__dir__, '..', 'fixtures')
 request_callback = Proc.new { |req, res|
   pp "INTERCEPTED REQUEST TO SERVER: #{req}"
+  pp "BODY ---"
+  pp req.body
+  pp "BODY END ---"
 }
 
 server = WEBrick::HTTPServer.new :Port => ENV['PORT'], :DocumentRoot => root, :RequestCallback => request_callback
