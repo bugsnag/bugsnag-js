cc27e07d1d2c6d1a9bb40ee3276060cf80d90ae2
Ben Wilson <ben.wilson@smartbear.com>
2022-05-17 17:02:50 +0100

    clean up throttle test

--- packages/plugin-simple-throttle/test/throttle.test.ts
+++ packages/plugin-simple-throttle/test/throttle.test.ts
@@ -3,21 +3,19 @@ import plugin from '../'
 import Client from '@bugsnag/core/client'
 
 describe('plugin: throttle', () => {
-  describe('addOnError', () => {
-    const payloads = []
-    const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa' }, undefined, [plugin])
-    const mockWarn = jest.fn(message => message)
-    c._logger.warn = mockWarn
-    c._setDelivery(client => ({ sendEvent: (payload) => payloads.push(payload), sendSession: () => {} }))
+  const payloads = []
+  const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa' }, undefined, [plugin])
+  const mockWarn = jest.fn()
+  c._logger.warn = mockWarn
+  c._setDelivery(client => ({ sendEvent: (payload) => payloads.push(payload), sendSession: () => {} }))
 
-    it('prevents more than maxEvents being sent', () => {
-      for (let i = 0; i < 100; i++) c.notify(new Error('This is fail'))
-      expect(payloads.length).toBe(10)
-      expect(mockWarn).toHaveBeenCalledTimes(90)
-    })
+  it('prevents more than maxEvents being sent', () => {
+    for (let i = 0; i < 100; i++) c.notify(new Error('This is fail'))
+    expect(payloads.length).toBe(10)
+    expect(mockWarn).toHaveBeenCalledTimes(90)
+  })
 
-    it('throws an appropriate error message', () => {
-      expect(mockWarn.mock.results[11].value).toBe('Cancelling event send due to maxEvents per session limit of 10 being reached')
-    })
+  it('logs an appropriate error message', () => {
+    expect(mockWarn).toHaveBeenCalledWith('Cancelling event send due to maxEvents per session limit of 10 being reached')
   })
 })
