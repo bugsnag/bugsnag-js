b9f7de438208d17f93c8b96b6b9f1bff1623d43e
Ben Wilson <ben.wilson@smartbear.com>
2022-05-17 13:51:22 +0100

    add unit tests for node notifier

---
+++ packages/node/test/notifier.test.ts
@@ -0,0 +1,24 @@
+import Bugsnag from '..'
+
+describe('node notifier', () => {
+  beforeAll(() => {
+    jest.spyOn(console, 'debug').mockImplementation(() => {})
+    jest.spyOn(console, 'warn').mockImplementation(() => {})
+  })
+
+  beforeEach(() => {
+    // @ts-ignore:
+    Bugsnag._client = null
+  })
+
+  describe('isStarted', () => {
+    it('returns false when the notifier has not been initialised', () => {
+      expect(Bugsnag.isStarted).toBe(false)
+    })
+
+    it('returns true when the notifier has been initialised', () => {
+      Bugsnag.start('abcd12abcd12abcd12abcd12abcd12abcd')
+      expect(Bugsnag.isStarted).toBe(true)
+    })
+  })
+})
