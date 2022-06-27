534d324239d96b158457ed0e26711ec8e7bab633
sald19 <carl.sald19@gmail.com>
2022-06-27 15:43:07 -0600

    slint code, fix get component name when use script setup way

--- packages/plugin-vue/src/vue.js
+++ packages/plugin-vue/src/vue.js
@@ -1,28 +1,55 @@
 module.exports = (app, client) => {
-  const prev = app.config.errorHandler
+  const prev = app.config.errorHandler;
 
   const handler = (err, vm, info) => {
-    const handledState = { severity: 'error', unhandled: true, severityReason: { type: 'unhandledException' } }
-    const event = client.Event.create(err, true, handledState, 'vue error handler', 1)
+    const handledState = {
+      severity: "error",
+      unhandled: true,
+      severityReason: { type: "unhandledException" },
+    };
+    const event = client.Event.create(
+      err,
+      true,
+      handledState,
+      "vue error handler",
+      1
+    );
 
-    event.addMetadata('vue', {
+    event.addMetadata("vue", {
       errorInfo: ErrorTypeStrings[info] || info,
       component: vm ? formatComponentName(vm, true) : undefined,
-      props: (vm && vm.$options) ? vm.$options.propsData : undefined
-    })
+      props: vm && vm.$options ? vm.$options.propsData : undefined,
+    });
 
-    client._notify(event)
-    if (typeof console !== 'undefined' && typeof console.error === 'function') console.error(err)
+    client._notify(event);
+    if (typeof console !== "undefined" && typeof console.error === "function")
+      console.error(err);
 
-    if (typeof prev === 'function') prev.call(this, err, vm, info)
+    if (typeof prev === "function") prev.call(this, err, vm, info);
+  };
+
+  app.config.errorHandler = handler;
+};
+
+function formatComponentName(vm) {
+  if (vm.$parent === null) return "App";
+
+  if (!vm.$options) {
+    return "Anonymous";
   }
 
-  app.config.errorHandler = handler
-}
+  const file = vm.$options.__file;
+  const name = vm.$options.name;
 
-function formatComponentName (vm) {
-  if (vm.$parent === null) return 'App'
-  return (vm.$options && vm.$options.name) ? vm.$options.name : 'Anonymous'
+  if (!name && file) {
+    const match = file.match(/([^/\\]+)\.vue$/);
+
+    if (match) {
+      return match[1];
+    }
+  }
+
+  return name;
 }
 
 // We copy in the following data structures from Vue's source so we can map the "info" parameter in the errorhandler
@@ -30,20 +57,20 @@ function formatComponentName (vm) {
 
 // https://github.com/vuejs/vue-next/blob/d5cce47789db8f37b9f5f8ea6602ea63e3a04b07/packages/runtime-core/src/component.ts#L153-L167
 const LifecycleHooks = {
-  BEFORE_CREATE: 'bc',
-  CREATED: 'c',
-  BEFORE_MOUNT: 'bm',
-  MOUNTED: 'm',
-  BEFORE_UPDATE: 'bu',
-  UPDATED: 'u',
-  BEFORE_UNMOUNT: 'bum',
-  UNMOUNTED: 'um',
-  DEACTIVATED: 'da',
-  ACTIVATED: 'a',
-  RENDER_TRIGGERED: 'rtg',
-  RENDER_TRACKED: 'rtc',
-  ERROR_CAPTURED: 'ec'
-}
+  BEFORE_CREATE: "bc",
+  CREATED: "c",
+  BEFORE_MOUNT: "bm",
+  MOUNTED: "m",
+  BEFORE_UPDATE: "bu",
+  UPDATED: "u",
+  BEFORE_UNMOUNT: "bum",
+  UNMOUNTED: "um",
+  DEACTIVATED: "da",
+  ACTIVATED: "a",
+  RENDER_TRIGGERED: "rtg",
+  RENDER_TRACKED: "rtc",
+  ERROR_CAPTURED: "ec",
+};
 
 // https://github.com/vuejs/vue-next/blob/d5cce47789db8f37b9f5f8ea6602ea63e3a04b07/packages/runtime-core/src/errorHandling.ts#L6-L24
 const ErrorCodes = {
@@ -61,39 +88,39 @@ const ErrorCodes = {
   APP_WARN_HANDLER: 11,
   FUNCTION_REF: 12,
   ASYNC_COMPONENT_LOADER: 13,
-  SCHEDULER: 14
-}
+  SCHEDULER: 14,
+};
 
 // https://github.com/vuejs/vue-next/blob/d5cce47789db8f37b9f5f8ea6602ea63e3a04b07/packages/runtime-core/src/errorHandling.ts#L26-L57
 const ErrorTypeStrings = {
-  [LifecycleHooks.BEFORE_CREATE]: 'beforeCreate hook',
-  [LifecycleHooks.CREATED]: 'created hook',
-  [LifecycleHooks.BEFORE_MOUNT]: 'beforeMount hook',
-  [LifecycleHooks.MOUNTED]: 'mounted hook',
-  [LifecycleHooks.BEFORE_UPDATE]: 'beforeUpdate hook',
-  [LifecycleHooks.UPDATED]: 'updated',
-  [LifecycleHooks.BEFORE_UNMOUNT]: 'beforeUnmount hook',
-  [LifecycleHooks.UNMOUNTED]: 'unmounted hook',
-  [LifecycleHooks.ACTIVATED]: 'activated hook',
-  [LifecycleHooks.DEACTIVATED]: 'deactivated hook',
-  [LifecycleHooks.ERROR_CAPTURED]: 'errorCaptured hook',
-  [LifecycleHooks.RENDER_TRACKED]: 'renderTracked hook',
-  [LifecycleHooks.RENDER_TRIGGERED]: 'renderTriggered hook',
-  [ErrorCodes.SETUP_FUNCTION]: 'setup function',
-  [ErrorCodes.RENDER_FUNCTION]: 'render function',
-  [ErrorCodes.WATCH_GETTER]: 'watcher getter',
-  [ErrorCodes.WATCH_CALLBACK]: 'watcher callback',
-  [ErrorCodes.WATCH_CLEANUP]: 'watcher cleanup function',
-  [ErrorCodes.NATIVE_EVENT_HANDLER]: 'native event handler',
-  [ErrorCodes.COMPONENT_EVENT_HANDLER]: 'component event handler',
-  [ErrorCodes.VNODE_HOOK]: 'vnode hook',
-  [ErrorCodes.DIRECTIVE_HOOK]: 'directive hook',
-  [ErrorCodes.TRANSITION_HOOK]: 'transition hook',
-  [ErrorCodes.APP_ERROR_HANDLER]: 'app errorHandler',
-  [ErrorCodes.APP_WARN_HANDLER]: 'app warnHandler',
-  [ErrorCodes.FUNCTION_REF]: 'ref function',
-  [ErrorCodes.ASYNC_COMPONENT_LOADER]: 'async component loader',
+  [LifecycleHooks.BEFORE_CREATE]: "beforeCreate hook",
+  [LifecycleHooks.CREATED]: "created hook",
+  [LifecycleHooks.BEFORE_MOUNT]: "beforeMount hook",
+  [LifecycleHooks.MOUNTED]: "mounted hook",
+  [LifecycleHooks.BEFORE_UPDATE]: "beforeUpdate hook",
+  [LifecycleHooks.UPDATED]: "updated",
+  [LifecycleHooks.BEFORE_UNMOUNT]: "beforeUnmount hook",
+  [LifecycleHooks.UNMOUNTED]: "unmounted hook",
+  [LifecycleHooks.ACTIVATED]: "activated hook",
+  [LifecycleHooks.DEACTIVATED]: "deactivated hook",
+  [LifecycleHooks.ERROR_CAPTURED]: "errorCaptured hook",
+  [LifecycleHooks.RENDER_TRACKED]: "renderTracked hook",
+  [LifecycleHooks.RENDER_TRIGGERED]: "renderTriggered hook",
+  [ErrorCodes.SETUP_FUNCTION]: "setup function",
+  [ErrorCodes.RENDER_FUNCTION]: "render function",
+  [ErrorCodes.WATCH_GETTER]: "watcher getter",
+  [ErrorCodes.WATCH_CALLBACK]: "watcher callback",
+  [ErrorCodes.WATCH_CLEANUP]: "watcher cleanup function",
+  [ErrorCodes.NATIVE_EVENT_HANDLER]: "native event handler",
+  [ErrorCodes.COMPONENT_EVENT_HANDLER]: "component event handler",
+  [ErrorCodes.VNODE_HOOK]: "vnode hook",
+  [ErrorCodes.DIRECTIVE_HOOK]: "directive hook",
+  [ErrorCodes.TRANSITION_HOOK]: "transition hook",
+  [ErrorCodes.APP_ERROR_HANDLER]: "app errorHandler",
+  [ErrorCodes.APP_WARN_HANDLER]: "app warnHandler",
+  [ErrorCodes.FUNCTION_REF]: "ref function",
+  [ErrorCodes.ASYNC_COMPONENT_LOADER]: "async component loader",
   [ErrorCodes.SCHEDULER]:
-    'scheduler flush. This is likely a Vue internals bug. ' +
-    'Please open an issue at https://new-issue.vuejs.org/?repo=vuejs/vue-next'
-}
+    "scheduler flush. This is likely a Vue internals bug. " +
+    "Please open an issue at https://new-issue.vuejs.org/?repo=vuejs/vue-next",
+};
