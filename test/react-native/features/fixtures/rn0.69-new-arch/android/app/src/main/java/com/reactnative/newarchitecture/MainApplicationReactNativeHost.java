c51b5a3245f47946ef9a45efd484cfd9b4b2da04
Ben Wilson <ben.wilson@smartbear.com>
2022-09-05 15:03:50 +0100

    update test fixture

--- test/react-native/features/fixtures/rn0.69-new-arch/android/app/src/main/java/com/reactnative/newarchitecture/MainApplicationReactNativeHost.java
+++ test/react-native/features/fixtures/rn0.69-new-arch/android/app/src/main/java/com/reactnative/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.testfixture.newarchitecture;
+package com.reactnative.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.testfixture.BuildConfig;
-import com.testfixture.newarchitecture.components.MainComponentsRegistry;
-import com.testfixture.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.reactnative.BuildConfig;
+import com.reactnative.newarchitecture.components.MainComponentsRegistry;
+import com.reactnative.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
--- test/react-native/features/fixtures/rn0.69-new-arch/ios/reactnative/AppDelegate.mm
+++ test/react-native/features/fixtures/rn0.69-new-arch/ios/reactnative/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"TestFixture", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"reactnative", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
