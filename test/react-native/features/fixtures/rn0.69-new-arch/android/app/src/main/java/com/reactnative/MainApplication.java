c063a3123e77b35ab67923a929654f17f49763b6
Ben Wilson <ben.wilson@smartbear.com>
2022-09-05 16:03:45 +0100

    set up android fixture

--- test/react-native/features/fixtures/rn0.69-new-arch/android/app/src/main/java/com/reactnative/MainApplication.java
+++ test/react-native/features/fixtures/rn0.69-new-arch/android/app/src/main/java/com/reactnative/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.reactnative.module.BugsnagModulePackage;
 import com.reactnative.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new BugsnagModulePackage());
           return packages;
         }
 
