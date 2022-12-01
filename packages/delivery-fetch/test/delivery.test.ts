04eb0e1468ac5dfa0c8366bebbf862f68156e075
Ben Wilson <ben.wilson@smartbear.com>
2022-12-01 14:16:50 +0000

    test: :white_check_mark: add basic delivery test

--- packages/delivery-fetch/test/delivery.test.ts
+++ packages/delivery-fetch/test/delivery.test.ts
@@ -1,18 +1,37 @@
-// import delivery from '../dist/delivery'
-// import type { Client } from '@bugsnag/core'
-// import type { EventDeliveryPayload } from '@bugsnag/core/client'
+import delivery from '../src/delivery'
+import type { Client } from '@bugsnag/core'
 
-interface MockFetchRequest {
-  method: string | null
-  url: string | null
-  data: string | null
-  headers: { [key: string]: string }
-  readyState: string | null
-}
+// Prevent typescript from complaining about fetch not existing on global object
+const globalAny: any = global
 
 describe('delivery:fetch', () => {
   it('sends events successfully', done => {
-    expect(true).toBeTruthy()
-    done()
+    globalAny.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve()
+    }))
+
+    const config = {
+      apiKey: 'aaaaaaaa',
+      endpoints: { notify: '/echo/' },
+      redactedKeys: []
+    }
+
+    const payload = { sample: 'payload' }
+
+    delivery({ logger: {}, _config: config } as unknown as Client).sendEvent(payload, (err) => {
+      expect(err).toBe(null)
+      expect(globalAny.fetch).toHaveBeenCalled()
+      expect(globalAny.fetch).toHaveBeenCalledWith('/echo/', expect.objectContaining({
+        method: 'POST',
+        body: expect.stringMatching(JSON.stringify(payload)),
+        headers: expect.objectContaining({
+          'Bugsnag-Api-Key': 'aaaaaaaa',
+          'Bugsnag-Payload-Version': '4',
+          'Bugsnag-Sent-At': expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/),
+          'Content-Type': 'application/json'
+        })
+      }))
+      done()
+    })
   })
 })
