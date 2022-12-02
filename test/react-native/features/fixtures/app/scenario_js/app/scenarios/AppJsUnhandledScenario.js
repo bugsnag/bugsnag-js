d807b11c9b372d228c0eefaed07c8a02f04a4bba
Jason <jason.morris@bugsnag.com>
2022-12-02 15:40:39 +0000

    test(rn): change getPayloadInfo to use promises, but not `async` functions

--- test/react-native/features/fixtures/app/scenario_js/app/scenarios/AppJsUnhandledScenario.js
+++ test/react-native/features/fixtures/app/scenario_js/app/scenarios/AppJsUnhandledScenario.js
@@ -4,6 +4,7 @@ export class AppJsUnhandledScenario extends Scenario {
   constructor (configuration, jsConfig) {
     super()
     configuration.appVersion = '1.2.3'
+    configuration.enabledErrorTypes = { unhandledRejections: false }
     jsConfig.codeBundleId = '1.2.3-r00110011'
   }
 
--- test/react-native/features/fixtures/app/scenario_js/app/scenarios/DeviceJsUnhandledScenario.js
+++ test/react-native/features/fixtures/app/scenario_js/app/scenarios/DeviceJsUnhandledScenario.js
@@ -1,6 +1,11 @@
 import Scenario from './Scenario'
 
 export class DeviceJsUnhandledScenario extends Scenario {
+  constructor (configuration) {
+    super()
+    configuration.enabledErrorTypes = { unhandledRejections: false }
+  }
+
   run () {
     throw new Error('DeviceJsUnhandledScenario')
   }
