ba0685dc11165e88f4dc5bc0bf60fcdcfadb208c
Ben Wilson <ben.wilson@smartbear.com>
2022-12-07 17:02:12 +0000

    test prevent-discard functionality

--- packages/worker/src/prevent-discard.js
+++ packages/worker/src/prevent-discard.js
@@ -1,13 +1,13 @@
+const extensionRegex = /^(chrome|moz|safari|safari-web)-extension:/
+
 module.exports = {
   name: 'preventDiscard',
   load: client => {
     client.addOnError(event => {
-      event.errors[0].stacktrace = event.errors[0].stacktrace.map(function (frame) {
-        frame.file = frame.file.replace(/chrome-extension:/g, 'chrome_extension:')
-        frame.file = frame.file.replace(/moz-extension:/g, 'moz_extension:')
-        frame.file = frame.file.replace(/safari-extension:/g, 'safari_extension:')
-        frame.file = frame.file.replace(/safari-web-extension:/g, 'safari_web_extension:')
-        return frame
+      event.errors.forEach(({ stacktrace }) => {
+        stacktrace.forEach(function (frame) {
+          frame.file = frame.file.replace(extensionRegex, '$1_extension:')
+        })
       })
     }, true)
   }
--- packages/worker/test/notifier.test.ts
+++ packages/worker/test/notifier.test.ts
@@ -19,18 +19,18 @@ const testConfig = {
   redactedKeys: []
 }
 
+beforeAll(() => {
+  mockFetch()
+  typedGlobal.__VERSION__ = ''
+  jest.spyOn(console, 'debug').mockImplementation(() => {})
+  jest.spyOn(console, 'warn').mockImplementation(() => {})
+})
+
+beforeEach(() => {
+  jest.resetModules()
+})
+
 describe('worker notifier', () => {
-  beforeAll(() => {
-    mockFetch()
-    typedGlobal.__VERSION__ = ''
-    jest.spyOn(console, 'debug').mockImplementation(() => {})
-    jest.spyOn(console, 'warn').mockImplementation(() => {})
-  })
-
-  beforeEach(() => {
-    jest.resetModules()
-  })
-
   it('accepts plugins', () => {
     const Bugsnag = getBugsnag()
     Bugsnag.start({
@@ -81,5 +81,111 @@ describe('worker notifier', () => {
     expect(Bugsnag.isStarted()).toBe(false)
     Bugsnag.start(testConfig)
     expect(Bugsnag.isStarted()).toBe(true)
+  })
+})
+
+describe('prevent-discard', () => {
+  const browsers = ['chrome', 'moz', 'safari', 'safari-web']
+
+  it.each(browsers)('renames stacktrace.file on %s-extension exceptions', (type) => {
+    const Bugsnag = getBugsnag()
+    Bugsnag.start(testConfig)
+
+    const testError = new Error('123')
+    testError.stack = `Error: message from content\n    at ${type}-extension://notifier.test.ts:11:19`
+
+    Bugsnag.notify(testError, undefined, (err, event) => {
+      expect(err).toBeNull()
+      expect(event.errors[0].stacktrace[0].file).toStrictEqual(`${type}_extension://notifier.test.ts`)
+    })
+  })
+
+  it('renames stacktrace.file with multiple exceptions', (done) => {
+    const Bugsnag = getBugsnag()
+    Bugsnag.start(testConfig)
+
+    const err1 = new Error('123')
+    err1.stack = 'Error: message from content\n    at chrome-extension://notifier.test.ts:19:46'
+
+    // @ts-ignore
+    const err2 = new Error('456', { cause: err1 })
+    // @ts-ignore
+    err2.cause = err1
+    err2.stack = 'Error: 456\n    at generateErrors (chrome-extension://notifier.test.ts:14:16)\n    at chrome-extension://notifier.test.ts:19:46'
+
+    // @ts-ignore
+    const err3 = new Error('789')
+    // @ts-ignore
+    err3.cause = err2
+    err3.stack = 'Error: 789\n    at generateErrors (chrome-extension://notifier.test.ts:12:15)\n    at chrome-extension://notifier.test.ts:19:46'
+
+    Bugsnag.notify(err3, undefined, (err, event) => {
+      expect(err).toBeNull()
+      expect(event.errors).toHaveLength(3)
+      expect(event.errors).toStrictEqual([
+        {
+          errorClass: 'Error',
+          errorMessage: '789',
+          type: 'browserjs',
+          stacktrace: [
+            {
+              code: undefined,
+              inProject: undefined,
+              file: 'chrome_extension://notifier.test.ts',
+              method: 'generateErrors',
+              lineNumber: 12,
+              columnNumber: 15
+            },
+            {
+              code: undefined,
+              inProject: undefined,
+              file: 'chrome_extension://notifier.test.ts',
+              method: undefined,
+              lineNumber: 19,
+              columnNumber: 46
+            }
+          ]
+        },
+        {
+          errorClass: 'Error',
+          errorMessage: '456',
+          type: 'browserjs',
+          stacktrace: [
+            {
+              code: undefined,
+              inProject: undefined,
+              file: 'chrome_extension://notifier.test.ts',
+              method: 'generateErrors',
+              lineNumber: 14,
+              columnNumber: 16
+            },
+            {
+              code: undefined,
+              inProject: undefined,
+              file: 'chrome_extension://notifier.test.ts',
+              method: undefined,
+              lineNumber: 19,
+              columnNumber: 46
+            }
+          ]
+        },
+        {
+          errorClass: 'Error',
+          errorMessage: '123',
+          type: 'browserjs',
+          stacktrace: [
+            {
+              code: undefined,
+              columnNumber: 46,
+              file: 'chrome_extension://notifier.test.ts',
+              inProject: undefined,
+              lineNumber: 19,
+              method: undefined
+            }
+          ]
+        }
+      ])
+      done()
+    })
   })
 })
