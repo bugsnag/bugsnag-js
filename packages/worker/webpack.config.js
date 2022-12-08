d8a9d4ddb9ed89727aa6dfe1ab03630eef2571fb
Ben Wilson <ben.wilson@smartbear.com>
2022-12-08 11:13:39 +0000

    update webpack to support earlier browser versions

--- packages/worker/webpack.config.js
+++ packages/worker/webpack.config.js
@@ -24,6 +24,16 @@ module.exports = {
       {
         test: /\.ts/,
         loader: 'ts-loader'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
