b10e54cbc829b6faccd939ec0e73ebf23c264d7c
Ben Wilson <ben.wilson@smartbear.com>
2023-01-17 10:20:51 +0000

    chore: :label: Update delivery type declaration

--- packages/delivery-fetch/delivery.d.ts
+++ packages/delivery-fetch/delivery.d.ts
@@ -1,6 +1,6 @@
 import type { Client } from '@bugsnag/core'
 import type { Delivery } from '@bugsnag/core/client'
 
-declare const delivery: (client: Client) => Delivery
+declare const delivery: (client: Client, fetch?: GlobalFetch['fetch']) => Delivery
 
 export default delivery
