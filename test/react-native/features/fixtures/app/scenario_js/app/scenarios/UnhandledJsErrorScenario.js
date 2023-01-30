2b955d5ee22ae5481a090beed2773e24e9b6b2cc
Alex Moinet <alex@bugsnag.com>
2023-01-30 15:38:59 +0000

    Remove logs from Unhandled JS scenario

--- test/react-native/features/fixtures/app/scenario_js/app/scenarios/UnhandledJsErrorScenario.js
+++ test/react-native/features/fixtures/app/scenario_js/app/scenarios/UnhandledJsErrorScenario.js
@@ -1,17 +1,7 @@
 import Scenario from './Scenario'
 
 export class UnhandledJsErrorScenario extends Scenario {
-  constructor (configuration, jsConfig) {
-    super()
-    function log (...msg) {
-      console.log(...msg)
-    }
-    configuration.logger = { debug: log, info: log, warn: log, error: log }
-    console.log('I have created the UnhandledJsErrorScenario ---------------------------------------------------------')
-  }
-
   run () {
-    console.log('UnhandledJsErrorScenario is about to crash! ---------------------------------------------------------')
     throw new Error('UnhandledJsErrorScenario')
   }
 }
