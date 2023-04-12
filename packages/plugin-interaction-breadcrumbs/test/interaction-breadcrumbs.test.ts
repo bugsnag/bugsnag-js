d2365df81691dab689ac471b00db32d34dde8e0d
Joe Haines <hello@joehaines.co.uk>
2023-04-12 10:46:30 +0100

    Add more tests

--- packages/plugin-interaction-breadcrumbs/test/interaction-breadcrumbs.test.ts
+++ packages/plugin-interaction-breadcrumbs/test/interaction-breadcrumbs.test.ts
@@ -1,6 +1,7 @@
 import plugin from '../'
 
 import Client from '@bugsnag/core/client'
+import Breadcrumb from '@bugsnag/core/breadcrumb'
 
 describe('plugin: interaction breadcrumbs', () => {
   beforeEach(() => {
@@ -41,5 +42,144 @@ describe('plugin: interaction breadcrumbs', () => {
     document.querySelector('button')!.click()
 
     expect(c._breadcrumbs).toHaveLength(1)
+  })
+
+  it("includes the target's text and selector", () => {
+    const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: null, plugins: [plugin(window)] })
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toStrictEqual([
+      new Breadcrumb(
+        'UI click',
+        { targetText: 'Click me', targetSelector: 'BUTTON.button' },
+        'user',
+        expect.any(Date)
+      )
+    ])
+  })
+
+  it('strips leading and trailing whitespace in target text', () => {
+    document.body.innerHTML = `
+      <button>
+
+        hello there
+
+      </button>
+    `
+
+    const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: null, plugins: [plugin(window)] })
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toStrictEqual([
+      new Breadcrumb(
+        'UI click',
+        { targetText: 'hello there', targetSelector: 'BUTTON' },
+        'user',
+        expect.any(Date)
+      )
+    ])
+  })
+
+  it('includes 140 characters of text', () => {
+    document.body.innerHTML = `
+      <button>
+
+        ${'a'.repeat(140)}
+
+      </button>
+    `
+
+    const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: null, plugins: [plugin(window)] })
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toStrictEqual([
+      new Breadcrumb(
+        'UI click',
+        { targetText: 'a'.repeat(140), targetSelector: 'BUTTON' },
+        'user',
+        expect.any(Date)
+      )
+    ])
+  })
+
+  it('truncates to 135 characters of text + "(...)" when text is too long', () => {
+    document.body.innerHTML = `
+      <button>
+
+        ${'a'.repeat(141)}
+
+      </button>
+    `
+
+    const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: null, plugins: [plugin(window)] })
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toStrictEqual([
+      new Breadcrumb(
+        'UI click',
+        { targetText: 'a'.repeat(135) + '(...)', targetSelector: 'BUTTON' },
+        'user',
+        expect.any(Date)
+      )
+    ])
+  })
+
+  it("doesn't strip whitespace between words", () => {
+    document.body.innerHTML = `
+      <button>
+
+        a          b          c
+
+      </button>
+    `
+
+    const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: null, plugins: [plugin(window)] })
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toStrictEqual([
+      new Breadcrumb(
+        'UI click',
+        { targetText: 'a          b          c', targetSelector: 'BUTTON' },
+        'user',
+        expect.any(Date)
+      )
+    ])
+  })
+
+  it("doesn't strip trailing whitespace after the character limit", () => {
+    document.body.innerHTML = `
+      <button>
+
+        a          b ${' '.repeat(200)} c
+
+      </button>
+    `
+
+    const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: null, plugins: [plugin(window)] })
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toStrictEqual([
+      new Breadcrumb(
+        'UI click',
+        {
+          targetText: 'a          b                                                                                                                           (...)',
+          targetSelector: 'BUTTON'
+        },
+        'user',
+        expect.any(Date)
+      )
+    ])
   })
 })
