8d6218daaa6fb5d47c3ed7d1414cd9ba5dba3846
Joe Haines <hello@joehaines.co.uk>
2023-04-12 09:40:37 +0100

    Rewrite tests to take advantage of jsdom

--- packages/plugin-interaction-breadcrumbs/test/interaction-breadcrumbs.test.ts
+++ packages/plugin-interaction-breadcrumbs/test/interaction-breadcrumbs.test.ts
@@ -3,78 +3,43 @@ import plugin from '../'
 import Client from '@bugsnag/core/client'
 
 describe('plugin: interaction breadcrumbs', () => {
-  it('should drop a breadcrumb when an element is clicked', () => {
-    const { window, winHandlers, els } = getMockWindow()
+  beforeEach(() => {
+    document.body.innerHTML = '<div><button class="button">Click me</button></div>'
+  })
+
+  it('should be enabled by default', () => {
     const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', plugins: [plugin(window)] })
-    winHandlers.click.forEach(fn => fn.call(window, { target: els[0] }))
-    expect(c._breadcrumbs.length).toBe(1)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toHaveLength(1)
   })
 
   it('should not be enabled when enabledBreadcrumbTypes=[]', () => {
-    const { window, winHandlers, els } = getMockWindow()
     const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: [], plugins: [plugin(window)] })
-    winHandlers.click.forEach(fn => fn.call(window, { target: els[0] }))
-    expect(c._breadcrumbs.length).toBe(0)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toHaveLength(0)
   })
 
   it('should be enabled when enabledBreadcrumbTypes=["user"]', () => {
-    const { window, winHandlers, els } = getMockWindow()
     const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: ['user'], plugins: [plugin(window)] })
-    winHandlers.click.forEach(fn => fn.call(window, { target: els[0] }))
-    expect(c._breadcrumbs.length).toBe(1)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toHaveLength(1)
   })
 
   it('should be enabled when enabledBreadcrumbTypes=null', () => {
-    const { window, winHandlers, els } = getMockWindow()
     const c = new Client({ apiKey: 'aaaa-aaaa-aaaa-aaaa', enabledBreadcrumbTypes: null, plugins: [plugin(window)] })
-    winHandlers.click.forEach(fn => fn.call(window, { target: els[0] }))
-    expect(c._breadcrumbs.length).toBe(1)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.querySelector('button')!.click()
+
+    expect(c._breadcrumbs).toHaveLength(1)
   })
 })
-
-const getMockWindow = () => {
-  const els = [
-    {
-      tagName: 'BUTTON',
-      className: 'button',
-      textContent: 'Click me',
-      parentNode: null
-    },
-    {
-      tagName: 'BUTTON',
-      className: 'button',
-      textContent: 'or me',
-      parentNode: null
-    }
-  ]
-
-  const parent = {
-    tagName: 'DIV',
-    id: 'buttons',
-    childNodes: els,
-    className: '',
-    parentNode: null
-  } as any
-
-  parent.parentNode = { childNodes: [parent] }
-  els.forEach(el => { el.parentNode = parent })
-
-  const winHandlers: { [key: string]: Array<(event: any) => void> } = { click: [] }
-  const window = {
-    addEventListener: function (evt: string, handler: (event: any) => void) {
-      winHandlers[evt] = winHandlers[evt] ? winHandlers[evt].concat(handler) : [handler]
-    },
-    document: {
-      querySelectorAll: function (query: string) {
-        switch (query) {
-          case 'BUTTON.button': return els
-          case 'BUTTON.button:nth-child(1)': return [els[0]]
-          case 'DIV#buttons': return [parent]
-          default: return []
-        }
-      }
-    }
-  } as unknown as Window & typeof globalThis
-
-  return { els, window, winHandlers }
-}
