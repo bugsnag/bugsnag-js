992ced337915b7cbc9c96b9ebc1d3315dd6fff9a
Alex Moinet <alex@bugsnag.com>
2023-04-27 16:01:29 +0100

    Slight refactor for testing

--- test/react-native/features/fixtures/app/scenario_js/app/App.js
+++ test/react-native/features/fixtures/app/scenario_js/app/App.js
@@ -19,8 +19,8 @@ export default class App extends Component {
       currentScenario: '',
       scenarioMetaData: '',
       apiKey: '12312312312312312312312312312312',
-      notifyEndpoint: 'http://bs-local.com:9339/notify',
-      sessionsEndpoint: 'http://bs-local.com:9339/sessions'
+      notifyEndpoint: 'http://' + address + '/notify',
+      sessionsEndpoint: 'http://' + address + '/sessions'
     }
   }
 
