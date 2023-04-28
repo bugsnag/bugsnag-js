a16e8f23df2aa06a265893cd90e891621d794405
Alex Moinet <alex@bugsnag.com>
2023-04-28 09:35:18 +0100

    Work around missing references

--- test/react-native/features/fixtures/reactnative/module/ConfigFileReader.kt
+++ test/react-native/features/fixtures/reactnative/module/ConfigFileReader.kt
@@ -5,13 +5,15 @@ import org.json.JSONObject
 import java.io.File
 import java.io.IOException
 
+const val CONFIG_FILE_TIMEOUT = 5000
+
 class ConfigFileReader {
 
     fun getMazeRunnerAddress(context: Context): String {
         val externalFilesDir = context.getExternalFilesDir(null)
         val configFile = File(externalFilesDir, "fixture_config.json")
         var mazeAddress: String? = null
-        log("Attempting to read Maze Runner address from config file ${configFile.path}")
+        Log.i("Bugsnag", "Attempting to read Maze Runner address from config file ${configFile.path}")
 
         // Poll for the fixture config file
         val pollEnd = System.currentTimeMillis() + CONFIG_FILE_TIMEOUT
@@ -21,7 +23,7 @@ class ConfigFileReader {
                 val fixtureConfig = runCatching { JSONObject(fileContents) }.getOrNull()
                 mazeAddress = getStringSafely(fixtureConfig, "maze_address")
                 if (!mazeAddress.isNullOrBlank()) {
-                    log("Maze Runner address set from config file: $mazeAddress")
+                    Log.i("Bugsnag", "Maze Runner address set from config file: $mazeAddress")
                     break
                 }
             }
@@ -29,10 +31,14 @@ class ConfigFileReader {
             Thread.sleep(250)
         }
         if (mazeAddress.isNullOrBlank()) {
-            log("Failed to read Maze Runner address from config file, reverting to legacy address")
+            Log.i("Bugsnag", "Failed to read Maze Runner address from config file, reverting to legacy address")
             mazeAddress = "bs-local.com:9339"
         }
         return mazeAddress
+    }
+
+    private fun getStringSafely(jsonObject: JSONObject?, key: String): String {
+        return jsonObject?.optString(key) ?: ""
     }
 
 }
