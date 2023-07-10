27c0d4391918fcbd09fb0514811af3e43491649e
Josh Edney <josh.edney@smartbear.com>
2023-07-10 17:40:02 +0100

    remove pearl

--- scripts/react-native-cli-helper.js
+++ scripts/react-native-cli-helper.js
@@ -35,10 +35,10 @@ module.exports = {
       common.run(initCommand, true)
 
       // Use Perl to replace the Bugsnag start command to use a loaded configuration
-      const applicationPath = `android/app/src/main/java/com/${rnVersion}/`
-      common.changeDir(`${destFixtures}/${rnVersion}/${applicationPath}`)
-      const perlCommand = 'perl -pi -e "s/Bugsnag.start\\(this\\);/Bugsnag.start\\(this, createConfiguration\\(\\)\\);/g" MainApplication.java'
-      common.run(perlCommand, true)
+      // const applicationPath = `android/app/src/main/java/com/${rnVersion}/`
+      // common.changeDir(`${destFixtures}/${rnVersion}/${applicationPath}`)
+      // const perlCommand = 'perl -pi -e "s/Bugsnag.start\\(this\\);/Bugsnag.start\\(this, createConfiguration\\(\\)\\);/g" MainApplication.java'
+      // common.run(perlCommand, true)
 
       // Native layer
       common.changeDir(`${destFixtures}/${rnVersion}/android`)
