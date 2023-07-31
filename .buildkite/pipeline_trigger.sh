22ad9f600c3c8d896d08ff75f4e572009c34cd74
Yousif Ahmed <yousif@bugsnag.com>
2023-07-31 12:38:35 +0100

    split react native and CLI pipeline into basic and full stages

---
+++ .buildkite/pipeline_trigger.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+if [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
+  "$BUILDKITE_BRANCH" == "next" ||
+  "$BUILDKITE_BRANCH" == "main" ||
+  "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ]]; then
+  echo "Running full build"
+  buildkite-agent pipeline upload .buildkite/pipeline.full.yml
+else
+  # Basic build, but allow a full build to be triggered
+  echo "Running basic build"
+  buildkite-agent pipeline upload .buildkite/block.full.yml
+fi
