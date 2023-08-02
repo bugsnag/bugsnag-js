ad648d73fddf54b6ea5cbc4a006a617a22e86448
Yousif Ahmed <yousif@bugsnag.com>
2023-08-02 13:27:49 +0100

    Split pipeline files into full and basic subdirectories

--- .buildkite/pipeline_trigger.sh
+++ .buildkite/pipeline_trigger.sh
@@ -5,9 +5,9 @@ if [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
   "$BUILDKITE_BRANCH" == "main" ||
   "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ]]; then
   echo "Running full build"
-  buildkite-agent pipeline upload .buildkite/pipeline.full.yml
+  buildkite-agent pipeline upload .buildkite/full/pipeline.full.yml
 else
   # Basic build, but allow a full build to be triggered
   echo "Running basic build"
-  buildkite-agent pipeline upload .buildkite/block.full.yml
+  buildkite-agent pipeline upload .buildkite/full/block.yml
 fi
