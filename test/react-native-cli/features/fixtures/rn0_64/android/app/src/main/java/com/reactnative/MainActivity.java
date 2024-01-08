9dda8399a84998db113d6bdb53c8745f4f95721c
Steve Kirkland-Walton <steve@bugsnag.com>
2024-01-08 16:02:15 +0000

    Refactor all fixture packages to use com.reactnative [full ci]

---
+++ test/react-native-cli/features/fixtures/rn0_64/android/app/src/main/java/com/reactnative/MainActivity.java
@@ -0,0 +1,34 @@
+package com.reactnative;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "rn0_64";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Attempt to dismiss any system dialogs (such as "reactnative keeps stopping")
+    Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+    sendBroadcast(closeDialog);
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle SavedInstanceState) {
+    // Do not write any state, to avoid crashes on relaunch after a crash.  If Android keeps the state Bundle from
+    // before the crash, passing it back into the application it can do so with a Drawable that does not implement
+    // the getConstantState method, causing another crash.
+  }
+}
---
+++ test/react-native-cli/features/fixtures/rn0_64/android/app/src/main/java/com/reactnative/MainApplication.java
@@ -0,0 +1,63 @@
+package com.reactnative;
+
+import com.bugsnag.android.Configuration;
+import com.bugsnag.android.EndpointConfiguration;
+import android.app.Application;
+import android.content.Context;
+import java.util.Map;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import android.util.Log;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          packages.add(new CrashyPackage());
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+
+  private Configuration createConfiguration() {
+    TestUtils testUtils = new TestUtils();
+    Map<String, String> defaultParams = testUtils.loadDefaultParams(this);
+    Configuration config = new Configuration(defaultParams.get("apiKey"));
+    String mazeAddress = testUtils.getMazeRunnerAddress(this);
+    String notifyEndpoint = "http://" + mazeAddress + "/notify";
+    String sessionEndpoint = "http://" + mazeAddress + "/sessions";
+    config.setEndpoints(new EndpointConfiguration(notifyEndpoint, sessionEndpoint));
+    config.addMetadata("default", defaultParams);
+    return config;
+  }
+}
