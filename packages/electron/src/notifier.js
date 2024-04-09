7c9c81e66ec0d2351eb1412c52e28d812c302962
Ben Wilson <ben.wilson@smartbear.com>
2024-04-09 10:08:02 +0100

    fix imports

--- packages/electron/src/notifier.js
+++ packages/electron/src/notifier.js
@@ -1,6 +1,6 @@
 const isMain = typeof process !== 'undefined' && process.type === 'browser'
 
-const Bugsnag = isMain ? require('./main') : require('./renderer')
+const Bugsnag = isMain ? require('./client/main') : require('./client/renderer')
 
 // commonjs
 module.exports = Bugsnag
