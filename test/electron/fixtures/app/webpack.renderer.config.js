be604e74001cb8975f32325dc6d2c19a0e9096b9
Ben Wilson <ben.wilson@smartbear.com>
2024-04-24 11:54:02 +0100

    test: :white_check_mark: update event assertions

--- test/electron/fixtures/app/webpack.renderer.config.js
+++ test/electron/fixtures/app/webpack.renderer.config.js
@@ -1,10 +1,3 @@
-const { readdirSync } = require('fs')
-const { parse } = require('path')
-const entrypoints = {}
-readdirSync('./src/preloads').forEach(name => {
-  entrypoints[parse(name).name] = `./src/preloads/${name}`
-})
-
 module.exports = {
   resolve: {
     fallback: {
