af1cf046168cd5604364c5f6277121e0e74528ba
Yousif Ahmed <yousif@bugsnag.com>
2024-05-16 16:36:30 +0100

    fix: add discard pattern for TM exceptions

--- packages/react-native/ios/BugsnagReactNative/BugsnagReactNativePlugin.m
+++ packages/react-native/ios/BugsnagReactNative/BugsnagReactNativePlugin.m
@@ -25,9 +25,11 @@
     // The exception name gets recorded as the error's `errorClass`.
     //
     NSString *discardPattern = @"^RCTFatalException: Unhandled JS Exception: ";
+    NSString *TMDiscardPattern = @"^facebook::jsi::Error";
     
     NSMutableSet *discardClasses = [client.configuration.discardClasses mutableCopy] ?: [NSMutableSet set];
     [discardClasses addObject:[NSRegularExpression regularExpressionWithPattern:discardPattern options:0 error:nil]];
+    [discardClasses addObject:[NSRegularExpression regularExpressionWithPattern:TMDiscardPattern options:0 error:nil]];
     client.configuration.discardClasses = discardClasses;
 }
 
