103674e7973ff804c75d7feee4644a1d6ba5e40c
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2024-09-20 17:01:56 +0200

    changed startsWith to substring

--- packages/delivery-xml-http-request/delivery.js
+++ packages/delivery-xml-http-request/delivery.js
@@ -28,7 +28,7 @@ module.exports = (client, win = window) => ({
       req.setRequestHeader('Bugsnag-Api-Key', event.apiKey || client._config.apiKey)
       req.setRequestHeader('Bugsnag-Payload-Version', '4')
       req.setRequestHeader('Bugsnag-Sent-At', (new Date()).toISOString())
-      if (url.startsWith('https')) {
+      if (url.substring(0, 5) === 'https') {
         req.setRequestHeader('Access-Control-Max-Age', 86400)
       }
       req.send(body)
