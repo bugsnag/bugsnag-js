c81a572f9c90704f25ec77d31f36bdf56e83d0a1
Dan Skinner <dan.skinner@smartbear.com>
2024-10-16 10:16:45 +0100

    set Bugsnag-Integrity header in delivery-xml-http-request

--- packages/delivery-xml-http-request/delivery.js
+++ packages/delivery-xml-http-request/delivery.js
@@ -1,5 +1,18 @@
 const payload = require('@bugsnag/core/lib/json-payload')
 
+async function getIntegrity (windowOrWorkerGlobalScope, requestBody) {
+  if (windowOrWorkerGlobalScope.isSecureContext && windowOrWorkerGlobalScope.crypto && windowOrWorkerGlobalScope.crypto.subtle && windowOrWorkerGlobalScope.crypto.subtle.digest && typeof TextEncoder === 'function') {
+    const msgUint8 = new TextEncoder().encode(requestBody)
+    const hashBuffer = await windowOrWorkerGlobalScope.crypto.subtle.digest('SHA-1', msgUint8)
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('')
+
+    return 'sha1 ' + hashHex
+  }
+}
+
 module.exports = (client, win = window) => ({
   sendEvent: (event, cb = () => {}) => {
     try {
@@ -11,31 +24,39 @@ module.exports = (client, win = window) => ({
       const req = new win.XMLHttpRequest()
       const body = payload.event(event, client._config.redactedKeys)
 
-      req.onreadystatechange = function () {
-        if (req.readyState === win.XMLHttpRequest.DONE) {
-          const status = req.status
-          if (status === 0 || status >= 400) {
-            const err = new Error(`Request failed with status ${status}`)
-            client._logger.error('Event failed to send…', err)
-            if (body.length > 10e5) {
-              client._logger.warn(`Event oversized (${(body.length / 10e5).toFixed(2)} MB)`)
+      getIntegrity(win, body).then((integrity) => {
+        req.onreadystatechange = function () {
+          if (req.readyState === win.XMLHttpRequest.DONE) {
+            const status = req.status
+            if (status === 0 || status >= 400) {
+              const err = new Error(`Request failed with status ${status}`)
+              client._logger.error('Event failed to send…', err)
+              if (body.length > 10e5) {
+                client._logger.warn(`Event oversized (${(body.length / 10e5).toFixed(2)} MB)`)
+              }
+              cb(err)
+            } else {
+              cb(null)
             }
-            cb(err)
-          } else {
-            cb(null)
           }
         }
-      }
+        req.open('POST', url)
+        req.setRequestHeader('Content-Type', 'application/json')
+        req.setRequestHeader('Bugsnag-Api-Key', event.apiKey || client._config.apiKey)
+        req.setRequestHeader('Bugsnag-Payload-Version', '4')
+        req.setRequestHeader('Bugsnag-Sent-At', (new Date()).toISOString())
+        if (url.substring(0, 5) === 'https') {
+          req.setRequestHeader('Access-Control-Max-Age', 86400)
+        }
+        if (integrity) {
+          req.setRequestHeader('Bugsnag-Integrity', integrity)
+        }
 
-      req.open('POST', url)
-      req.setRequestHeader('Content-Type', 'application/json')
-      req.setRequestHeader('Bugsnag-Api-Key', event.apiKey || client._config.apiKey)
-      req.setRequestHeader('Bugsnag-Payload-Version', '4')
-      req.setRequestHeader('Bugsnag-Sent-At', (new Date()).toISOString())
-      if (url.substring(0, 5) === 'https') {
-        req.setRequestHeader('Access-Control-Max-Age', 86400)
-      }
-      req.send(body)
+        req.send(body)
+      }).catch((err) => {
+        client._logger.error(err)
+        cb(err)
+      })
     } catch (e) {
       client._logger.error(e)
     }
