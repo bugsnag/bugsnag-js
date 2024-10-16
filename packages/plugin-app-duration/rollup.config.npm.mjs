5f9f19743fa8cc1c8326f9eb25da48e632b1ea71
Ben Wilson <ben.wilson@smartbear.com>
2024-10-16 14:45:11 +0100

    refactor: :recycle: simplify rollup config

--- packages/plugin-app-duration/rollup.config.npm.mjs
+++ packages/plugin-app-duration/rollup.config.npm.mjs
@@ -1,25 +1,5 @@
 import createRollupConfig from '../../.rollup/index.mjs'
 
 export default createRollupConfig({
-    input: 'src/app-duration.ts',
-    output: [
-        {
-            dir: `dist`,
-            entryFileNames: '[name].js',
-            format: 'cjs',
-            preserveModules: true,
-            generatedCode: {
-              preset: 'es2015',
-            }
-        },
-        {
-            dir: `dist`,
-            entryFileNames: '[name].mjs',
-            format: 'esm',
-            preserveModules: true,
-            generatedCode: {
-                preset: 'es2015',
-            }
-        }
-    ]
+    input: 'src/app-duration.ts'
 })
