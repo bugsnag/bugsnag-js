9de0065bc6acbb5344ac949dd98237cff4a4960d
Dan Skinner <dan.skinner@smartbear.com>
2024-10-17 09:04:04 +0100

    handle no promises in ie11

--- packages/delivery-xml-http-request/delivery.js
+++ packages/delivery-xml-http-request/delivery.js
@@ -27,39 +27,44 @@ module.exports = (client, win = window) => ({
       const req = new win.XMLHttpRequest()
       const body = payload.event(event, client._config.redactedKeys)
 
-      getIntegrity(win, body).then((integrity) => {
-        req.onreadystatechange = function () {
-          if (req.readyState === win.XMLHttpRequest.DONE) {
-            const status = req.status
-            if (status === 0 || status >= 400) {
-              const err = new Error(`Request failed with status ${status}`)
-              client._logger.error('Event failed to send…', err)
-              if (body.length > 10e5) {
-                client._logger.warn(`Event oversized (${(body.length / 10e5).toFixed(2)} MB)`)
-              }
-              cb(err)
-            } else {
-              cb(null)
+      req.onreadystatechange = function () {
+        if (req.readyState === win.XMLHttpRequest.DONE) {
+          const status = req.status
+          if (status === 0 || status >= 400) {
+            const err = new Error(`Request failed with status ${status}`)
+            client._logger.error('Event failed to send…', err)
+            if (body.length > 10e5) {
+              client._logger.warn(`Event oversized (${(body.length / 10e5).toFixed(2)} MB)`)
             }
+            cb(err)
+          } else {
+            cb(null)
           }
         }
-        req.open('POST', url)
-        req.setRequestHeader('Content-Type', 'application/json')
-        req.setRequestHeader('Bugsnag-Api-Key', event.apiKey || client._config.apiKey)
-        req.setRequestHeader('Bugsnag-Payload-Version', '4')
-        req.setRequestHeader('Bugsnag-Sent-At', (new Date()).toISOString())
-        if (url.substring(0, 5) === 'https') {
-          req.setRequestHeader('Access-Control-Max-Age', 86400)
-        }
-        if (integrity) {
-          req.setRequestHeader('Bugsnag-Integrity', integrity)
-        }
+      }
 
+      req.open('POST', url)
+      req.setRequestHeader('Content-Type', 'application/json')
+      req.setRequestHeader('Bugsnag-Api-Key', event.apiKey || client._config.apiKey)
+      req.setRequestHeader('Bugsnag-Payload-Version', '4')
+      req.setRequestHeader('Bugsnag-Sent-At', (new Date()).toISOString())
+      if (url.substring(0, 5) === 'https') {
+        req.setRequestHeader('Access-Control-Max-Age', 86400)
+      }
+
+      if (typeof Promise !== 'undefined') {
+        getIntegrity(win, body).then((integrity) => {
+          if (integrity) {
+            req.setRequestHeader('Bugsnag-Integrity', integrity)
+          }
+        }).catch((err) => {
+          client._logger.error(err)
+        }).finally(() => {
+          req.send(body)
+        })
+      } else {
         req.send(body)
-      }).catch((err) => {
-        client._logger.error(err)
-        cb(err)
-      })
+      }
     } catch (e) {
       client._logger.error(e)
     }
