1ee928f6661d61e825966838da57ab25aa974a35
Ben Wilson <ben.wilson@smartbear.com>
2024-10-17 17:35:21 +0100

    revert: :rewind: Revert PLAT-12111 (#2229)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -37,10 +37,6 @@ As well as some bug fixes and **breaking changes** described in the [Upgrade Gui
 
 ## [7.25.1] - 2024-08-27
 
-### Added
-
-- (delivery-xml-http-request) Add Access-Control-Max-Age header to CORS preflight responses [#2160](https://github.com/bugsnag/bugsnag-js/pull/2160)
-
 ### Changed
 
 - (react-native) Update bugsnag-cocoa from v6.29.0 to [v6.30.1](https://github.com/bugsnag/bugsnag-cocoa/blob/master/CHANGELOG.md#6301-2024-07-25)
--- packages/delivery-xml-http-request/delivery.js
+++ packages/delivery-xml-http-request/delivery.js
@@ -32,9 +32,6 @@ module.exports = (client, win = window) => ({
       req.setRequestHeader('Bugsnag-Api-Key', event.apiKey || client._config.apiKey)
       req.setRequestHeader('Bugsnag-Payload-Version', '4')
       req.setRequestHeader('Bugsnag-Sent-At', (new Date()).toISOString())
-      if (url.substring(0, 5) === 'https') {
-        req.setRequestHeader('Access-Control-Max-Age', 86400)
-      }
       req.send(body)
     } catch (e) {
       client._logger.error(e)
