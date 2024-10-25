4a104b18a67aa98fcc6beab9cb4391360bf9f7bc
hannah-smartbear <hannah.syed@smartbear.com>
2024-10-25 10:26:09 +0100

    updating hono app.js in  test fixtures

--- test/node/features/fixtures/hono/scenarios/app.js
+++ test/node/features/fixtures/hono/scenarios/app.js
@@ -1,10 +1,15 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import Bugsnag from '@bugsnag/js';
-// import { createServer } from 'node:http2'
-// Bugsnag.start({
-//     apiKey: '93ddd9eb8ccf8706fd18fe9d74a2dcd8',
-// });
+
+Bugsnag.start({
+    apiKey: process.env.BUGSNAG_API_KEY,
+    endpoints: {
+      notify: process.env.BUGSNAG_NOTIFY_ENDPOINT,
+      sessions: process.env.BUGSNAG_SESSIONS_ENDPOINT
+    }
+})
+
 const app = new Hono();
 app.get('/', (c) => {
     Bugsnag.notify(new Error('unresolveable musical differences'));
