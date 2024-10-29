258c27e78dbb279fc993dd8bf75e8f14854ecabb
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2024-10-29 17:25:53 +0100

    fix: types

--- packages/plugin-network-breadcrumbs/rollup.config.npm.mjs
+++ packages/plugin-network-breadcrumbs/rollup.config.npm.mjs
@@ -1,6 +1,6 @@
 import createRollupConfig from "../../.rollup/index.mjs";
 
 export default createRollupConfig({
-  input: "src/console-breadcrumbs.ts",
+  input: "src/network-breadcrumbs.ts",
   external: ["@bugsnag/core/lib/es-utils/includes"]
 });
