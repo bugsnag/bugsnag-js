7f8b24f799f2e20a7de0ba367409c6c0b13bca9a
Ben Wilson <ben.wilson@smartbear.com>
2024-10-29 09:59:30 +0000

    chore: :label: fix rollup build by updating types

--- packages/plugin-console-breadcrumbs/rollup.config.npm.mjs
+++ packages/plugin-console-breadcrumbs/rollup.config.npm.mjs
@@ -1,5 +1,6 @@
 import createRollupConfig from "../../.rollup/index.mjs";
 
 export default createRollupConfig({
-  input: "src/console-breadcrumbs.ts"
+  input: "src/console-breadcrumbs.ts",
+  external: ["@bugsnag/core/lib/es-utils/filter", "@bugsnag/core/lib/es-utils/map", "@bugsnag/core/lib/es-utils/reduce"]
 });
