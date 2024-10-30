13aaae99edbd5f355e09de02774eed2a1558ec31
Ben Wilson <ben.wilson@smartbear.com>
2024-10-30 11:56:57 +0000

    chore: :label: fix types and remove usage of ts-expect-error

--- packages/core/lib/es-utils/reduce.d.ts
+++ packages/core/lib/es-utils/reduce.d.ts
@@ -1,1 +1,1 @@
-export default function reduce<T, U>(arr: T[], fn: (accum: any, arg: any, item: T) => any, accum: any): any
+export default function reduce<T, U>(arr: T[], fn: (accum: any, item: T, index: number, arr: T[]) => any, accum: any): any
