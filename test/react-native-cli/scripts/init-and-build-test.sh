63e09cc01dc435b85f14f2792a18b7c621106916
Josh <46817760+joshedney@users.noreply.github.com>
2024-11-01 14:31:01 +0000

    [PLAT-12987] FIx React Native CLI iOS test (#2250)
        Fix the React Native CLI iOS tests to ensure that they run on CI
        Ensure that CI tests fail if the Maze Runner tests fail

--- test/react-native-cli/scripts/init-and-build-test.sh
+++ test/react-native-cli/scripts/init-and-build-test.sh
@@ -1,11 +1,6 @@
-VERSION=$1
+set -euo pipefail
 
-function check_status {
-  if [[ $1 != 0 ]]; then
-    popd
-    exit $1
-  fi
-}
+VERSION=$1
 
 # Build the app within Maze Runner, checking for the source map upload
 mkdir -p build
@@ -13,19 +8,13 @@ pushd test/react-native-cli
 bundle install
 REACT_NATIVE_VERSION=$VERSION bundle exec maze-runner features/build-app-tests/build-ios-app.feature
 
-# TODO: Reinstate as part of PLAT-6764
-#check_status $?
-
 # Export the IPA separately from MazeRunner (running it inside failed for an unknown reason)
 cd features/fixtures
 xcrun --log xcodebuild -exportArchive -archivePath "${VERSION}/${VERSION}.xcarchive" -exportPath output -verbose -exportOptionsPlist exportOptions.plist
-check_status $?
 
 # Clear the archive away
 rm -rf ${VERSION}/${VERSION}.xcarchive
 
 # Copy file to build directory, ensuring the exit code bubbles up
 cp output/${VERSION}.ipa ../../../../build
-STATUS=$?
 popd
-exit $STATUS
