3daf7167058f218c442f46e2cd1cadcf1d80351b
Ben Wilson <ben.wilson@smartbear.com>
2024-11-06 17:12:02 +0000

    refactor: :recycle: convert browser package to typescript

---
+++ packages/browser/rollup.config.npm.mjs
@@ -0,0 +1,33 @@
+import createRollupConfig from "../../.rollup/index.mjs";
+
+export default createRollupConfig({
+  input: "src/request.ts",
+  external: [
+    '@bugsnag/core/client',
+    '@bugsnag/core/event',
+    '@bugsnag/core/session',
+    '@bugsnag/core/breadcrumb',
+    '@bugsnag/core/config',
+    '@bugsnag/core/types',
+    '@bugsnag/core/lib/es-utils/map',
+    '@bugsnag/core/lib/es-utils/keys',
+    '@bugsnag/core/lib/es-utils/assign',
+    '@bugsnag/plugin-window-onerror',
+    '@bugsnag/plugin-window-unhandled-rejection',
+    '@bugsnag/plugin-app-duration',
+    '@bugsnag/plugin-browser-device',
+    '@bugsnag/plugin-browser-context',
+    '@bugsnag/plugin-browser-request',
+    '@bugsnag/plugin-simple-throttle',
+    '@bugsnag/plugin-console-breadcrumbs',
+    '@bugsnag/plugin-network-breadcrumbs',
+    '@bugsnag/plugin-navigation-breadcrumbs',
+    '@bugsnag/plugin-interaction-breadcrumbs',
+    '@bugsnag/plugin-inline-script-content',
+    '@bugsnag/plugin-browser-session',
+    '@bugsnag/plugin-client-ip',
+    '@bugsnag/plugin-strip-query-string',
+    '@bugsnag/delivery-x-domain-request',
+    '@bugsnag/delivery-xml-http-request'
+  ]
+});
---
+++ packages/browser/src/config.ts
@@ -0,0 +1,25 @@
+import { schema } from '@bugsnag/core/config'
+import assign from '@bugsnag/core/lib/es-utils/assign'
+import getPrefixedConsole from './get-prefixed-console'
+
+const config = {
+  releaseStage: assign({}, schema.releaseStage, {
+    defaultValue: () => {
+      if (/^localhost(:\d+)?$/.test(window.location.host)) return 'development'
+      return 'production'
+    }
+  }),
+  appType: {
+    ...schema.appType,
+    defaultValue: () => 'browser'
+  },
+  logger: assign({}, schema.logger, {
+    defaultValue: () =>
+      // set logger based on browser capability
+      (typeof console !== 'undefined' && typeof console.debug === 'function')
+        ? getPrefixedConsole()
+        : undefined
+  })
+}
+
+export default config
