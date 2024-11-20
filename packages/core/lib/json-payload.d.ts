09f0d0756a882420544d77cab210b71f8542ad8f
Ben Wilson <ben.wilson@smartbear.com>
2024-11-20 12:27:28 +0000

    chore: :label: update type definition

--- packages/core/lib/json-payload.d.ts
+++ packages/core/lib/json-payload.d.ts
@@ -2,9 +2,11 @@ import { Event, Session } from '@bugsnag/core'
 
 type RedactedKey = string | RegExp
 
-declare const JsonPayload: {
+interface JsonPayload {
   event: (event: Event, redactedKeys: RedactedKey[]) => string
   session: (session: Session, redactedKeys: RedactedKey[]) => string
 }
 
-export default JsonPayload
+declare const jsonPayload: JsonPayload
+
+export default jsonPayload
