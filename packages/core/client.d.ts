5d0aab7e755fdeed1b499de5dffb2fa380d80037
Ben Wilson <ben.wilson@smartbear.com>
2024-11-20 12:21:34 +0000

    refactor: :recycle: refactor delivery-x-domain-request to TypeScript ESModules

--- packages/core/client.d.ts
+++ packages/core/client.d.ts
@@ -5,7 +5,7 @@ interface LoggerConfig {
   debug: (msg: any) => void
   info: (msg: any) => void
   warn: (msg: any) => void
-  error: (msg: any) => void
+  error: (msg: any, err?: unknown) => void
 }
 
 interface Notifier {
