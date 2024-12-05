35eb28c37e10f0509df0369b7596be07f10b9d8f
Ben Wilson <ben.wilson@smartbear.com>
2024-12-05 15:22:45 +0000

    Update browser matrix to use iOS 11 (#2269)
    * update test matrix to ios_11
    
    * add ios_11 to browser errors
    
    * re-order browserstack tests
    
    * test: :white_check_mark: create new `skip_before_ios_12` step

---
+++ test/browser/features/support/skip.rb
@@ -0,0 +1,15 @@
+(1..50).each do |version|
+    Before("@skip_ios_#{version}") do
+        if Maze.config.browser == "ios_#{version}"
+            skip_this_scenario("Skipping scenario on iOS #{current_browser}")
+        end
+    end
+  
+    Before("@skip_before_ios_#{version}") do
+        browser_version = Maze.config.browser.sub("ios_", "").to_i
+
+        if browser_version < version
+            skip_this_scenario("Skipping scenario on iOS #{browser_version}")
+        end
+    end
+end
