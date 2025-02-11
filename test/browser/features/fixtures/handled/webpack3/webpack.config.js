50bebe433be573d0fa2453aee431521d13a8cfc7
Dan Skinner <dan.skinner@smartbear.com>
2025-02-11 08:19:59 +0000

    remove es3ify-webpack-plugin

--- test/browser/features/fixtures/handled/webpack3/webpack.config.js
+++ test/browser/features/fixtures/handled/webpack3/webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const webpack = require('webpack')
-const es3ifyPlugin = require('es3ify-webpack-plugin')
 
 module.exports = {
   entry: { a: './src/a.js', b: './src/b.js', c: './src/c.js' },
@@ -8,9 +6,5 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
-  },
-  plugins: [
-    new es3ifyPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ compress: false, mangle: false, ie8: true })
-  ]
+  }
 }
--- test/browser/features/fixtures/handled/webpack4/webpack.config.js
+++ test/browser/features/fixtures/handled/webpack4/webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const es3ifyPlugin = require('es3ify-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: { a: './src/a.js', b: './src/b.js', c: './src/c.js' },
@@ -8,9 +6,5 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
-  },
-  plugins: [
-    new es3ifyPlugin(),
-    new UglifyJsPlugin({ sourceMap: true, uglifyOptions: { compress: false, mangle: false, ie8: true } })
-  ]
+  }
 }
