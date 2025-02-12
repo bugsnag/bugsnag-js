457bb7ba639ca32b8deb3c15c47a94396879778b
Dan Skinner <dan.skinner@smartbear.com>
2025-02-12 15:09:24 +0000

    Revert "remove es3ify-webpack-plugin"
    This reverts commit 50bebe433be573d0fa2453aee431521d13a8cfc7.

--- test/browser/features/fixtures/handled/webpack3/webpack.config.js
+++ test/browser/features/fixtures/handled/webpack3/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const webpack = require('webpack')
+const es3ifyPlugin = require('es3ify-webpack-plugin')
 
 module.exports = {
   entry: { a: './src/a.js', b: './src/b.js', c: './src/c.js' },
@@ -6,5 +8,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
-  }
+  },
+  plugins: [
+    new es3ifyPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ compress: false, mangle: false, ie8: true })
+  ]
 }
--- test/browser/features/fixtures/handled/webpack4/webpack.config.js
+++ test/browser/features/fixtures/handled/webpack4/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const es3ifyPlugin = require('es3ify-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: { a: './src/a.js', b: './src/b.js', c: './src/c.js' },
@@ -6,5 +8,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
-  }
+  },
+  plugins: [
+    new es3ifyPlugin(),
+    new UglifyJsPlugin({ sourceMap: true, uglifyOptions: { compress: false, mangle: false, ie8: true } })
+  ]
 }
