29635176d944bce68a811d411f95822539db5c24
Alex Moinet <alex.moinet@smartbear.com>
2025-02-12 15:14:43 +0000

    Ensure targets are modifiable

--- scripts/update-dependencies.rb
+++ scripts/update-dependencies.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
-require 'bumpsnag'
-
-target_submodule = ENV['TARGET_SUBMODULE']
-target_version = ENV['TARGET_VERSION']
+target_submodule = ENV['TARGET_SUBMODULE'].dup
+target_version = ENV['TARGET_VERSION'].dup
 
 if target_submodule.nil? || target_version.nil?
   raise 'Submodule or version targets not provided, exiting'
@@ -11,7 +9,11 @@ end
 
 pp "Attempting upgrade of #{target_submodule} to #{target_version}"
 
+pp target_version
+
 target_version.delete_prefix!('v') if target_version.start_with?('v')
+
+pp target_version
 
 if target_submodule.eql?('bugsnag-android')
   `packages/react-native/update-android.sh --version #{target_version}`
