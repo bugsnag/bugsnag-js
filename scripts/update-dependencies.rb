419e20f17b0f43bdbcade5d0dfe29cac1b5970fa
Alex Moinet <alex.moinet@smartbear.com>
2025-02-12 15:09:21 +0000

    Ensure version string fed to updated scripts to formatted correctly

--- scripts/update-dependencies.rb
+++ scripts/update-dependencies.rb
@@ -11,6 +11,8 @@ end
 
 pp "Attempting upgrade of #{target_submodule} to #{target_version}"
 
+target_version.delete_prefix!('v') if target_version.start_with?('v')
+
 if target_submodule.eql?('bugsnag-android')
   `packages/react-native/update-android.sh --version #{target_version}`
 elsif target_submodule.eql?('bugsnag-cocoa')
