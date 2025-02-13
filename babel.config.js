04704ba98283b355a0b8afbe59f39f82b413204e
Dan Skinner <dan.skinner@smartbear.com>
2025-02-13 14:56:42 +0000

    revert babel config change non test

--- babel.config.js
+++ babel.config.js
@@ -20,6 +20,22 @@ module.exports = api => {
       test: './packages/plugin-react-navigation/**/*',
       presets: ['@babel/preset-react', 'module:metro-react-native-babel-preset']
     })
+  } else {
+    plugins.push(
+      ['@babel/plugin-transform-arrow-functions'],
+      ['@babel/plugin-transform-block-scoping'],
+      ['@babel/plugin-transform-classes', { loose: true }],
+      ['@babel/plugin-transform-computed-properties', { loose: true }],
+      ['@babel/plugin-transform-destructuring', { loose: true }],
+      ['@babel/plugin-transform-member-expression-literals'],
+      ['@babel/plugin-transform-property-literals'],
+      ['@babel/plugin-transform-parameters', { loose: true }],
+      ['@babel/plugin-transform-shorthand-properties'],
+      ['@babel/plugin-transform-spread', { loose: true }],
+      ['@babel/plugin-transform-template-literals', { loose: true }],
+      ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+      ['@babel/syntax-object-rest-spread']
+    )
   }
 
   if (api && !api.env('test')) {
