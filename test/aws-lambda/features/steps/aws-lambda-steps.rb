2104d8a797324034cb0fe989f2b18dd782ee08b2
Alex Moinet <alex.moinet@smartbear.com>
2025-02-18 09:22:53 +0000

    Test using localhost

--- test/aws-lambda/features/steps/aws-lambda-steps.rb
+++ test/aws-lambda/features/steps/aws-lambda-steps.rb
@@ -1,7 +1,7 @@
 Given('I setup the environment') do
   steps %Q{
     Given I store the api key in the environment variable "BUGSNAG_API_KEY"
-    And I set environment variable "BUGSNAG_NOTIFY_ENDPOINT" to "http://host.docker.internal:9339/notify"
-    And I set environment variable "BUGSNAG_SESSIONS_ENDPOINT" to "http://host.docker.internal:9339/sessions"
+    And I set environment variable "BUGSNAG_NOTIFY_ENDPOINT" to "http://localhost:9339/notify"
+    And I set environment variable "BUGSNAG_SESSIONS_ENDPOINT" to "http://localhost:9339/sessions"
   }
 end
