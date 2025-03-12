487bdea6a33e5307e17bc00b5c5ce516dd713456
hannah-smartbear <hannah.syed@smartbear.com>
2025-03-12 17:32:53 +0000

    Fixing failing tests for hono

--- packages/plugin-hono/src/request-info.js
+++ packages/plugin-hono/src/request-info.js
@@ -3,7 +3,7 @@ module.exports = c => {
     url: c.req.url,
     body: c.req.parseBody(),
     httpMethod: c.req.method,
-    httpVersion: c.env.outgoing.req.httpVersion,
+    httpVersion: c.env.outgoing?.req.httpVersion,
     headers: c.req.header(),
     params: c.req.param(),
     query: c.req.query(),
