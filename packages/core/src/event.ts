7b51408470cf962da00e3343f3bb6224e13c7023
Dan Skinner <dan.skinner@smartbear.com>
2025-03-20 09:39:36 +0000

    type fixes

--- packages/core/src/event.ts
+++ packages/core/src/event.ts
@@ -1,4 +1,4 @@
-import { App, Device, FeatureFlag, Logger, Request, Stackframe, Thread, User, BugsnagError } from "./common"
+import { App, Device, FeatureFlag, Logger, Request, Stackframe, Thread, User, BugsnagError, NotifiableError } from "./common"
 
 import ErrorStackParser from './lib/error-stack-parser'
 // @ts-expect-error no types
@@ -50,7 +50,7 @@ export default class Event {
     public readonly errorMessage: string,
     public readonly stacktrace: any[] = [],
     public readonly _handledState: HandledState = defaultHandledState(),
-    public readonly originalError?: Error | Event | string
+    public readonly originalError?: NotifiableError
   ) {
     this.apiKey = undefined
     this.context = undefined
@@ -173,7 +173,7 @@ static getStacktrace = function (error: Error, errorFramesToSkip: number, backtr
   }
 }
 
-static create = function (maybeError: Error | string, tolerateNonErrors: boolean, handledState: HandledState | undefined, component: string, errorFramesToSkip = 0, logger?: Logger) {
+static create = function (maybeError: NotifiableError, tolerateNonErrors: boolean, handledState: HandledState | undefined, component: string, errorFramesToSkip = 0, logger?: Logger) {
   const [error, internalFrames] = normaliseError(maybeError, tolerateNonErrors, component, logger)
   let event
   try {
