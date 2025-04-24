b34374095e2a1cb9c80de11da966e9bffdfa009c
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-04-24 16:15:14 +0200

    delete iserror from package.json

--- packages/plugin-window-unhandled-rejection/src/unhandled-rejection.ts
+++ packages/plugin-window-unhandled-rejection/src/unhandled-rejection.ts
@@ -1,4 +1,5 @@
-import { isError, Config, Plugin } from '@bugsnag/core'
+import { isError } from '@bugsnag/core'
+import type { Config, Plugin } from '@bugsnag/core'
 import fixBluebirdStacktrace from './fix-bluebird-stacktrace'
 
 type Listener = (evt: PromiseRejectionEvent) => void
