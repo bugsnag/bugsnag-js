f59f6d444b2f1761d29b63599c198aa3766ce2fe
hannah-smartbear <hannah.syed@smartbear.com>
2025-05-02 16:37:58 +0100

    adding ip address and other connection info

--- packages/plugin-hono/src/request-info.js
+++ packages/plugin-hono/src/request-info.js
@@ -1,4 +1,4 @@
-module.exports = c => {
+module.exports = async c => {
   const request = {
     url: c.req.url,
     body: c.req.parseBody(),
@@ -9,6 +9,17 @@ module.exports = c => {
     query: c.req.query(),
     path: c.req.path,
     routePath: c.req.routePath
+  }
+  try {
+    const { getConnInfo } = await import('@hono/node-server/conninfo')
+    if (getConnInfo) {
+      const connection = getConnInfo(c)
+      request.remoteAddress = connection.remote.address
+      request.remoteAddressType = connection.remote.addressType
+      request.remotePort = connection.remote.port
+    }
+  } catch (err) {
+    console.warn('Module not found')
   }
   return request
 }
