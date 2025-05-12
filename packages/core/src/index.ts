7d5787ac8ddc0bfc07c10afba0140a4a3beb72c0
AnastasiiaSvietlova <119674359+AnastasiiaSvietlova@users.noreply.github.com>
2025-05-12 10:59:39 +0200

    Convert @bugsnag/core/lib/iserror to TypeScript (#2422)
    * core: convert iserror to typescript
    
    * fix isError function
    
    * delete iserror from package.json
    
    * fix naming
    
    * delete iserror from rn package.json
    
    * delete iserror from all packages as it not used

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,5 +6,6 @@ export { default as schema } from './config'
 export { default as cloneClient } from './lib/clone-client'
 export { default as jsonPayload } from './lib/json-payload'
 export { default as intRange } from './lib/validators/int-range'
+export { default as isError } from './lib/iserror'
 
 export * from './common'
