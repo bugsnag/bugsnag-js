c2233f04edb860cda3a2b55e54ebd178a5adb5ca
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-05-26 15:24:35 +0200

    convert normalize path to typescript

--- packages/core/src/lib/path-normalizer.ts
+++ packages/core/src/lib/path-normalizer.ts
@@ -2,6 +2,6 @@ import { join, resolve } from 'path'
 
 // normalise a path to a directory, adding a trailing slash if it doesn't already
 // have one and resolve it to make it absolute (e.g. get rid of any ".."s)
-const normalizePath = (p: string): string =>join(resolve(p), '/')
+const normalizePath = (p: string) => join(resolve(p), '/')
 
 export default normalizePath
