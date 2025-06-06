df45cf38ede9e38496d0fa34bf60745b256807dd
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-06-06 15:05:32 +0200

    Convert string-with-length to TypeScript

--- packages/core/src/lib/validators/list-of-functions.js
+++
@@ -1,3 +0,0 @@
-const isArray = require('../es-utils/is-array')
-
-module.exports = value => typeof value === 'function' || (isArray(value) && value.filter(f => typeof f === 'function').length === value.length)
