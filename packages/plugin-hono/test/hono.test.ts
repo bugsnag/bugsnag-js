f33607492ab3380b8f5de4084a518988cdaade74
Ben Wilson <ben.wilson@smartbear.com>
2025-06-11 17:53:31 +0100

    fixup

--- packages/plugin-hono/test/hono.test.ts
+++ packages/plugin-hono/test/hono.test.ts
@@ -1,7 +1,6 @@
-import Client from '@bugsnag/core/client'
 import plugin from '../src/hono'
-import { EventPayload } from '@bugsnag/core'
-import Event from '@bugsnag/core/event'
+import { Client, Event } from '@bugsnag/core'
+import type { EventDeliveryPayload } from '@bugsnag/core'
 
 jest.mock('../src/load-connection-info', () => {
   return jest.fn().mockResolvedValueOnce({
@@ -77,7 +76,7 @@ describe('plugin: hono', () => {
       client._sessionDelegate = { startSession: id, pauseSession: noop, resumeSession: id }
       client._logger = logger()
       client._setDelivery(() => ({
-        sendEvent (payload: EventPayload, c: (err: Error|null, obj: unknown) => void) {
+        sendEvent (payload: EventDeliveryPayload, c: (err: Error|null, obj: unknown) => void) {
           expect(payload.events).toHaveLength(1)
           c(null, payload.events[0])
         },
@@ -147,7 +146,7 @@ describe('plugin: hono', () => {
       client._sessionDelegate = { startSession: id, pauseSession: noop, resumeSession: id }
       client._logger = logger()
       client._setDelivery(() => ({
-        sendEvent (payload: EventPayload, c: (err: Error|null, obj: unknown) => void) {
+        sendEvent (payload: EventDeliveryPayload, c: (err: Error|null, obj: unknown) => void) {
           expect(payload.events).toHaveLength(1)
           c(null, payload.events[0])
         },
@@ -259,7 +258,7 @@ describe('plugin: hono', () => {
       const events: Event[] = []
 
       client2._setDelivery(() => ({
-        sendEvent (payload: EventPayload, c: (err: Error|null, obj: unknown) => void) {
+        sendEvent (payload: EventDeliveryPayload, c: (err: Error|null, obj: unknown) => void) {
           expect(payload.events).toHaveLength(1)
           events.push(payload.events[0] as Event)
         },
@@ -296,7 +295,7 @@ describe('plugin: hono', () => {
       const events: Event[] = []
 
       client._setDelivery(() => ({
-        sendEvent (payload: EventPayload, c: (err: Error|null, obj: unknown) => void) {
+        sendEvent (payload: EventDeliveryPayload, c: (err: Error|null, obj: unknown) => void) {
           expect(payload.events).toHaveLength(1)
           events.push(payload.events[0] as Event)
         },
