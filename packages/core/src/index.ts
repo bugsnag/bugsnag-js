57a421c1eeb57bba29976aff912f4b6103705bbb
AnastasiiaSvietlova <119674359+AnastasiiaSvietlova@users.noreply.github.com>
2025-06-24 15:32:37 +0200

    Convert @bugsnag/core/lib/sync-callback-runner to TypeScript  (#2446)
    * core: convert sync-callback-runner to typescript
    
    * fix types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,5 +7,6 @@ export { default as cloneClient } from './lib/clone-client'
 export { default as jsonPayload } from './lib/json-payload'
 export { default as intRange } from './lib/validators/int-range'
 export { default as isError } from './lib/iserror'
+export { default as runSyncCallbacks } from './lib/sync-callback-runner'
 
 export * from './common'
