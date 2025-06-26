ca0444ab5cc2afb69fe2a8f21f21479f13d47795
Ben Wilson <ben.wilson@smartbear.com>
2025-06-26 15:19:10 +0100

    update polyfill configuration

--- packages/core/rollup.config.npm.mjs
+++ packages/core/rollup.config.npm.mjs
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 import { sharedOutput } from "../../.rollup/index.mjs"
 
@@ -12,6 +13,9 @@ const plugins = [
     extensions
   }),
   commonjs(),
+  nodePolyfills({
+    include: ['path']
+  }),
   babel({
     babelHelpers: 'bundled',
     exclude: 'node_modules/**',
