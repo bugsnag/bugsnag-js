3e9a8181015b4c29005727f5fb871b948666ec05
Ben Wilson <ben.wilson@smartbear.com>
2025-07-09 14:45:48 +0100

    refactor: :wrench: configure babel

--- packages/browser/babel.config.js
+++ packages/browser/babel.config.js
@@ -1,3 +1,33 @@
-const babelConfig = require('../../babel.config.js')
+const rootBabelConfig = require('../../babel.config.js')
 
-module.exports = babelConfig
+module.exports = api => {
+  // For production builds (browser), use modern preset targeting old browsers
+  if (api && !api.env('test')) {
+    return {
+      presets: [
+        ['@babel/preset-env', {
+          // Target Chrome 43 and IE 11 for maximum compatibility
+          targets: {
+            chrome: '43',
+            ie: '11'
+          },
+          // Only include transforms that are needed
+          bugfixes: true,
+          // Use loose mode for smaller bundle size
+          loose: true,
+          // Don't include polyfills, only transform syntax
+          useBuiltIns: false,
+          // Exclude modules transformation since rollup handles it
+          modules: false
+        }]
+      ],
+      plugins: [
+        // Add specific plugins for Object.assign polyfill if needed
+        ['@babel/plugin-transform-object-assign']
+      ]
+    }
+  }
+  
+  // For tests and other environments, use the root config
+  return rootBabelConfig(api)
+}
