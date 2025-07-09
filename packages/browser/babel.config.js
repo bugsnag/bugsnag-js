e711de5b65a5e08d7de30a0d9204688db85ade40
Ben Wilson <ben.wilson@smartbear.com>
2025-07-09 16:56:34 +0100

    update babel config

--- packages/browser/babel.config.js
+++ packages/browser/babel.config.js
@@ -11,19 +11,33 @@ module.exports = api => {
             chrome: '43',
             ie: '11'
           },
-          // Only include transforms that are needed
-          bugfixes: true,
+          // Disable bugfixes to ensure maximum compatibility
+          bugfixes: false,
           // Use loose mode for smaller bundle size
           loose: true,
           // Don't include polyfills, only transform syntax
           useBuiltIns: false,
           // Exclude modules transformation since rollup handles it
-          modules: false
+          modules: false,
+          // Force all transforms to ensure ES5 compatibility
+          forceAllTransforms: true
         }]
       ],
       plugins: [
         // Add specific plugins for Object.assign polyfill if needed
-        ['@babel/plugin-transform-object-assign']
+        ['@babel/plugin-transform-object-assign'],
+        // Explicitly transform const/let to var
+        ['@babel/plugin-transform-block-scoping'],
+        // Transform arrow functions
+        ['@babel/plugin-transform-arrow-functions'],
+        // Transform classes
+        ['@babel/plugin-transform-classes'],
+        // Transform spread
+        ['@babel/plugin-transform-spread'],
+        // Transform template literals
+        ['@babel/plugin-transform-template-literals'],
+        // Transform destructuring
+        ['@babel/plugin-transform-destructuring']
       ]
     }
   }
