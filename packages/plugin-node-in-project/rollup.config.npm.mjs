400a3abe8a9d4aa8b0ab2fbf71dc28169158e28f
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-07-29 11:07:25 +0200

    update rollup

--- packages/plugin-node-in-project/rollup.config.npm.mjs
+++ packages/plugin-node-in-project/rollup.config.npm.mjs
@@ -2,5 +2,5 @@ import createRollupConfig from "../../.rollup/index.mjs";
 
 export default createRollupConfig({
   input: "src/in-project.ts",
-  external: [/node_modules/],
+  external: ["@bugsnag/path-normalizer"],
 });
