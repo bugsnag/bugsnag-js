f67c13fadd6b075c081a3c982afbbee190e54125
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-09-05 13:53:42 +0200

    update rollup config

--- packages/plugin-contextualize/rollup.config.npm.mjs
+++ packages/plugin-contextualize/rollup.config.npm.mjs
@@ -2,5 +2,5 @@ import createRollupConfig from "../../.rollup/index.mjs"
 
 export default createRollupConfig({
     input: "src/contextualize.ts",
-    external: [/node_modules/],
+    external: ['async_hooks'],
 })
