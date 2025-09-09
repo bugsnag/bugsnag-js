1c73e036a852aaa0c61142e4ba8549045ba23c88
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-09-09 12:58:42 +0200

    convert plugin-restify to ts

---
+++ packages/plugin-restify/src/request-info.ts
@@ -0,0 +1,86 @@
+import { extractObject } from '@bugsnag/core'
+
+export interface RestifyRequest {
+  url?: string
+  connection?: {
+    remoteAddress?: string
+    remotePort?: number
+    bytesRead?: number
+    bytesWritten?: number
+    localPort?: number
+    localAddress?: string
+    IPVersion?: string
+    address?: () => {
+      port?: number
+      address?: string
+      family?: string
+    }
+  }
+  method: string  // Required in practice
+  headers: Record<string, string | undefined>
+  httpVersion?: string
+  getPath(): string  // Required in practice
+  absoluteUri(path: string): string  // Required in practice
+  params?: Record<string, any>
+  query?: Record<string, any>
+  body?: Record<string, any>
+}
+
+export interface RequestInfo {
+    url: string
+    path: string
+    httpMethod: string
+    headers: Record<string, string | undefined>
+    httpVersion?: string
+    params?: Record<string, any>
+    query?: Record<string, any>
+    body?: Record<string, any>
+    clientIp?: string
+    referer?: string
+    connection?: {
+      remoteAddress?: string
+      remotePort?: number
+      bytesRead?: number
+      bytesWritten?: number
+      localPort?: number
+      localAddress?: string
+      IPVersion?: string
+    }
+}
+
+const extractRequestInfo = (req: RestifyRequest): RequestInfo => {
+  const connection = req.connection
+  const address = connection && connection.address && connection.address()
+  const portNumber = address && address.port
+  const path = req.getPath() || req.url
+  const url = req.absoluteUri(path as string)
+  const request: RequestInfo = {
+    url: url,
+    path: path as string,
+    httpMethod: req.method,
+    headers: req.headers,
+    httpVersion: req.httpVersion
+  }
+
+  request.params = extractObject(req, 'params')
+  request.query = extractObject(req, 'query')
+  request.body = extractObject(req, 'body')
+
+  request.clientIp = req.headers['x-forwarded-for'] || (connection ? connection.remoteAddress : undefined)
+  request.referer = req.headers.referer || req.headers.referrer
+
+  if (connection) {
+    request.connection = {
+      remoteAddress: connection.remoteAddress,
+      remotePort: connection.remotePort,
+      bytesRead: connection.bytesRead,
+      bytesWritten: connection.bytesWritten,
+      localPort: portNumber,
+      localAddress: address ? address.address : undefined,
+      IPVersion: address ? address.family : undefined
+    }
+  }
+  return request
+}
+
+export default extractRequestInfo
---
+++ packages/plugin-restify/src/restify.ts
@@ -0,0 +1,130 @@
+import type { Client, Plugin } from '@bugsnag/core'
+import { cloneClient } from '@bugsnag/core'
+import { AsyncLocalStorage } from 'async_hooks'
+import type { Next, Request, Response } from 'restify'
+import restify from 'restify'
+import type { RequestInfo, RestifyRequest } from './request-info'
+import extractRequestInfo from './request-info'
+
+declare module 'restify' {
+  interface Request {
+    bugsnag?: Client
+  }
+}
+
+// add a new call signature for the getPlugin() method that types the plugin result
+declare module '@bugsnag/core' {
+  interface Client {
+    getPlugin(id: 'restify'): BugsnagPluginRestifyResult | undefined
+  }
+}
+
+interface RestifyError extends Error {
+  statusCode?: number
+}
+
+interface BugsnagPluginRestifyResult {
+  requestHandler: restify.RequestHandler
+  errorHandler: (req: restify.Request, res: restify.Response, err: RestifyError, cb: (...args: any[]) => void) => void
+}
+
+interface ExtractedRequestData {
+  metadata: Omit<RequestInfo, 'body'>
+  request: {
+    body?: Record<string, any>
+    clientIp?: string
+    headers: Record<string, any>
+    httpMethod: string
+    url: string
+    referer?: string
+  }
+}
+
+interface InternalClient extends Client {
+  _clientContext: AsyncLocalStorage<Client>
+}
+
+const handledState = {
+  severity: 'error',
+  unhandled: true,
+  severityReason: {
+    type: 'unhandledErrorMiddleware',
+    attributes: { framework: 'Restify' }
+  }
+}
+
+const plugin: Plugin = {
+  name: 'restify',
+  load: (client: Client): BugsnagPluginRestifyResult => {
+    const internalClient = client as InternalClient
+
+    const requestHandler = (req: Request, res: Response, next: Next) => {
+      // clone the client to be scoped to this request. If sessions are enabled, start one
+      const requestClient = cloneClient(internalClient)
+      if (requestClient._config.autoTrackSessions) {
+        requestClient.startSession()
+      }
+
+      // attach it to the request
+      req.bugsnag = requestClient
+
+      // extract request info and pass it to the relevant bugsnag properties
+      requestClient.addOnError((event) => {
+        const { request, metadata } = getRequestAndMetadataFromReq(req)
+        event.request = { ...event.request, ...request }
+        event.addMetadata('request', metadata)
+        if (event._handledState.severityReason.type === 'unhandledException') {
+          // Modify the event properties directly instead of reassigning readonly _handledState
+          event.severity = handledState.severity
+          event.unhandled = handledState.unhandled
+          event._handledState.severityReason = handledState.severityReason
+        }
+      }, true);
+
+      internalClient._clientContext.run(requestClient, next)
+    }
+
+    const errorHandler = (req: Request, res: Response, err: RestifyError, cb: () => void) => {
+      const internalClient = client as InternalClient
+
+      if (!internalClient._config.autoDetectErrors) return cb()
+      if (err.statusCode && err.statusCode < 500) return cb()
+
+      const event = internalClient.Event.create(err, false, handledState, 'restify middleware', 1)
+      const { metadata, request } = getRequestAndMetadataFromReq(req)
+      event.request = { ...event.request, ...request }
+      event.addMetadata('request', metadata)
+
+      if (req.bugsnag) {
+        req.bugsnag._notify(event)
+      } else {
+        internalClient._logger.warn(
+          'req.bugsnag is not defined. Make sure the @bugsnag/plugin-restify requestHandler middleware is added first.'
+        )
+        internalClient._notify(event)
+      }
+      cb()
+    }
+
+    return { requestHandler, errorHandler }
+  }
+}
+
+const getRequestAndMetadataFromReq = (req: Request): ExtractedRequestData => {
+  // Type assertion is safe here because we know Restify requests have the properties we need
+  const { body, ...requestInfo } = extractRequestInfo(req as unknown as RestifyRequest)
+  return {
+    metadata: requestInfo,
+    request: {
+      body,
+      clientIp: requestInfo.clientIp,
+      headers: requestInfo.headers,
+      httpMethod: requestInfo.httpMethod,
+      url: requestInfo.url,
+      referer: requestInfo.referer // Not part of the notifier spec for request but leaving for backwards compatibility
+    }
+  }
+}
+
+// module.exports.default = module.exports
+export default plugin
