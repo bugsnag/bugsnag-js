a5dd56e646d716d9ea565b4e91da543c73f53e05
Ben Wilson <ben.wilson@smartbear.com>
2025-09-24 09:20:12 +0100

    refactor web-worker package

---
+++ packages/web-worker/src/config.ts
@@ -0,0 +1,26 @@
+/* eslint-env worker, serviceworker */
+
+import { schema } from '@bugsnag/core'
+
+import getPrefixedConsole from './get-prefixed-console'
+
+export default {
+  appType: {
+    ...schema.appType,
+    defaultValue: () => 'workerjs'
+  },
+  logger: Object.assign({}, schema.logger, {
+    defaultValue: () =>
+      (typeof console !== 'undefined' && typeof console.debug === 'function')
+        ? getPrefixedConsole()
+        : undefined
+  }),
+  autoTrackSessions: {
+    ...schema.autoTrackSessions,
+    defaultValue: () => false
+  },
+  autoDetectErrors: {
+    ...schema.autoTrackSessions,
+    defaultValue: () => false
+  }
+}
