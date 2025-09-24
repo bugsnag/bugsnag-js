a8b2bea1997eeb17d211e86591ee47705988bc10
Ben Wilson <ben.wilson@smartbear.com>
2025-09-24 09:20:12 +0100

    refactor web-worker package

--- packages/web-worker/src/prevent-discard.ts
+++ packages/web-worker/src/prevent-discard.ts
@@ -1,11 +1,13 @@
 /* eslint-env worker, serviceworker */
 
+import { Client } from '@bugsnag/core'
+
 const extensionRegex = /^(chrome|moz|safari|safari-web)-extension:/
 
-module.exports = {
+export default {
   name: 'preventDiscard',
-  load: client => {
-    client.addOnError(event => {
+  load: (client: Client) => {
+    client.addOnError((event) => {
       event.errors.forEach(({ stacktrace }) => {
         stacktrace.forEach(function (frame) {
           frame.file = frame.file.replace(extensionRegex, '$1_extension:')
