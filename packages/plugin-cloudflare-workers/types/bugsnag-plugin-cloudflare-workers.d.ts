84a453878b65345944e7d4d1785d8a6b7a23a589
Yousif Ahmed <yousif.ahmed@smartbear.com>
2025-12-15 17:14:53 +0000

    refactor(plugin-cloudflare-workers): rework type declarations

--- packages/plugin-cloudflare-workers/types/bugsnag-plugin-cloudflare-workers.d.ts
+++ packages/plugin-cloudflare-workers/types/bugsnag-plugin-cloudflare-workers.d.ts
@@ -1,22 +1,11 @@
-import { Plugin, Client } from '@bugsnag/core'
-
+import { Plugin } from '@bugsnag/core'
 declare const BugsnagPluginCloudflareWorkers: Plugin
 export default BugsnagPluginCloudflareWorkers
-
-interface ExecutionContext {
-  waitUntil(promise: Promise<any>): void
-}
-
-type CloudflareWorkersHandler = (request: Request, env: any, ctx: ExecutionContext) => Promise<Response>
-
-export type BugsnagPluginCloudflareWorkersHandler = (handler: CloudflareWorkersHandler) => CloudflareWorkersHandler
-
 export interface BugsnagPluginCloudflareWorkersConfiguration {
   flushTimeoutMs?: number
 }
-
 export interface BugsnagPluginCloudflareWorkersResult {
-  createHandler(configuration?: BugsnagPluginCloudflareWorkersConfiguration): BugsnagPluginCloudflareWorkersHandler
+  createHandler(configuration?: BugsnagPluginCloudflareWorkersConfiguration): <T>(handler: T) => T
 }
 
 // add a new call signature for the getPlugin() method that types the plugin result
