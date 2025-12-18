f474aff05e4aee8c797a7b8d2d1e83880c5d7ea3
Ben Wilson <ben.wilson@smartbear.com>
2025-12-18 12:07:31 +0000

    remove use of Object.entries

--- packages/plugin-http-errors/lib/redact-query-parameters.js
+++ packages/plugin-http-errors/lib/redact-query-parameters.js
@@ -4,7 +4,14 @@ const defaultBaseURL = 'http://invalid-base.com'
 module.exports = function (url, redactedKeys) {
   const urlObj = new URL(url, defaultBaseURL) // base needed for relative URLs
   const params = new URLSearchParams(urlObj.search)
-  const redactedParams = redactValues(Object.fromEntries(params), redactedKeys)
+
+  // Convert URLSearchParams to object without using Object.fromEntries()
+  const paramsObject = {}
+  params.forEach((value, key) => {
+    paramsObject[key] = value
+  })
+
+  const redactedParams = redactValues(paramsObject, redactedKeys)
   urlObj.search = new URLSearchParams(redactedParams).toString()
   const urlString = decodeURI(urlObj.toString())
   return urlString.startsWith(defaultBaseURL)
