dcac4278f4a8d712dd4b8e93aa7bc42b8a71690f
Yousif Ahmed <yousif.ahmed@smartbear.com>
2025-12-19 12:30:26 +0000

    test(plugin-cloudflare-workers): add e2e test fixture and feature

---
+++ test/cloudflare-workers/features/support/env.rb
@@ -0,0 +1,16 @@
+BeforeAll do
+  success = system(File.realpath("#{__dir__}/../scripts/build-fixtures"))
+
+  unless success
+    puts "Unable to build fixtures!"
+    exit 1
+  end
+
+  Maze.config.enforce_bugsnag_integrity = false
+  Maze.config.receive_no_requests_wait = 10
+  Maze.config.receive_requests_wait = 10
+end
+
+After do
+  Maze::Runner.kill_running_scripts
+end
