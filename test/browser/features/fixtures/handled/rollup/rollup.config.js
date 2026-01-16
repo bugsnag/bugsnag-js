7ad230f37329199763b816cf06dc8c75ebce87a0
Ben Wilson <ben.wilson@smartbear.com>
2026-01-16 12:29:02 +0000

    update rollup fixture to use latest version

--- test/browser/features/fixtures/handled/rollup/rollup.config.js
+++ test/browser/features/fixtures/handled/rollup/rollup.config.js
@@ -1,13 +1,12 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: `src/${process.env.ENTRY_NAME}.js`,
   output: {
     file: `dist/${process.env.ENTRY_NAME}.js`,
     format: 'iife',
-    legacy: true,
     name: '____'
   },
-  plugins: [ resolve({ browser: true }), commonjs() ]
+  plugins: [ nodeResolve({ browser: true }), commonjs() ]
 };
