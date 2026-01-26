02fcbdc2d1cd40d2df2955da98b269f91bb8d067
Yousif Ahmed <yousif.ahmed@smartbear.com>
2026-01-26 15:05:18 +0000

    chore(plugin-aws-lambda): remove erroneous files

--- packages/plugin-aws-lambda/src/lambda-timeout-approaching.js
+++
@@ -1,8 +0,0 @@
-export default class LambdaTimeoutApproaching extends Error {
-    constructor(remainingMs) {
-        const message = `Lambda will timeout in ${remainingMs}ms`;
-        super(message);
-        this.name = 'LambdaTimeoutApproaching';
-        this.stack = undefined;
-    }
-}
