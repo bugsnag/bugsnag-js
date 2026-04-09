8f719a1fa792a408ddf40f4bcf6874d62e2c4bf8
Yousif Ahmed <yousif.ahmed@smartbear.com>
2026-04-09 12:02:06 +0100

    build(deps): update react native dev dependencies and fix react-navigation unit tests

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = api => {
     presets.push(['@babel/preset-env', {targets: {node: 'current'}}])
     presets.push('@babel/preset-typescript')
     overrides.push({
-      test: '**/node_modules/react-native/**/*',
+      test: ['node_modules/react-native/**/*', 'node_modules/@react-native/**/*'],
       presets: ['module:metro-react-native-babel-preset', '@babel/preset-typescript']
     })
     overrides.push({
