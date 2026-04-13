40d8216f8a9f809add027617fb6c77e6d485bdfd
Yousif Ahmed <yousif.ahmed@smartbear.com>
2026-04-13 17:06:54 +0100

    test(react-native): fix issue with react-test-renderer

--- packages/react-native/src/test/setup.js
+++ packages/react-native/src/test/setup.js
@@ -4,3 +4,6 @@ if (global.window === undefined) {
 
 // trick the notifier in thinking it's not running in the remote debugger
 global.nativeCallSyncHook = () => {}
+
+// Fix for react-test-renderer causing tests to hang, see https://github.com/facebook/react/issues/20756#issuecomment-780927519
+delete global.MessageChannel;
