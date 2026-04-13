10a4193fb53ca73e94aff7c0c13ac3695d41ba44
Yousif Ahmed <yousif.ahmed@smartbear.com>
2026-04-13 16:23:22 +0100

    fuxip fixjsdom

--- jest/FixJSDOMEnvironment.js
+++ jest/FixJSDOMEnvironment.js
@@ -1,18 +1,28 @@
+const JSDOMEnvironment = require('jest-environment-jsdom').TestEnvironment;
 const { TextDecoder, TextEncoder } = require('node:util')
 const crypto = require('crypto')
-
-const JSDOMEnvironment = require('jest-environment-jsdom')
-
 class FixJSDOMEnvironment extends JSDOMEnvironment {
-  constructor (...args) {
-    super(...args)
+  constructor(config, context) {
+    super(config, context);
+  }
 
+  async setup() {
+    await super.setup();
     this.global.TextEncoder = TextEncoder
     this.global.TextDecoder = TextDecoder
-    this.global.crypto = {
-      subtle: crypto.webcrypto.subtle
-    }
+    this.global.crypto.subtle = crypto.webcrypto.subtle
+  }
+
+  async teardown() {
+    this.global.TextEncoder = undefined
+    this.global.TextDecoder = undefined
+    this.global.crypto = undefined
+    await super.teardown();
+  }
+
+  getVmContext() {
+    return super.getVmContext();
   }
 }
 
-module.exports = FixJSDOMEnvironment
+module.exports = FixJSDOMEnvironment;
