ec0dcf066f4e03da0dd6f504e4442867b11d7136
Ben Wilson <ben.wilson@smartbear.com>
2026-04-14 18:23:42 +0100

    bundle node dependencies

--- packages/node/rollup.config.mjs
+++ packages/node/rollup.config.mjs
@@ -1,26 +1,5 @@
 import createRollupConfig from '../../.rollup/index.mjs'
 
 export default createRollupConfig({
-  input: './src/bugsnag.ts',
-  external: [
-    '@bugsnag/core',
-    '@bugsnag/plugin-node-surrounding-code',
-    '@bugsnag/delivery-node',
-    '@bugsnag/plugin-app-duration',
-    '@bugsnag/plugin-console-breadcrumbs',
-    '@bugsnag/plugin-contextualize',
-    '@bugsnag/plugin-intercept',
-    '@bugsnag/plugin-node-device',
-    '@bugsnag/plugin-node-in-project',
-    '@bugsnag/plugin-node-surrounding-code',
-    '@bugsnag/plugin-node-uncaught-exception',
-    '@bugsnag/plugin-node-unhandled-rejection',
-    '@bugsnag/plugin-server-session',
-    '@bugsnag/plugin-stackframe-path-normaliser',
-    '@bugsnag/plugin-strip-project-root',
-    'byline',
-    'error-stack-parser',
-    'pump',
-    'stack-generator'
-  ]
+  input: './src/bugsnag.ts'
 })
