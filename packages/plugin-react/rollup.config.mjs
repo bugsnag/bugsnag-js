0bb8d2e2156881f019a849de945c52ea6624d405
Yousif Ahmed <yousif.ahmed@smartbear.com>
2026-04-22 16:47:10 +0100

    build: reinstate babel for react and vue plugin builds

--- packages/plugin-react/rollup.config.mjs
+++ packages/plugin-react/rollup.config.mjs
@@ -1,9 +1,16 @@
+import babel from '@rollup/plugin-babel'
 import createRollupConfig from "../../.rollup/index.mjs"
 
 const config = createRollupConfig({
   external: [
     '@bugsnag/core',
     'react'
+  ],
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.ts', '.tsx']
+    })
   ]
 })
 
--- packages/plugin-vue/rollup.config.mjs
+++ packages/plugin-vue/rollup.config.mjs
@@ -1,7 +1,14 @@
+import babel from '@rollup/plugin-babel'
 import createRollupConfig from "../../.rollup/index.mjs"
 
 const config = createRollupConfig({
-  input: 'src/index.ts'
+  input: 'src/index.ts',
+  plugins: [
+      babel({
+        babelHelpers: 'bundled',
+        extensions: ['.ts']
+      })
+    ]
 })
 
 export default config
