b56b767b1e6478d99e0251bd61265fc3ab0c2d4b
Juraci <juraci.vieira@gmail.com>
2026-05-04 11:38:17 -0300

    fix(plugin-electron-client-state-persistence): unbreak build on glibc >= 2.28
    Vendored tinycthread redefined once_flag and call_once on POSIX, which
    collides with the C11 once_flag glibc 2.28+ exposes through <stdlib.h>.
    The plugin only uses mtx_* primitives; once_flag/call_once are unused on
    POSIX. Drop the colliding #else branches; Win32 path is untouched.

--- packages/plugin-electron-client-state-persistence/src/deps/tinycthread/tinycthread.h
+++ packages/plugin-electron-client-state-persistence/src/deps/tinycthread/tinycthread.h
@@ -456,9 +456,6 @@ int tss_set(tss_t key, void *val);
     CRITICAL_SECTION lock;
   } once_flag;
   #define ONCE_FLAG_INIT {0,}
-#else
-  #define once_flag pthread_once_t
-  #define ONCE_FLAG_INIT PTHREAD_ONCE_INIT
 #endif
 
 /** Invoke a callback exactly once
@@ -468,8 +465,6 @@ int tss_set(tss_t key, void *val);
  */
 #if defined(_TTHREAD_WIN32_)
   void call_once(once_flag *flag, void (*func)(void));
-#else
-  #define call_once(flag,func) pthread_once(flag,func)
 #endif
 
 #ifdef __cplusplus
