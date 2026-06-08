a7280695b4e29a67847932da96d9055c433480f6
SB-priyankap <priyanka.potdar@smartbear.com>
2026-06-08 14:40:45 +0530

    plat-16468-plugin-server-session-JS-to-TS

---
+++ packages/plugin-server-session/rollup.config.mjs
@@ -0,0 +1,12 @@
+import createRollupConfig from "../../.rollup/index.mjs"
+
+const config = createRollupConfig({
+  input: './src/session.ts',
+  external: [
+    '@bugsnag/core',
+    "events", 
+    "backo"
+  ]
+})
+
+export default config
