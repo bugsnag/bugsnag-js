ac7799981aa5ee0428bafdd26c51b9da596546ee
SB-priyankap <priyanka.potdar@smartbear.com>
2026-06-09 17:20:33 +0530

    fix review changes

--- packages/plugin-node-in-project/src/in-project.ts
+++ packages/plugin-node-in-project/src/in-project.ts
@@ -5,17 +5,30 @@ interface PluginConfig extends Config {
   projectRoot?: string
 }
 
+const NODE_MODULES_PATH_REGEX = /\/node_modules\//
+
 const plugin: Plugin<PluginConfig> = {
-  load: client => client.addOnError(event => {
-    if (!client._config.projectRoot) return
-    const projectRoot = normalizePath(client._config.projectRoot)
-    const allFrames: Stackframe[]= event.errors.reduce((accum: Stackframe[], er) => accum.concat(er.stacktrace), [])
-    allFrames.map(stackframe => {
-      stackframe.inProject = typeof stackframe.file === 'string' &&
-        stackframe.file.indexOf(projectRoot) === 0 &&
-        !/\/node_modules\//.test(stackframe.file)
+  load: (client) => {
+    client.addOnError((event) => {
+      const configuredProjectRoot = client._config.projectRoot
+      if (!configuredProjectRoot) return
+
+      const projectRoot = normalizePath(configuredProjectRoot)
+
+      const allFrames: Stackframe[] = event.errors.reduce(
+        (accum: Stackframe[], error) => accum.concat(error.stacktrace),
+        []
+      )
+
+      allFrames.forEach((stackframe: Stackframe) => {
+        const file = stackframe.file
+        stackframe.inProject =
+          typeof file === 'string' &&
+          file.startsWith(projectRoot) &&
+          !NODE_MODULES_PATH_REGEX.test(file)
+      })
     })
-  })
+  }
 }
 
 export default plugin
