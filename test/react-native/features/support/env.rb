d5b14732832fffb66150e80c4780f17c9ab65cb6
sb-gaurava <gaurav.agarwal@smartbear.com>
2026-09-21 02:14:39 +0530

    "fix(e2e): fix platform skip detection for BitBar Android 15 matrix [PLAT-17304]"

--- test/react-native/features/support/env.rb
+++ test/react-native/features/support/env.rb
@@ -8,16 +8,59 @@ BeforeAll do
   end
 end
 
+def is_android?
+  return true if Maze.config.os&.downcase == 'android'
+  return true if Maze.config.device&.downcase&.include?('android')
+  return true if Maze::Helper.get_current_platform&.downcase == 'android'
+  return true if Maze.driver && Maze.driver.capabilities['platformName']&.downcase == 'android'
+  false
+end
+
+def is_ios?
+  return true if Maze.config.os&.downcase == 'ios'
+  return true if Maze.config.device&.downcase&.include?('ios')
+  return true if Maze::Helper.get_current_platform&.downcase == 'ios'
+  return true if Maze.driver && Maze.driver.capabilities['platformName']&.downcase == 'ios'
+  false
+end
+
 Before do
   Maze::Api::Appium::DeviceManager.new.set_rotation(:portrait)
+end
 
-  # Push fixture config directly to app sandbox on Android 15
-  if Maze.driver && Maze::Helper.get_current_platform == 'android'
-    config = JSON.generate({ maze_address: Maze.public_address })
-    begin
-      Maze.driver.push_file('/sdcard/Android/data/com.reactnative/files/fixture_config.json', config)
-    rescue => e
-      $logger.warn "Failed to push to app sandbox: #{e.message}"
-    end
-  end
+Before('@android_only') do |scenario|
+  skip_this_scenario("Skipping scenario on non-Android") unless is_android?
+end
+
+Before('@ios_only') do |scenario|
+  skip_this_scenario("Skipping scenario on non-iOS") if is_android? || !is_ios?
+end
+
+Before('@navigation') do |scenario|
+  skip_this_scenario("Skipping scenario") if ENV['SKIP_NAVIGATION_SCENARIOS'].eql?('true')
+end
+
+Before('@react_navigation') do |scenario|
+  skip_this_scenario("Skipping scenario: Not running react-navigation fixture") unless ENV["REACT_NAVIGATION"] == 'true'
+end
+
+Before('@react-native-navigation') do |scenario|
+  skip_this_scenario("Skipping scenario") unless ENV['REACT_NATIVE_NAVIGATION'].eql?('true')
+end
+
+Before('@skip_hermes') do |_scenario|
+  skip_this_scenario("Skipping scenario") if ENV['HERMES'].eql?('true')
+end
+
+Before('@skip_new_arch') do |scenario|
+  skip_this_scenario("Skipping scenario") if ENV['RCT_NEW_ARCH_ENABLED'].eql?('1')
+end
+
+Before('@skip_old_arch') do |scenario|
+  skip_this_scenario("Skipping scenario") unless ENV['RCT_NEW_ARCH_ENABLED'].eql?('1')
+end
+
+Before('@skip_new_arch_below_074') do |scenario|
+  current_version = ENV['RN_VERSION'].nil? ? 0 : ENV['RN_VERSION'].to_f
+  skip_this_scenario("Skipping scenario") if ENV['RCT_NEW_ARCH_ENABLED'].eql?('1') && current_version < 0.74
 end
